0ca494dd87ed1929d6a2733617c9699fd6101f40
James Nightingale <james.w.nightingale@durham.ac.uk>
2017-10-02 19:21:52 +0100

    Added function get_dimensions_arcsec.

--- LensData/ArrayTools.py
+++ LensData/ArrayTools.py
@@ -8,3 +8,18 @@ def get_dimensions_pixels(array):
     ydim = array.shape[1]
 
     return xdim, ydim
+
+def get_dimensions_arcsec(xdim, ydim, pixel_scale):
+    """Get the x and y dimensions of a 2D array in arc seconds, given its number of pixels and pixel scale"""
+
+    if (xdim <= 0):
+        raise ValueError('ArrayTools.get_dimensions_arcsec - xdim = {} - negative x dimension supplied'.format(xdim))
+    elif (ydim <= 0):
+        raise ValueError('ArrayTools.get_dimensions_arcsec - ydim = {} - negative y dimension supplied'.format(ydim))
+    elif (pixel_scale <= 0):
+        raise ValueError('ArrayTools.get_dimensions_arcsec - pixel_scale = {} - negative pixel scale supplied'.format(pixel_scale))
+
+    xarc = xdim*pixel_scale
+    yarc = ydim*pixel_scale
+
+    return xarc, yarc
