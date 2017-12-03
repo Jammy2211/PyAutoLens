d289ed551c6898b6cc71804c904aab973eb2f71f
Richard <richardhayes777@gmail.com>
2017-12-03 16:56:06 +0000

    docs

--- auto_lens/profile.py
+++ auto_lens/profile.py
@@ -360,21 +360,49 @@ class SersicLightProfile(EllipticalProfile, LightProfile):
         self.sersic_index = sersic_index
 
     def as_sersic_profile(self):
+        """
+
+        Returns
+        -------
+        profile : SersicLightProfile
+            A new sersic profile with parameters taken from this profile
+        """
         return SersicLightProfile(axis_ratio=self.axis_ratio, phi=self.phi, flux=self.flux,
                                   effective_radius=self.effective_radius, sersic_index=self.sersic_index,
                                   centre=self.centre)
 
     def as_core_sersic_profile(self, radius_break, flux_break, gamma, alpha):
+        """
+
+        Returns
+        -------
+        profile : CoreSersicLightProfile
+            A new core sersic profile with parameters taken from this profile
+        """
         return CoreSersicLightProfile(axis_ratio=self.axis_ratio, phi=self.phi, flux=self.flux,
                                       effective_radius=self.effective_radius, sersic_index=self.sersic_index,
                                       radius_break=radius_break, flux_break=flux_break, gamma=gamma, alpha=alpha,
                                       centre=self.centre)
 
     def as_exponential_profile(self):
+        """
+
+        Returns
+        -------
+        profile : ExponentialLightProfile
+            A new exponential profile with parameters taken from this profile
+        """
         return ExponentialLightProfile(axis_ratio=self.axis_ratio, phi=self.phi, flux=self.flux,
                                        effective_radius=self.effective_radius, centre=self.centre)
 
     def as_dev_vaucouleurs_profile(self):
+        """
+
+        Returns
+        -------
+        profile : DevVaucouleursLightProfile
+            A new dev vaucouleurs profile with parameters taken from this profile
+        """
         return DevVaucouleursLightProfile(axis_ratio=self.axis_ratio, phi=self.phi, flux=self.flux,
                                           effective_radius=self.effective_radius, centre=self.centre)
 
