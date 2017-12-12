e9fdbc06b14ec804dbed63152eeb8047da61eed5
Jammy2211 <james.w.nightingale@durham.ac.uk>
2017-12-12 11:33:27 +0000

    Updating image class and tests to use tuples

--- auto_lens/test_image.py
+++ auto_lens/test_image.py
@@ -6,17 +6,6 @@ import os
 
 test_data_dir = "{}/../data/test_data/".format(os.path.dirname(os.path.realpath(__file__)))
 
-
-@pytest.fixture(scope='class')
-def test_image():
-    im = image.Image.via_fits(file_name='3x3_ones.fits', hdu=0, pixel_scale=0.1, path=test_data_dir)
-    return im
-
-@pytest.fixture(scope='class')
-def test_psf():
-    psf = image.PSF.via_fits(file_name='3x3_ones.fits', hdu=0, pixel_scale=0.1, path=test_data_dir)
-    return psf
-
 # noinspection PyClassHasNoInit,PyShadowingNames
 class TestData:
 
@@ -27,106 +16,147 @@ class TestData:
         def test__trim_7x7_to_3x3_centred_on_image_middle(self):
 
             data = np.ones((7, 7))
-            data[3,3] = 2
+            data[3,3] = 2.0
 
-            psf = image.PSF(data, pixel_scale=0.1)
+            data = image.Data(data, pixel_scale=0.1)
 
+            data.trim_data(x_size=3, y_size=3)
 
+            assert data.data == np.array([[1.0, 1.0, 1.0],
+                                         [1.0, 2.0, 1.0],
+                                         [1.0, 1.0, 1.0]])
+
+            assert data.xy_dim[:] == 3
+            assert data.xy_arcsec[:] == 0.3
 
 # noinspection PyClassHasNoInit,PyShadowingNames
 class TestImage:
 
-    class TestInput:
+    class TestSetup:
 
-        def test__init__input_image_3x3__all_attributes_correct(self, test_image):
+        def test__init__input_image_3x3__all_attributes_correct(self):
+
+            test_image = image.Image(image=np.ones((3,3)), pixel_scale=0.1)
 
             assert (test_image.data == np.ones((3, 3))).all()
-            assert test_image.xy_dim[0] == 3
-            assert test_image.xy_dim[1] == 3
-            assert test_image.xy_arcsec[0] == pytest.approx(0.3)
-            assert test_image.xy_arcsec[1] == pytest.approx(0.3)
+            assert test_image.pixel_scale == 0.1
+            assert test_image.xy_dim == (3, 3)
+            assert test_image.xy_cen_pixel == (1.0, 1.0)
+            assert test_image.xy_arcsec == pytest.approx((0.3, 0.3))
+
+        def test__init__input_image_4x3__all_attributes_correct(self):
+
+            test_image = image.Image(image=np.ones((4,3)), pixel_scale=0.1)
+
+            assert (test_image.data == np.ones((4, 3))).all()
+            assert test_image.pixel_scale == 0.1
+            assert test_image.xy_dim == (4, 3)
+            assert test_image.xy_cen_pixel == (1.5, 1.0)
+            assert test_image.xy_arcsec == pytest.approx((0.4, 0.3))
+
+        def test__via_fits__input_image_3x3__all_attributes_correct(self):
+
+            test_image = image.Image.via_fits('3x3_ones.fits', hdu=0, pixel_scale=0.1, path=test_data_dir)
+
+            assert (test_image.data == np.ones((3, 3))).all()
+            assert test_image.pixel_scale == 0.1
+            assert test_image.xy_dim == (3, 3)
+            assert test_image.xy_cen_pixel == (1.0, 1.0)
+            assert test_image.xy_arcsec == pytest.approx((0.3, 0.3))
+
+        def test__via_fits__input_image_4x3__all_attributes_correct(self):
+
+            test_image = image.Image.via_fits('4x3_ones.fits', hdu=0, pixel_scale=0.1, path=test_data_dir)
+
+            assert (test_image.data == np.ones((4, 3))).all()
+            assert test_image.pixel_scale == 0.1
+            assert test_image.xy_dim == (4, 3)
+            assert test_image.xy_cen_pixel == (1.5, 1.0)
+            assert test_image.xy_arcsec == pytest.approx((0.4, 0.3))
 
     class TestSetSky:
 
-        def test__via_edges__input_all_ones__sky_bg_level_1(self, test_image):
-            test_image.data = np.ones((3, 3))
-            test_image.xy_dim = (3, 3)
+        def test__via_edges__input_all_ones__sky_bg_level_1(self):
+            
+            test_image = image.Image(image=np.ones((3,3)), pixel_scale=0.1)
             test_image.set_sky_via_edges(no_edges=1)
 
             assert test_image.sky_background_level == 1.0
             assert test_image.sky_background_noise == 0.0
 
-        def test__via_edges__3x3_image_simple_gaussian__answer_ignores_central_pixel(self, test_image):
-            test_image.data = np.array([[1, 1, 1],
-                                           [1, 100, 1],
-                                           [1, 1, 1]])
-            test_image.xy_dim = (3, 3)
+        def test__via_edges__3x3_image_simple_gaussian__answer_ignores_central_pixel(self):
+        
+            image_data = np.array([[1, 1, 1],
+                                   [1, 100, 1],
+                                   [1, 1, 1]])
+            
+            test_image = image.Image(image=image_data, pixel_scale=0.1)
             test_image.set_sky_via_edges(no_edges=1)
 
             assert test_image.sky_background_level == 1.0
             assert test_image.sky_background_noise == 0.0
 
-        def test__via_edges__4x3_image_simple_gaussian__ignores_central_pixels(self, test_image):
-            test_image.data = np.array([[1, 1, 1],
-                                           [1, 100, 1],
-                                           [1, 100, 1],
-                                           [1, 1, 1]])
-            test_image.xy_dim = (4, 3)
-
+        def test__via_edges__4x3_image_simple_gaussian__ignores_central_pixels(self):
+            image_data = np.array([[1, 1, 1],
+                                   [1, 100, 1],
+                                   [1, 100, 1],
+                                   [1, 1, 1]])
+            
+            test_image = image.Image(image=image_data, pixel_scale=0.1)
             test_image.set_sky_via_edges(no_edges=1)
 
             assert test_image.sky_background_level == 1.0
             assert test_image.sky_background_noise == 0.0
 
-        def test__via_edges__4x4_image_simple_gaussian__ignores_central_pixels(self, test_image):
-            test_image.data = np.array([[1, 1, 1, 1],
-                                           [1, 100, 100, 1],
-                                           [1, 100, 100, 1],
-                                           [1, 1, 1, 1]])
-            test_image.xy_dim = (4, 4)
-
+        def test__via_edges__4x4_image_simple_gaussian__ignores_central_pixels(self):
+            image_data = np.array([[1, 1, 1, 1],
+                                   [1, 100, 100, 1],
+                                   [1, 100, 100, 1],
+                                   [1, 1, 1, 1]])
+            
+            test_image = image.Image(image=image_data, pixel_scale=0.1)
             test_image.set_sky_via_edges(no_edges=1)
 
             assert test_image.sky_background_level == 1.0
             assert test_image.sky_background_noise == 0.0
 
-        def test__via_edges__5x5_image_simple_gaussian_two_edges__ignores_central_pixel(self, test_image):
-            test_image.data = np.array([[1, 1, 1, 1, 1],
-                                           [1, 1, 1, 1, 1],
-                                           [1, 1, 100, 1, 1],
-                                           [1, 1, 1, 1, 1],
-                                           [1, 1, 1, 1, 1]])
-            test_image.xy_dim = [5, 5]
-
+        def test__via_edges__5x5_image_simple_gaussian_two_edges__ignores_central_pixel(self):
+            image_data = np.array([[1, 1, 1, 1, 1],
+                                   [1, 1, 1, 1, 1],
+                                   [1, 1, 100, 1, 1],
+                                   [1, 1, 1, 1, 1],
+                                   [1, 1, 1, 1, 1]])
+            
+            test_image = image.Image(image=image_data, pixel_scale=0.1)
             test_image.set_sky_via_edges(no_edges=2)
 
             assert test_image.sky_background_level == 1.0
             assert test_image.sky_background_noise == 0.0
 
-        def test__via_edges__6x5_image_two_edges__correct_values(self, test_image):
-            test_image.data = np.array([[0, 1, 2, 3, 4],
-                                           [5, 6, 7, 8, 9],
-                                           [10, 11, 100, 12, 13],
-                                           [14, 15, 100, 16, 17],
-                                           [18, 19, 20, 21, 22],
-                                           [23, 24, 25, 26, 27]])
-            test_image.xy_dim = [6, 5]
-
+        def test__via_edges__6x5_image_two_edges__correct_values(self):
+            image_data = np.array([[0, 1, 2, 3, 4],
+                                   [5, 6, 7, 8, 9],
+                                   [10, 11, 100, 12, 13],
+                                   [14, 15, 100, 16, 17],
+                                   [18, 19, 20, 21, 22],
+                                   [23, 24, 25, 26, 27]])
+            
+            test_image = image.Image(image=image_data, pixel_scale=0.1)
             test_image.set_sky_via_edges(no_edges=2)
 
             assert test_image.sky_background_level == np.mean(np.arange(28))
             assert test_image.sky_background_noise == np.std(np.arange(28))
 
-        def test__via_edges__7x7_image_three_edges__correct_values(self, test_image):
-            test_image.data = np.array([[0, 1, 2, 3, 4, 5, 6],
-                                           [7, 8, 9, 10, 11, 12, 13],
-                                           [14, 15, 16, 17, 18, 19, 20],
-                                           [21, 22, 23, 100, 24, 25, 26],
-                                           [27, 28, 29, 30, 31, 32, 33],
-                                           [34, 35, 36, 37, 38, 39, 40],
-                                           [41, 42, 43, 44, 45, 46, 47]])
-            test_image.xy_dim = [7, 7]
-
+        def test__via_edges__7x7_image_three_edges__correct_values(self):
+            image_data = np.array([[0, 1, 2, 3, 4, 5, 6],
+                                   [7, 8, 9, 10, 11, 12, 13],
+                                   [14, 15, 16, 17, 18, 19, 20],
+                                   [21, 22, 23, 100, 24, 25, 26],
+                                   [27, 28, 29, 30, 31, 32, 33],
+                                   [34, 35, 36, 37, 38, 39, 40],
+                                   [41, 42, 43, 44, 45, 46, 47]])
+            
+            test_image = image.Image(image=image_data, pixel_scale=0.1)
             test_image.set_sky_via_edges(no_edges=3)
 
             assert test_image.sky_background_level == np.mean(np.arange(48))
@@ -139,23 +169,67 @@ class TestPSF:
 
     class TestSetup:
 
-        def test__init__input_image_3x3__all_attributes_correct(self, test_psf):
+        def test__init__input_psf_3x3__all_attributes_correct(self):
+
+            test_psf = image.PSF(psf=np.ones((3,3)), pixel_scale=0.1)
 
             assert (test_psf.data == np.ones((3, 3))).all()
-            assert test_psf.xy_dim[0] == 3
-            assert test_psf.xy_dim[1] == 3
-            assert test_psf.xy_arcsec[0] == pytest.approx(0.3)
-            assert test_psf.xy_arcsec[1] == pytest.approx(0.3)
+            assert test_psf.pixel_scale == 0.1
+            assert test_psf.xy_dim == (3, 3)
+            assert test_psf.xy_cen_pixel == (1.0, 1.0)
+            assert test_psf.xy_arcsec == pytest.approx((0.3, 0.3))
+
+        def test__init__input_psf_4x3__all_attributes_correct(self):
+
+            test_psf = image.PSF(psf=np.ones((4,3)), pixel_scale=0.1)
+
+            assert (test_psf.data == np.ones((4, 3))).all()
+            assert test_psf.pixel_scale == 0.1
+            assert test_psf.xy_dim == (4, 3)
+            assert test_psf.xy_cen_pixel == (1.5, 1.0)
+            assert test_psf.xy_arcsec == pytest.approx((0.4, 0.3))
+
+        def test__via_fits__input_psf_3x3__all_attributes_correct(self):
+
+            test_psf = image.PSF.via_fits('3x3_ones.fits', hdu=0, pixel_scale=0.1, path=test_data_dir)
+
+            assert (test_psf.data == np.ones((3, 3))).all()
+            assert test_psf.pixel_scale == 0.1
+            assert test_psf.xy_dim == (3, 3)
+            assert test_psf.xy_cen_pixel == (1.0, 1.0)
+            assert test_psf.xy_arcsec == pytest.approx((0.3, 0.3))
+
+        def test__via_fits__input_psf_4x3__all_attributes_correct(self):
+
+            test_psf = image.PSF.via_fits('4x3_ones.fits', hdu=0, pixel_scale=0.1, path=test_data_dir)
+
+            assert (test_psf.data == np.ones((4, 3))).all()
+            assert test_psf.pixel_scale == 0.1
+            assert test_psf.xy_dim == (4, 3)
+            assert test_psf.xy_cen_pixel == (1.5, 1.0)
+            assert test_psf.xy_arcsec == pytest.approx((0.4, 0.3))
             
-        def test__input_image_3x3__setup_from_image(self, test_image):
+        def test__input_image_3x3__setup_from_image(self):
+
+            test_image = image.Image(image=np.ones((3,3)), pixel_scale=0.1)
 
             test_psf = test_image.load_psf(file_name='3x3_ones.fits', hdu=0, path=test_data_dir)
 
             assert (test_psf.data == np.ones((3, 3))).all()
-            assert test_psf.xy_dim[0] == 3
-            assert test_psf.xy_dim[1] == 3
-            assert test_psf.xy_arcsec[0] == pytest.approx(0.3)
-            assert test_psf.xy_arcsec[1] == pytest.approx(0.3)
+            assert test_psf.pixel_scale == 0.1
+            assert test_psf.xy_dim == (3, 3)
+            assert test_psf.xy_arcsec == pytest.approx((0.3, 0.3))
+
+        def test__input_image_4x3__setup_from_image(self):
+
+            test_image = image.Image(image=np.ones((3,3)), pixel_scale=0.1)
+
+            test_psf = test_image.load_psf(file_name='4x3_ones.fits', hdu=0, path=test_data_dir)
+
+            assert (test_psf.data == np.ones((4, 3))).all()
+            assert test_psf.pixel_scale == 0.1
+            assert test_psf.xy_dim == (4, 3)
+            assert test_psf.xy_arcsec == pytest.approx((0.4, 0.3))
 
 # noinspection PyClassHasNoInit,PyShadowingNames
 class TestMask:
@@ -298,33 +372,3 @@ class TestMask:
                                             [0, 0, 0, 0],
                                             [0, 0, 0, 0],
                                             [1, 0, 0, 1]])).all()
-
-
-# noinspection PyClassHasNoInit
-class TestLoadFits:
-    
-    def test__input_fits_3x3_ones__loads_data_as_type_numpy_array(self):
-
-        assert type(image.Image.via_fits('3x3_ones.fits', hdu=0, pixel_scale=1, path=test_data_dir).data) == np.ndarray
-
-    def test__input_fits_3x3_ones__loads_correct_data(self):
-
-        assert (image.Image.via_fits('3x3_ones.fits', hdu=0, pixel_scale=1, path=test_data_dir).data == np.ones((3, 3))).all()
-
-    def test__input_fits_4x3_ones__loads_correct_data(self):
-
-        assert (image.Image.via_fits('4x3_ones.fits', hdu=0, pixel_scale=1, path=test_data_dir).data == np.ones((4, 3))).all()
-
-    def test__input_files_3x3_ones__loads_correct_dimensions(self):
-
-        xy_dim = image.Image.via_fits('3x3_ones.fits', hdu=0, pixel_scale=1, path=test_data_dir).xy_dim
-
-        assert xy_dim[0] == 3
-        assert xy_dim[1] == 3
-
-    def test__input_files_4x3_ones__loads_correct_dimensions(self):
-
-        xy_dim = image.Image.via_fits('4x3_ones.fits', hdu=0, pixel_scale=1, path=test_data_dir).xy_dim
-
-        assert xy_dim[0] == 4
-        assert xy_dim[1] == 3
