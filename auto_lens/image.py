8d7969861450e03c0916d818c1bdfcf1a1d43e6d
Richard <richardhayes777@gmail.com>
2017-12-14 16:14:52 +0000

    extracted convenience methods, more explicit dimensions

--- auto_lens/image.py
+++ auto_lens/image.py
@@ -17,17 +17,54 @@ def numpy_array_from_fits(file_path, hdu):
     return np.array(hdu_list[hdu].data)
 
 
+def pixel_dimensions_to_arc_seconds(pixel_dimensions, pixel_scale):
+    return tuple(map(lambda d: d * pixel_scale, pixel_dimensions))
+
+
+def arc_second_dimensions_to_pixel(arc_second_dimensions, pixel_scale):
+    return tuple(map(lambda d: d / pixel_scale, arc_second_dimensions))
+
+
+def central_pixel(pixel_dimensions):
+    return tuple(map(lambda d: (float(d + 1) / 2) - 1, pixel_dimensions))
+
+
 def keep_type(func):
+    """
+    
+    Parameters
+    ----------
+    func: function(T:ndarray) -> ndarray 
+        A function that takes a child of ndarray and returns an ndarray
+    Returns
+    -------
+    func: function(T:ndarray) -> ndarray 
+        A function that takes a child of ndarray and returns the same class
+    """
+
     @wraps(func)
     def wrapper(array, *args, **kwargs):
+        """
+        
+        Parameters
+        ----------
+        array
+        args
+        kwargs
+
+        Returns
+        -------
+
+        """
         new_array = func(array, *args, **kwargs)
         return new_array.view(array.__class__)
-    return func
+
+    return wrapper
 
 
 # TODO: It seemed to meet that many of these functions are best made general. They really can apply to any array.
 @keep_type
-def trim_array(array, new_dimensions):
+def trim_array(array, pixel_dimensions):
     """ Trim the data array to a new size around its central pixel.
     NOTE: The centre of the array cannot be shifted. Therefore, even arrays are trimmed to even arrays
     (e.g. 8x8 -> 4x4) and odd to odd (e.g. 5x5 -> 3x3).
@@ -35,23 +72,23 @@ def trim_array(array, new_dimensions):
     ----------
     array: ndarray (or Noise or PSF)
         The image array
-    new_dimensions : (int, int)
-        The new x and y dimensions of the trimmed data-array
+    pixel_dimensions : (int, int)
+        The new pixel dimensions of the trimmed data-array
     """
     shape = array.shape
-    if new_dimensions[0] > shape[0]:
+    if pixel_dimensions[0] > shape[0]:
         raise ValueError('image.Data.trim_data - You have specified a new x_size bigger than the data array')
-    elif new_dimensions[1] > shape[1]:
+    elif pixel_dimensions[1] > shape[1]:
         raise ValueError('image.Data.trim_data - You have specified a new y_size bigger than the data array')
-    x_trim = int((shape[0] - new_dimensions[0]) / 2)
-    y_trim = int((shape[1] - new_dimensions[1]) / 2)
+    x_trim = int((shape[0] - pixel_dimensions[0]) / 2)
+    y_trim = int((shape[1] - pixel_dimensions[1]) / 2)
     array = array[x_trim:shape[0] - x_trim, y_trim:shape[1] - y_trim]
-    if shape[0] != new_dimensions[0]:
+    if shape[0] != pixel_dimensions[0]:
         logger.debug(
             'image.data.trim_data - Your specified x_size was odd (even) when the image x dimension is even (odd)')
         logger.debug(
             'The method has automatically used x_size+1 to ensure the image is not miscentred by a half-pixel.')
-    elif shape[1] != new_dimensions[1]:
+    elif shape[1] != pixel_dimensions[1]:
         logger.debug(
             'image.data.trim_data - Your specified y_size was odd (even) when the image y dimension is even (odd)')
         logger.debug(
@@ -60,7 +97,7 @@ def trim_array(array, new_dimensions):
 
 
 @keep_type
-def pad_array(array, new_dimensions):
+def pad_array(array, pixel_dimensions):
     """ Pad the data array with zeros around its central pixel.
     NOTE: The centre of the array cannot be shifted. Therefore, even arrays are padded to even arrays
     (e.g. 8x8 -> 4x4) and odd to odd (e.g. 5x5 -> 3x3).
@@ -68,16 +105,16 @@ def pad_array(array, new_dimensions):
     ----------
     array: ndarray (or Noise or PSF)
         The image array
-    new_dimensions : (int, int)
-        The new x dimension of the data-array
+    pixel_dimensions : (int, int)
+        The new pixel dimension of the data-array
     """
     shape = array.shape
-    if new_dimensions[0] < shape[0]:
+    if pixel_dimensions[0] < shape[0]:
         raise ValueError('image.Data.pad_data - You have specified a new x_size smaller than the data array')
-    elif new_dimensions[1] < shape[1]:
+    elif pixel_dimensions[1] < shape[1]:
         raise ValueError('image.Data.pad_data - You have specified a new y_size smaller than the data array')
-    x_pad = int((new_dimensions[0] - shape[0] + 1) / 2)
-    y_pad = int((new_dimensions[1] - shape[1] + 1) / 2)
+    x_pad = int((pixel_dimensions[0] - shape[0] + 1) / 2)
+    y_pad = int((pixel_dimensions[1] - shape[1] + 1) / 2)
     return np.pad(array, ((x_pad, y_pad), (x_pad, y_pad)), 'constant')
 
 
@@ -158,59 +195,43 @@ class Image(np.ndarray):
             self.sky_background_level = getattr(obj, 'sky_background_level', None)
             self.sky_background_noise = getattr(obj, 'sky_background_noise', None)
 
-    def padded(self, new_dimensions):
+    def padded(self, pixel_dimensions):
         """
 
         Parameters
         ----------
-        new_dimensions: (int, int)
+        pixel_dimensions: (int, int)
             The new dimensions of this image in pixels
 
         Returns
         -------
             A new Image that has been padded
         """
-        return Image(pad_array(self, new_dimensions), self.pixel_scale, self.sky_background_level,
+        return Image(pad_array(self, pixel_dimensions), self.pixel_scale, self.sky_background_level,
                      self.sky_background_noise)
 
-    def trimmed(self, new_dimensions):
+    def trimmed(self, pixel_dimensions):
         """
 
         Parameters
         ----------
-        new_dimensions: (int, int)
+        pixel_dimensions: (int, int)
             The new dimensions of this image in pixel
             
         Returns
         -------
             A new Image that has been padded
         """
-        return Image(trim_array(self, new_dimensions), self.pixel_scale, self.sky_background_level,
+        return Image(trim_array(self, pixel_dimensions), self.pixel_scale, self.sky_background_level,
                      self.sky_background_noise)
 
     @property
     def central_pixels(self):
-        return self.central_pixel(self.shape)
+        return central_pixel(self.shape)
 
     @property
     def shape_arc_seconds(self):
-        return self.dimensions_to_arc_seconds(self.shape, self.pixel_scale)
-
-    @staticmethod
-    def central_pixel(dimensions):
-        return tuple(map(lambda l: (float(l + 1) / 2) - 1, dimensions))
-
-    @staticmethod
-    def dimensions_to_arc_seconds(dimensions, pixel_scale):
-        return tuple(map(lambda l: l * pixel_scale, dimensions))
-
-    @property
-    def x_dimension(self):
-        return self.shape[0]
-
-    @property
-    def y_dimension(self):
-        return self.shape[1]
+        return pixel_dimensions_to_arc_seconds(self.shape, self.pixel_scale)
 
     @property
     def x_cen_pixel(self):
@@ -260,13 +281,14 @@ class Image(np.ndarray):
         edges = []
 
         for edge_no in range(no_edges):
-            top_edge = self[edge_no, edge_no:self.y_dimension - edge_no]
-            bottom_edge = self[self.x_dimension - 1 - edge_no, edge_no:self.y_dimension - edge_no]
-            left_edge = self[edge_no + 1:self.x_dimension - 1 - edge_no, edge_no]
-            right_edge = self[edge_no + 1:self.x_dimension - 1 - edge_no, self.y_dimension - 1 - edge_no]
+            top_edge = self[edge_no, edge_no:self.shape[1] - edge_no]
+            bottom_edge = self[self.shape[0] - 1 - edge_no, edge_no:self.shape[1] - edge_no]
+            left_edge = self[edge_no + 1:self.shape[0] - 1 - edge_no, edge_no]
+            right_edge = self[edge_no + 1:self.shape[0] - 1 - edge_no, self.shape[1] - 1 - edge_no]
 
             edges = np.concatenate((edges, top_edge, bottom_edge, right_edge, left_edge))
 
+        # noinspection PyAttributeOutsideInit
         self.sky_background_level, self.sky_background_noise = norm.fit(edges)
 
     def circle_mask(self, radius_arc):
@@ -282,7 +304,7 @@ class Image(np.ndarray):
         -------
         A circular mask for this image
         """
-        return Mask.circular(dimensions=self.shape_arc_seconds, pixel_scale=self.pixel_scale, radius=radius_arc)
+        return Mask.circular(arc_second_dimensions=self.shape_arc_seconds, pixel_scale=self.pixel_scale, radius=radius_arc)
 
     def annulus_mask(self, inner_radius_arc, outer_radius_arc):
         """
@@ -299,7 +321,7 @@ class Image(np.ndarray):
         -------
         An annulus mask for this image
         """
-        return Mask.annular(dimensions=self.shape_arc_seconds, pixel_scale=self.pixel_scale,
+        return Mask.annular(arc_second_dimensions=self.shape_arc_seconds, pixel_scale=self.pixel_scale,
                             outer_radius=outer_radius_arc,
                             inner_radius=inner_radius_arc)
 
@@ -356,14 +378,10 @@ class Noise(Array):
 class Mask(object):
     """Abstract Class for preparing and storing the image mask used for the AutoLens analysis"""
 
-    @staticmethod
-    def central_pixel(dimensions, pixel_scale):
-        return tuple(map(lambda d: (float(d / pixel_scale + 1) / 2) - 1, dimensions))
-
     # TODO: By having this function take a function that decides whether a pixel is part of the mask we can avoid
     # TODO: repeating loops
     @classmethod
-    def mask(cls, dimensions, pixel_scale, function, centre):
+    def mask(cls, arc_second_dimensions, pixel_scale, function, centre):
         """
 
         Parameters
@@ -374,34 +392,36 @@ class Mask(object):
             A function that determines what the value of a mask should be at particular coordinates
         pixel_scale: float
             The size of a pixel in arc seconds
-        dimensions: (float, float)
+        arc_second_dimensions: (float, float)
             The spatial dimensions of the mask in arc seconds
 
         Returns
         -------
             An empty array
         """
-        array = np.zeros((int(dimensions[0] / pixel_scale), int(dimensions[1] / pixel_scale)))
+        pixel_dimensions = arc_second_dimensions_to_pixel(arc_second_dimensions, pixel_scale)
+        print(pixel_dimensions)
+        array = np.zeros((int(pixel_dimensions[0]), int(pixel_dimensions[1])))
 
-        central_pixel = Mask.central_pixel(dimensions, pixel_scale)
-        for i in range(int(dimensions[0] / pixel_scale)):
-            for j in range(int(dimensions[1] / pixel_scale)):
+        central_pixel_coords = central_pixel(pixel_dimensions)
+        for i in range(int(pixel_dimensions[0])):
+            for j in range(int(pixel_dimensions[1])):
                 # Convert from pixel coordinates to image coordinates
-                x_pix = pixel_scale * (i - central_pixel[0]) - centre[0]
-                y_pix = pixel_scale * (j - central_pixel[1]) - centre[1]
+                x_pix = pixel_scale * (i - central_pixel_coords[0]) - centre[0]
+                y_pix = pixel_scale * (j - central_pixel_coords[1]) - centre[1]
 
                 array[i, j] = function(x_pix, y_pix)
         return np.ma.make_mask(array)
 
     @classmethod
-    def circular(cls, dimensions, pixel_scale, radius, centre=(0., 0.)):
+    def circular(cls, arc_second_dimensions, pixel_scale, radius, centre=(0., 0.)):
         """
 
         Parameters
         ----------
         centre: (float, float)
             The centre in image coordinates in arc seconds
-        dimensions : (int, int)
+        arc_second_dimensions : (int, int)
             The dimensions of the image (x, y) in arc seconds
         pixel_scale : float
             The scale size of a pixel (x, y) in arc seconds
@@ -413,17 +433,17 @@ class Mask(object):
             radius_arc = np.sqrt(x_pix ** 2 + y_pix ** 2)
             return radius_arc <= radius
 
-        return Mask.mask(dimensions, pixel_scale, is_within_radius, centre)
+        return Mask.mask(arc_second_dimensions, pixel_scale, is_within_radius, centre)
 
     @classmethod
-    def annular(cls, dimensions, pixel_scale, inner_radius, outer_radius, centre=(0., 0.)):
+    def annular(cls, arc_second_dimensions, pixel_scale, inner_radius, outer_radius, centre=(0., 0.)):
         """
 
         Parameters
         ----------
         centre: (float, float)
             The centre in arc seconds
-        dimensions : (int, int)
+        arc_second_dimensions : (int, int)
             The dimensions of the image in arcs seconds
         pixel_scale : float
             The scale size of a pixel (x, y) in arc seconds
@@ -437,4 +457,4 @@ class Mask(object):
             radius_arc = np.sqrt(x_pix ** 2 + y_pix ** 2)
             return outer_radius >= radius_arc >= inner_radius
 
-        return Mask.mask(dimensions, pixel_scale, is_within_radii, centre)
+        return Mask.mask(arc_second_dimensions, pixel_scale, is_within_radii, centre)
