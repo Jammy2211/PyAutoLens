cf3b4f664cde1e6c5bf6c1de9d512a88e23b725f
Richard <richardhayes777@gmail.com>
2017-12-15 10:33:40 +0000

    argument for

--- auto_lens/prior.py
+++ auto_lens/prior.py
@@ -1,5 +1,17 @@
-class UniformPrior(object):
+class Prior(object):
+    def __init__(self, name):
+        self.name = name
+
+    def argument_for(self, unit):
+        return self.name, self.value_for(unit)
+
+    def value_for(self, unit):
+        raise AssertionError("Prior.value_for should be overridden")
+
+
+class UniformPrior(Prior):
     def __init__(self, name, lower_limit=0., upper_limit=1.):
+        super(UniformPrior, self).__init__(name)
         self.lower_limit = lower_limit
         self.upper_limit = upper_limit
 
--- auto_lens/test_prior.py
+++ auto_lens/test_prior.py
@@ -3,15 +3,22 @@ import prior
 
 class TestUniformPrior(object):
     def test__simple_assumptions(self):
-        uniform_prior = prior.UniformPrior("Test", lower_limit=0., upper_limit=1.)
+        uniform_prior = prior.UniformPrior("test", lower_limit=0., upper_limit=1.)
 
         assert uniform_prior.value_for(0.) == 0.
         assert uniform_prior.value_for(1.) == 1.
         assert uniform_prior.value_for(0.5) == 0.5
 
     def test__non_zero_lower_limit(self):
-        uniform_prior = prior.UniformPrior("Test", lower_limit=0.5, upper_limit=1.)
+        uniform_prior = prior.UniformPrior("test", lower_limit=0.5, upper_limit=1.)
 
         assert uniform_prior.value_for(0.) == 0.5
         assert uniform_prior.value_for(1.) == 1.
         assert uniform_prior.value_for(0.5) == 0.75
+
+
+class TestArguments(object):
+    def test__argument(self):
+        uniform_prior = prior.UniformPrior("test", lower_limit=0., upper_limit=1.)
+
+        assert uniform_prior.argument_for(0.) == ("test", 0)
