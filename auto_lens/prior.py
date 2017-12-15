40a0d4f4fb64aff13f590d31496afa9d55ce221b
Richard <richardhayes777@gmail.com>
2017-12-15 10:50:02 +0000

    override and equals

--- auto_lens/prior.py
+++ auto_lens/prior.py
@@ -8,6 +8,15 @@ class Prior(object):
     def value_for(self, unit):
         raise AssertionError("Prior.value_for should be overridden")
 
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __ne__(self, other):
+        return self.name != other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
 
 class UniformPrior(Prior):
     def __init__(self, name, lower_limit=0., upper_limit=1.):
@@ -17,3 +26,17 @@ class UniformPrior(Prior):
 
     def value_for(self, unit):
         return self.lower_limit + unit * (self.upper_limit - self.lower_limit)
+
+
+class PriorCollection(list):
+    def __init__(self, *priors):
+        super(PriorCollection, self).__init__(priors)
+
+    def arguments_for_vector(self, vector):
+        return dict(map(lambda prior, unit: prior.argument_for(unit), self, vector))
+
+    def append(self, p_object):
+        if p_object in self:
+            self[self.index(p_object)] = p_object
+        else:
+            super(PriorCollection, self).append(p_object)
--- auto_lens/test_prior.py
+++ auto_lens/test_prior.py
@@ -1,25 +1,50 @@
 import prior
+import pytest
+
+
+@pytest.fixture(name='uniform_simple')
+def make_uniform_simple():
+    return prior.UniformPrior("one", lower_limit=0., upper_limit=1.)
+
+
+@pytest.fixture(name='uniform_half')
+def make_uniform_half():
+    return prior.UniformPrior("two", lower_limit=0.5, upper_limit=1.)
+
+
+@pytest.fixture(name='collection')
+def make_collection(uniform_simple, uniform_half):
+    return prior.PriorCollection(uniform_simple, uniform_half)
 
 
 class TestUniformPrior(object):
-    def test__simple_assumptions(self):
-        uniform_prior = prior.UniformPrior("test", lower_limit=0., upper_limit=1.)
+    def test__simple_assumptions(self, uniform_simple):
+        assert uniform_simple.value_for(0.) == 0.
+        assert uniform_simple.value_for(1.) == 1.
+        assert uniform_simple.value_for(0.5) == 0.5
 
-        assert uniform_prior.value_for(0.) == 0.
-        assert uniform_prior.value_for(1.) == 1.
-        assert uniform_prior.value_for(0.5) == 0.5
-
-    def test__non_zero_lower_limit(self):
-        uniform_prior = prior.UniformPrior("test", lower_limit=0.5, upper_limit=1.)
-
-        assert uniform_prior.value_for(0.) == 0.5
-        assert uniform_prior.value_for(1.) == 1.
-        assert uniform_prior.value_for(0.5) == 0.75
+    def test__non_zero_lower_limit(self, uniform_half):
+        assert uniform_half.value_for(0.) == 0.5
+        assert uniform_half.value_for(1.) == 1.
+        assert uniform_half.value_for(0.5) == 0.75
 
 
 class TestArguments(object):
-    def test__argument(self):
-        uniform_prior = prior.UniformPrior("test", lower_limit=0., upper_limit=1.)
+    def test__argument(self, uniform_simple):
+        assert uniform_simple.argument_for(0.) == ("one", 0)
+        assert uniform_simple.argument_for(0.5) == ("one", 0.5)
 
-        assert uniform_prior.argument_for(0.) == ("test", 0)
-        assert uniform_prior.argument_for(0.5) == ("test", 0.5)
+    def test__arguments(self, collection):
+        assert collection.arguments_for_vector([0., 0.]) == {"one": 0., "two": 0.5}
+        assert collection.arguments_for_vector([1., 0.]) == {"one": 1., "two": 0.5}
+
+    def test__equals(self, uniform_simple):
+        assert uniform_simple != prior.Prior("two")
+        assert uniform_simple == prior.Prior("one")
+
+    def test__override(self, collection):
+        collection.append(prior.UniformPrior("one", lower_limit=1., upper_limit=2.))
+
+        assert len(collection) == 2
+        collection[0].lower_limit = 1.
+        collection[0].name = "one"
