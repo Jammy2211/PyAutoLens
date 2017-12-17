db1313fd4b3f0f5d2c8180248837750295a4418e
Richard <richardhayes777@gmail.com>
2017-12-17 16:43:29 +0000

    simple example taken from tutorial

--- auto_lens/profiling.py
+++ auto_lens/profiling.py
@@ -1,6 +1,22 @@
 import cProfile
 from profile import mass_profile
 import pytest
+from numba import cfunc, carray, jit
+from numba.types import intc, intp, float64, voidptr
+from numba.types import CPointer
+import numpy as np
+from scipy import ndimage as ndi
+from scipy import LowLevelCallable
+from scipy import integrate
+
+invexp = lambda x: np.exp(-x)
+integrate.quad(invexp, 0, np.inf)
+
+image = np.random.random((2048, 2048))
+
+footprint = np.array([[0, 1, 0],
+                      [1, 1, 1],
+                      [0, 1, 0]], dtype=bool)
 
 
 def test_deflection_angles():
@@ -17,4 +33,27 @@ def test_deflection_angles():
             sersic.deflection_angles_at_coordinates(coordinates=(i * 0.01625, j * 0.01625))
 
 
-cProfile.run('test_deflection_angles()')
+# cProfile.run('test_deflection_angles()')
+
+
+@jit
+def other_func():
+    return 1
+
+
+@cfunc(intc(CPointer(float64), intp, CPointer(float64), voidptr))
+def nbmin(values_ptr, len_values, result, data):
+    values = carray(values_ptr, (len_values,), dtype=float64)
+    result[0] = np.inf
+    for v in values:
+
+        if v < result[0]:
+            result[0] = v
+            other_func()
+
+    return 1
+
+
+# LowLevelCallable(nbmin.ctypes)()
+
+print(ndi.generic_filter(image, LowLevelCallable(nbmin.ctypes), footprint=footprint))
