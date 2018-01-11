804c782068f6af3dd80eed027486a01c5fbbdcc4
Richard <richardhayes777@gmail.com>
2018-01-11 15:19:27 +0000

    extracting frames

--- auto_lens/frame_convolution.py
+++ auto_lens/frame_convolution.py
@@ -12,12 +12,27 @@ def number_array_for_mask(mask):
     return array
 
 
-def make_frame_dict(number_array, kernel_shape):
-    frame_dict = {}
+def make_frame_array(number_array, kernel_shape):
+    frame_array = []
     for x in range(number_array.shape[0]):
         for y in range(number_array.shape[1]):
-            number = number_array[x][y]
-            if number > -1:
-                frame_dict[number] = []
+            if number_array[x][y] > -1:
+                frame_array.append(frame_at_coords(number_array, (x, y), kernel_shape))
 
-    return frame_dict
+    return frame_array
+
+
+def frame_at_coords(number_array, coords, kernel_shape):
+    half_x = int(kernel_shape[0] / 2)
+    half_y = int(kernel_shape[1] / 2)
+
+    frame = -1 * np.ones(kernel_shape)
+
+    for i in range(kernel_shape[0]):
+        for j in range(kernel_shape[1]):
+            x = coords[0] - half_x + i
+            y = coords[1] - half_y + j
+            if 0 <= x < number_array.shape[0] and 0 <= y < number_array.shape[1]:
+                frame[i, j] = number_array[x, y]
+
+    return frame
--- auto_lens/test_frame_convolution.py
+++ auto_lens/test_frame_convolution.py
@@ -1,40 +1,69 @@
 import numpy as np
 import frame_convolution
+import pytest
+
+
+@pytest.fixture(name="simple_number_array")
+def make_simple_number_array():
+    return np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+
+
+@pytest.fixture(name="cross_mask")
+def make_cross_mask():
+    mask = np.ones((3, 3))
+
+    mask[0, 0] = 0
+    mask[0, 2] = 0
+    mask[2, 2] = 0
+    mask[2, 0] = 0
+
+    return mask
+
+
+@pytest.fixture(name="cross_number_array")
+def make_cross_number_array():
+    return np.array([[-1, 0, -1], [1, 2, 3], [-1, 4, -1]])
 
 
 class TestNumbering(object):
-    def test_simple_numbering(self):
+    def test_simple_numbering(self, simple_number_array):
         shape = (3, 3)
         number_array = frame_convolution.number_array_for_mask(np.ones(shape))
 
         assert number_array.shape == shape
-        assert (number_array == np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])).all()
+        # noinspection PyUnresolvedReferences
+        assert (number_array == simple_number_array).all()
 
-    def test_simple_mask(self):
-        shape = (3, 3)
-        mask = np.ones(shape)
-
-        mask[0, 0] = 0
-        mask[0, 2] = 0
-        mask[2, 2] = 0
-        mask[2, 0] = 0
-
-        number_array = frame_convolution.number_array_for_mask(mask)
+    def test_simple_mask(self, cross_mask):
+        number_array = frame_convolution.number_array_for_mask(cross_mask)
 
         assert (number_array == np.array([[-1, 0, -1], [1, 2, 3], [-1, 4, -1]])).all()
 
 
 class TestFrameExtraction(object):
-    def test_simple_square(self):
-        number_array = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+    def test_frame_at_coords(self, simple_number_array):
+        kernel_shape = (3, 3)
 
-        frame_dict = frame_convolution.make_frame_dict(number_array, kernel_shape=(3, 3))
+        # noinspection PyUnresolvedReferences
+        assert (simple_number_array == frame_convolution.frame_at_coords(simple_number_array, coords=(1, 1),
+                                                                         kernel_shape=kernel_shape)).all()
 
-        assert 9 == len(frame_dict)
+        corner_array = np.array([[-1, -1, -1], [-1, 0, 1], [-1, 3, 4]])
 
-    def test_masked_square(self):
-        number_array = np.array([[-1, 0, -1], [1, 2, 3], [-1, 4, -1]])
+        corner_frame = frame_convolution.frame_at_coords(simple_number_array, coords=(0, 0), kernel_shape=kernel_shape)
 
-        frame_dict = frame_convolution.make_frame_dict(number_array, kernel_shape=(3, 3))
+        assert (corner_array == corner_frame).all()
 
-        assert 5 == len(frame_dict)
+    def test_simple_square(self, simple_number_array):
+        frame_array = frame_convolution.make_frame_array(simple_number_array, kernel_shape=(3, 3))
+
+        assert 9 == len(frame_array)
+
+        assert frame_array[4].shape == simple_number_array.shape
+        # noinspection PyUnresolvedReferences
+        assert (frame_array[4] == simple_number_array).all()
+
+    def test_masked_square(self, cross_number_array):
+        frame_array = frame_convolution.make_frame_array(cross_number_array, kernel_shape=(3, 3))
+
+        assert 5 == len(frame_array)
