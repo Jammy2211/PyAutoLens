f01bcb1a23e288ddd868e9191d0fde88645174d3
Richard <richardhayes777@gmail.com>
2018-01-11 14:36:25 +0000

    counting with mask

--- auto_lens/frame_convolution.py
+++ auto_lens/frame_convolution.py
@@ -6,6 +6,7 @@ def number_array_for_mask(mask):
     n = 0
     for x in range(mask.shape[0]):
         for y in range(mask.shape[1]):
-            array[x, y] = n
-            n += 1
+            if mask[x, y] == 1:
+                array[x, y] = n
+                n += 1
     return array
--- auto_lens/test_frame_convolution.py
+++ auto_lens/test_frame_convolution.py
@@ -9,3 +9,16 @@ class TestFrameConvolution(object):
 
         assert number_array.shape == shape
         assert (number_array == np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])).all()
+
+    def test_simple_mask(self):
+        shape = (3, 3)
+        mask = np.ones(shape)
+
+        mask[0, 0] = 0
+        mask[0, 2] = 0
+        mask[2, 2] = 0
+        mask[2, 0] = 0
+
+        number_array = frame_convolution.number_array_for_mask(mask)
+
+        assert (number_array == np.array([[-1, 0, -1], [1, 2, 3], [-1, 4, -1]])).all()
