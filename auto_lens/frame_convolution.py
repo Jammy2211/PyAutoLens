b77cafbf47cdecabab65b4c40bba03518c7415be
Richard <richardhayes777@gmail.com>
2018-01-11 14:32:31 +0000

    simple counting

---
+++ auto_lens/frame_convolution.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def number_array_for_mask(mask):
+    array = -1 * np.ones(mask.shape)
+    n = 0
+    for x in range(mask.shape[0]):
+        for y in range(mask.shape[1]):
+            array[x, y] = n
+            n += 1
+    return array
