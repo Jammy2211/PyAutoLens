d1dea91bdfccc08e305f16ea39a1aa9c9da05483
Richard <richardhayes777@gmail.com>
2018-01-11 14:45:49 +0000

    scanning non-negative numbered pixels

--- auto_lens/frame_convolution.py
+++ auto_lens/frame_convolution.py
@@ -10,3 +10,14 @@ def number_array_for_mask(mask):
                 array[x, y] = n
                 n += 1
     return array
+
+
+def make_frame_dict(number_array, kernel_shape):
+    frame_dict = {}
+    for x in range(number_array.shape[0]):
+        for y in range(number_array.shape[1]):
+            number = number_array[x][y]
+            if number > -1:
+                frame_dict[number] = []
+
+    return frame_dict
--- auto_lens/test_frame_convolution.py
+++ auto_lens/test_frame_convolution.py
@@ -2,7 +2,7 @@ import numpy as np
 import frame_convolution
 
 
-class TestFrameConvolution(object):
+class TestNumbering(object):
     def test_simple_numbering(self):
         shape = (3, 3)
         number_array = frame_convolution.number_array_for_mask(np.ones(shape))
@@ -22,3 +22,19 @@ class TestFrameConvolution(object):
         number_array = frame_convolution.number_array_for_mask(mask)
 
         assert (number_array == np.array([[-1, 0, -1], [1, 2, 3], [-1, 4, -1]])).all()
+
+
+class TestFrameExtraction(object):
+    def test_simple_square(self):
+        number_array = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
+
+        frame_dict = frame_convolution.make_frame_dict(number_array, kernel_shape=(3, 3))
+
+        assert 9 == len(frame_dict)
+
+    def test_masked_square(self):
+        number_array = np.array([[-1, 0, -1], [1, 2, 3], [-1, 4, -1]])
+
+        frame_dict = frame_convolution.make_frame_dict(number_array, kernel_shape=(3, 3))
+
+        assert 5 == len(frame_dict)
