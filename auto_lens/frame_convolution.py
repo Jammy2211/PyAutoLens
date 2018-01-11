c3d6e68cd2d5a295cd4fc566ae8033ae56f536f4
Richard <richardhayes777@gmail.com>
2018-01-11 15:34:05 +0000

    docs

--- auto_lens/frame_convolution.py
+++ auto_lens/frame_convolution.py
@@ -2,6 +2,18 @@ import numpy as np
 
 
 def number_array_for_mask(mask):
+    """
+    Creates an array where points inside the mask are numbered
+    Parameters
+    ----------
+    mask: ndarray
+        A mask where 0 eliminates data
+
+    Returns
+    -------
+    number_array: ndarray
+        An array where non-masked elements are numbered 0, 1, 2,...N with masked elements designated -1
+    """
     array = -1 * np.ones(mask.shape)
     n = 0
     for x in range(mask.shape[0]):
@@ -13,6 +25,18 @@ def number_array_for_mask(mask):
 
 
 def make_frame_array(number_array, kernel_shape):
+    """
+    Parameters
+    ----------
+    number_array: ndarray
+        An array in which non-masked elements have been numbered 0, 1, 2,...N
+    kernel_shape: (int, int)
+        The shape of the kernel for which frames will be used
+    Returns
+    -------
+    frame_array: [ndarray]
+        A list of frames where the position of a frame corresponds to the number at the centre of that frame
+    """
     frame_array = []
     for x in range(number_array.shape[0]):
         for y in range(number_array.shape[1]):
@@ -23,6 +47,21 @@ def make_frame_array(number_array, kernel_shape):
 
 
 def frame_at_coords(number_array, coords, kernel_shape):
+    """
+    Parameters
+    ----------
+    number_array: ndarray
+        An array in which non-masked elements have been numbered 0, 1, 2,...N
+    coords: (int, int)
+        The coordinates of number_array on which the frame should be centred
+    kernel_shape: (int, int)
+        The shape of the kernel for which this frame will be used
+    Returns
+    -------
+    frame: ndarray
+        A subset of number_array of shape kernel_shape where elements with coordinates outside of frame_array have
+        value -1
+    """
     half_x = int(kernel_shape[0] / 2)
     half_y = int(kernel_shape[1] / 2)
 
