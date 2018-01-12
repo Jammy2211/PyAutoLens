bb504f9273bf9fa4ea918a78fcccd778ed7a22fb
Richard <richardhayes777@gmail.com>
2018-01-12 09:37:12 +0000

    created frame maker class to simplify API

--- auto_lens/frame_convolution.py
+++ auto_lens/frame_convolution.py
@@ -3,22 +3,26 @@ import numpy as np
 
 class FrameMaker(object):
     def __init__(self, mask):
+        """
+        Class to create number array and frames used in 1D convolution
+        Parameters
+        ----------
+        mask: ndarray
+                A mask where 0 eliminates data
+        """
         self.mask = mask
 
     @property
     def number_array(self):
         """
-            Creates an array where points inside the mask are numbered
-            Parameters
-            ----------
-            mask: ndarray
-                A mask where 0 eliminates data
+        Creates an array where points inside the mask are numbered
+        Parameters
 
-            Returns
-            -------
-            number_array: ndarray
-                An array where non-masked elements are numbered 0, 1, 2,...N with masked elements designated -1
-            """
+        Returns
+        -------
+        number_array: ndarray
+            An array where non-masked elements are numbered 0, 1, 2,...N with masked elements designated -1
+        """
         array = -1 * np.ones(self.mask.shape)
         n = 0
         for x in range(self.mask.shape[0]):
@@ -28,58 +32,53 @@ class FrameMaker(object):
                     n += 1
         return array
 
+    def make_frame_array(self, kernel_shape):
+        """
+        Parameters
+        ----------
+            An array in which non-masked elements have been numbered 0, 1, 2,...N
+        kernel_shape: (int, int)
+            The shape of the kernel for which frames will be used
+        Returns
+        -------
+        frame_array: [ndarray]
+            A list of frames where the position of a frame corresponds to the number at the centre of that frame
+        """
+        frame_array = []
+        for x in range(self.number_array.shape[0]):
+            for y in range(self.number_array.shape[1]):
+                if self.number_array[x][y] > -1:
+                    frame_array.append(self.frame_at_coords((x, y), kernel_shape))
 
-def make_frame_array(number_array, kernel_shape):
-    """
-    Parameters
-    ----------
-    number_array: ndarray
-        An array in which non-masked elements have been numbered 0, 1, 2,...N
-    kernel_shape: (int, int)
-        The shape of the kernel for which frames will be used
-    Returns
-    -------
-    frame_array: [ndarray]
-        A list of frames where the position of a frame corresponds to the number at the centre of that frame
-    """
-    frame_array = []
-    for x in range(number_array.shape[0]):
-        for y in range(number_array.shape[1]):
-            if number_array[x][y] > -1:
-                frame_array.append(frame_at_coords(number_array, (x, y), kernel_shape))
+        return frame_array
 
-    return frame_array
+    def frame_at_coords(self, coords, kernel_shape):
+        """
+        Parameters
+        ----------
+        coords: (int, int)
+            The coordinates of number_array on which the frame should be centred
+        kernel_shape: (int, int)
+            The shape of the kernel for which this frame will be used
+        Returns
+        -------
+        frame: ndarray
+            A subset of number_array of shape kernel_shape where elements with coordinates outside of frame_array have
+            value -1
+        """
+        half_x = int(kernel_shape[0] / 2)
+        half_y = int(kernel_shape[1] / 2)
 
+        frame = -1 * np.ones(kernel_shape)
 
-def frame_at_coords(number_array, coords, kernel_shape):
-    """
-    Parameters
-    ----------
-    number_array: ndarray
-        An array in which non-masked elements have been numbered 0, 1, 2,...N
-    coords: (int, int)
-        The coordinates of number_array on which the frame should be centred
-    kernel_shape: (int, int)
-        The shape of the kernel for which this frame will be used
-    Returns
-    -------
-    frame: ndarray
-        A subset of number_array of shape kernel_shape where elements with coordinates outside of frame_array have
-        value -1
-    """
-    half_x = int(kernel_shape[0] / 2)
-    half_y = int(kernel_shape[1] / 2)
+        for i in range(kernel_shape[0]):
+            for j in range(kernel_shape[1]):
+                x = coords[0] - half_x + i
+                y = coords[1] - half_y + j
+                if 0 <= x < self.number_array.shape[0] and 0 <= y < self.number_array.shape[1]:
+                    frame[i, j] = self.number_array[x, y]
 
-    frame = -1 * np.ones(kernel_shape)
-
-    for i in range(kernel_shape[0]):
-        for j in range(kernel_shape[1]):
-            x = coords[0] - half_x + i
-            y = coords[1] - half_y + j
-            if 0 <= x < number_array.shape[0] and 0 <= y < number_array.shape[1]:
-                frame[i, j] = number_array[x, y]
-
-    return frame
+        return frame
 
 
 class Convolver(object):
