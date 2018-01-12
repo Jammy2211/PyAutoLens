345e531dd73837fb36898f25e0de158b8f12f28d
Richard <richardhayes777@gmail.com>
2018-01-12 09:37:12 +0000

    created frame maker class to simplify API

--- auto_lens/test_frame_convolution.py
+++ auto_lens/test_frame_convolution.py
@@ -26,13 +26,23 @@ def make_cross_number_array():
 
 
 @pytest.fixture(name="simple_frame_array")
-def make_simple_frame_array(simple_number_array):
-    return frame_convolution.make_frame_array(simple_number_array, kernel_shape=(3, 3))
+def make_simple_frame_array(simple_frame_maker):
+    return simple_frame_maker.make_frame_array((3, 3))
 
 
 @pytest.fixture(name="cross_frame_array")
-def make_cross_frame_array(cross_number_array):
-    return frame_convolution.make_frame_array(cross_number_array, kernel_shape=(3, 3))
+def make_cross_frame_array(cross_frame_maker):
+    return cross_frame_maker.make_frame_array((3, 3))
+
+
+@pytest.fixture(name="simple_frame_maker")
+def make_simple_frame_maker():
+    return frame_convolution.FrameMaker(np.ones((3, 3)))
+
+
+@pytest.fixture(name="cross_frame_maker")
+def make_cross_frame_maker(cross_mask):
+    return frame_convolution.FrameMaker(cross_mask)
 
 
 @pytest.fixture(name="simple_kernel")
@@ -59,30 +69,30 @@ class TestNumbering(object):
 
 
 class TestFrameExtraction(object):
-    def test_frame_at_coords(self, simple_number_array):
+    def test_frame_at_coords(self, simple_frame_maker):
         kernel_shape = (3, 3)
 
         # noinspection PyUnresolvedReferences
-        assert (simple_number_array == frame_convolution.frame_at_coords(simple_number_array, coords=(1, 1),
-                                                                         kernel_shape=kernel_shape)).all()
+        assert (simple_frame_maker.number_array == simple_frame_maker.frame_at_coords(coords=(1, 1),
+                                                                                      kernel_shape=kernel_shape)).all()
 
         corner_array = np.array([[-1, -1, -1], [-1, 0, 1], [-1, 3, 4]])
 
-        corner_frame = frame_convolution.frame_at_coords(simple_number_array, coords=(0, 0), kernel_shape=kernel_shape)
+        corner_frame = simple_frame_maker.frame_at_coords(coords=(0, 0), kernel_shape=kernel_shape)
 
         assert (corner_array == corner_frame).all()
 
-    def test_simple_square(self, simple_number_array):
-        frame_array = frame_convolution.make_frame_array(simple_number_array, kernel_shape=(3, 3))
+    def test_simple_square(self, simple_frame_maker):
+        frame_array = simple_frame_maker.make_frame_array(kernel_shape=(3, 3))
 
         assert 9 == len(frame_array)
 
-        assert frame_array[4].shape == simple_number_array.shape
+        assert frame_array[4].shape == simple_frame_maker.number_array.shape
         # noinspection PyUnresolvedReferences
-        assert (frame_array[4] == simple_number_array).all()
+        assert (frame_array[4] == simple_frame_maker.number_array).all()
 
-    def test_masked_square(self, cross_number_array):
-        frame_array = frame_convolution.make_frame_array(cross_number_array, kernel_shape=(3, 3))
+    def test_masked_square(self, cross_frame_maker):
+        frame_array = cross_frame_maker.make_frame_array(kernel_shape=(3, 3))
 
         assert 5 == len(frame_array)
 
@@ -111,17 +121,17 @@ class TestConvolution(object):
         # noinspection PyUnresolvedReferences
         assert (result == [0.5, 0.5, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.5]).all()
 
-    # def test_cross_mask_convolution(self, cross_frame_array, cross_number_array):
-    #     pixel_vector = [1, 0, 0, 0, 1, 0, 0, 0, 1]
-    #     kernel = np.array([[0, 0, 0], [0, 0.5, 0.5], [0, 0, 0]])
-    #
-    #     print(cross_frame_array)
-    #
-    #     convolver = frame_convolution.Convolver(pixel_vector, cross_frame_array, cross_number_array, kernel)
-    #
-    #     result = convolver.convolution
-    #
-    #     print(result)
-    #
-    #     # noinspection PyUnresolvedReferences
-    #     assert (result == [0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0]).all()
+        # def test_cross_mask_convolution(self, cross_frame_array, cross_number_array):
+        #     pixel_vector = [1, 0, 0, 0, 1, 0, 0, 0, 1]
+        #     kernel = np.array([[0, 0, 0], [0, 0.5, 0.5], [0, 0, 0]])
+        #
+        #     print(cross_frame_array)
+        #
+        #     convolver = frame_convolution.Convolver(pixel_vector, cross_frame_array, cross_number_array, kernel)
+        #
+        #     result = convolver.convolution
+        #
+        #     print(result)
+        #
+        #     # noinspection PyUnresolvedReferences
+        #     assert (result == [0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0]).all()
