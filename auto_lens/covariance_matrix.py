01e6c5e00aa92bd4bcdaec38dd143bf6a1162593
Richard <richardhayes777@gmail.com>
2018-01-25 11:33:57 +0000

    covariance matrix generator class

--- auto_lens/covariance_matrix.py
+++ auto_lens/covariance_matrix.py
@@ -2,15 +2,24 @@ def calculate_covariance(mapping_dict_1, mapping_dict_2):
     return sum([mapping_dict_1[i] * mapping_dict_2[i] for i in mapping_dict_1.keys() if i in mapping_dict_2])
 
 
+class CovarianceMatrixGenerator(object):
+    def __init__(self, pixel_maps):
+        self.pixel_maps = pixel_maps
+
+    def calculate_covariance(self, source_index_a, source_index_b):
+        mapping_dict_1 = self.pixel_maps[source_index_a]
+        mapping_dict_2 = self.pixel_maps[source_index_b]
+        return sum([mapping_dict_1[i] * mapping_dict_2[i] for i in mapping_dict_1.keys() if i in mapping_dict_2])
+
+
 class TestCase(object):
     def test_calculate_covariance(self):
-        d1 = {1: 2, 2: 3}
-        d2 = {1: 1}
+        generator = CovarianceMatrixGenerator([{1: 2, 2: 3}, {1: 1}])
 
-        assert calculate_covariance(d1, d2) == 2
+        assert generator.calculate_covariance(0, 1) == 2
 
     def test_no_covariance(self):
-        d1 = {2: 3}
-        d2 = {1: 1}
+        generator = CovarianceMatrixGenerator([{2: 3}, {1: 1}])
 
-        assert calculate_covariance(d1, d2) == 0
+        assert generator.calculate_covariance(0, 1) == 0
+    
