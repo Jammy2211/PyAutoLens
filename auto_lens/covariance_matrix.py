559c4ca9cdb953fee029e73c1d18cd0df9c76c2e
Richard <richardhayes777@gmail.com>
2018-01-25 12:31:36 +0000

    test and failing implementation of contiguous covariance search

--- auto_lens/covariance_matrix.py
+++ auto_lens/covariance_matrix.py
@@ -13,7 +13,7 @@ else:
 class CovarianceMatrixGenerator(object):
     """Class for efficient calculation of big F from little f"""
 
-    def __init__(self, pixel_maps, noise_vector):
+    def __init__(self, pixel_maps, noise_vector, graph):
         """
         Parameters
         ----------
@@ -24,8 +24,18 @@ class CovarianceMatrixGenerator(object):
         """
         self.pixel_maps = pixel_maps
         self.noise_vector = noise_vector
+        self.graph = graph
         # dictionary mapping coordinate tuples to values {(a, b): covariance}
         self.calculated_covariances = {}
+
+    def find_contiguous_covariances(self, source_index):
+        self.add_covariance_for_indices(source_index, source_index)
+        bfs = BreadthFirstSearch(self.graph)
+        bfs.add_neighbours_of(source_index)
+
+        for index in bfs.neighbours():
+            if self.add_covariance_for_indices(source_index, index) > 0:
+                bfs.add_neighbours_of(index)
 
     def add_covariance_for_indices(self, source_index_a, source_index_b):
         """
@@ -122,16 +132,26 @@ class BreadthFirstSearch(object):
                 self.queue.put(neighbour)
 
 
+class TestContiguousCovariances(object):
+    def test_simple_example(self):
+        graph = [[1], [2], [3], [4], []]
+        generator = CovarianceMatrixGenerator([{0: 1}, {0: 1}, {0: 1}, {1: 1}, {0: 1}], [1, 1, 1, 1, 1], graph)
+
+        generator.find_contiguous_covariances(0)
+
+        assert {(0, 1): 1, (0, 2): 1, (0, 3): 0} == generator.calculated_covariances
+
+
 class TestReflexiveCovariances(object):
     def test_reflexive_calculation(self):
         """Does Fab == Fba?"""
-
-        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1}], [1, 1])
+        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1}], [1, 1], None)
 
         assert generator.calculate_covariance(0, 1) == generator.calculate_covariance(1, 0)
 
     def test_reflexive_recall(self):
-        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1}], [1, 1])
+        """Does Fab == Fba because of recall from memory?"""
+        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1}], [1, 1], None)
         generator.calculated_covariances[(0, 1)] = 7
 
         assert generator.add_covariance_for_indices(0, 1) == 7
@@ -185,18 +205,18 @@ class TestBreadthFirstSearch(object):
 class TestCalculateCovariance(object):
     def test_calculate_covariance(self):
         """Is covariance correct in a simple case?"""
-        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1}], [1, 1])
+        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1}], [1, 1], None)
 
         assert generator.calculate_covariance(0, 1) == 2
 
     def test_no_covariance(self):
         """Is covariance zero when two source pixels share no image pixels?"""
-        generator = CovarianceMatrixGenerator([{1: 3}, {0: 1}], [1, 1])
+        generator = CovarianceMatrixGenerator([{1: 3}, {0: 1}], [1, 1], None)
 
         assert generator.calculate_covariance(0, 1) == 0
 
     def test_variable_noise(self):
         """Is the result correct when noise is taken into account?"""
-        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1, 1: 1}], [2, 3])
+        generator = CovarianceMatrixGenerator([{0: 2, 1: 3}, {0: 1, 1: 1}], [2, 3], None)
 
         assert generator.calculate_covariance(0, 1) == 2
