ddfbc5f855cc3131226ee9e6984bb9339258aa4a
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-04-23 10:48:11 +0100

    Refactored ray_tracing unit tests for verbosity.

--- auto_lens/ray_tracing.py
+++ auto_lens/ray_tracing.py
@@ -4,8 +4,8 @@ import numpy as np
 
 
 class RayTrace(object):
-    """The ray-tracing calculations, defined by the image_grid and source planes of every galaxy in this lensing system.
-    These are computed in order of ascending galaxy redshift.
+    """The ray-tracing calculations, defined by a set of grids of coordinates and planes containing every galaxy in \
+    the lensing system. These are computed in order of ascending galaxy redshift.
 
     The ray-tracing calculations between every plane is determined using the angular diameter distances between each \
     set of galaxies.
@@ -26,7 +26,7 @@ class RayTrace(object):
 class TraceImageAndSource(object):
 
     def __init__(self, lens_galaxies, source_galaxies, image_plane_grids):
-        """The ray-tracing calculations, defined by a lensing system with just one image_grid-plane and source-plane.
+        """The ray-tracing calculations, defined by a lensing system with just one image-plane and source-plane.
 
         This has no associated cosmology, thus all calculations are performed in arc seconds and galaxies do not need \
         known redshift measurements. For computational efficiency, it is recommend this ray-tracing class is used for \
@@ -35,12 +35,12 @@ class TraceImageAndSource(object):
         Parameters
         ----------
         lens_galaxies : [Galaxy]
-            The list of lens galaxies in the image_grid-plane.
+            The list of lens galaxies in the image-plane.
         source_galaxies : [Galaxy]
             The list of source galaxies in the source-plane.
         image_plane_grids : RayTracingGrids
-            The image_grid of the ray-tracing calculation, (includes the image.grid, sub_grid, \
-            blurring_grid region etc.), which begins in the image_grid-plane.
+            The image-plane grids of coordinates where ray-tracing calculation are performed, (this includes the image.grid, \
+            sub_grid, blurring.grid etc.).
         """
         self.image_plane = ImagePlane(lens_galaxies, image_plane_grids)
 
@@ -56,8 +56,8 @@ class Plane(object):
     ----------
     galaxies : [Galaxy]
         The galaxies in the plane.
-    plane_coordinates : PlaneCoordinates
-        The x and y image_grid in the plane. Includes all image_grid e.g. the image_grid, sub_grid-grid, sparse_grid-grid, etc.
+    grids : grids.RayTracingGrids
+        The grids of (x,y) coordinates in the plane, including the image grid, sub-grid, blurring grid, etc.
     """
     def __init__(self, galaxies, grids):
 
@@ -68,18 +68,18 @@ class Plane(object):
 class LensPlane(Plane):
 
     def __init__(self, galaxies, grids):
-        """Represents a lens-plane, a set of galaxies and grids at an intermediate redshift in the lens \
-        ray-tracing calculation.
+        """Represents a lens-plane, a set of galaxies and grids at an intermediate redshift in the ray-tracing \
+        calculation.
 
-        A lens-plane is not the final ray-tracing plane and its grids will be traced another, higher \
-        redshift, plane. Thus, the deflection angles due to the plane's galaxies are calculated.
+        A lens-plane is not the final ray-tracing plane and its grids will be traced too another higher \
+        redshift plane. Thus, the deflection angles due to the plane's galaxies are calculated.
 
         Parameters
         ----------
         galaxies : [Galaxy]
             The galaxies in the image_grid-plane.
-        grids : PlaneCoordinates
-            The x and y image_grid in the plane. Includes all image_grid e.g. the image_grid, sub_grid-grid, sparse_grid-grid, etc.
+        grids : grids.RayTracingGrids
+            The grids of (x,y) coordinates in the plane, including the image grid, sub-grid, blurring grid, etc.
         """
 
         super(LensPlane, self).__init__(galaxies, grids)
@@ -87,39 +87,42 @@ class LensPlane(Plane):
         self.deflection_angles = self.grids.deflection_grids_from_galaxies(galaxies)
 
     def trace_to_next_plane(self):
-        """Trace the image_grid pixel image_grid to the next plane, the source-plane.
+        """Trace the grids to the next plane.
 
         NOTE : This does not work for multiplane lensing, which requires one to use the previous plane's deflection \
-        angles to perform the tracing. I guess we'll ultimately call this class 'LensPlanes' and have it as a list."""
-        return self.grids.trace_to_next_grid(self.deflection_angles)
+        angles to perform the tracing. I guess we'll ultimately call this class 'LensPlanes' and have it as a list.
+        """
+        return self.grids.new_grids_from_deflections(self.deflection_angles)
+
 
 class ImagePlane(LensPlane):
 
     def __init__(self, galaxies, grids):
-        """Represents an image_grid-plane, a set of galaxies and grids at the lowest redshift in the lens \
-        ray-tracing calculation.
+        """Represents an image-plane, a set of galaxies and grids at the lowest redshift in the lens ray-tracing \
+        calculation.
 
-        The image_grid-plane is, by definition, a lens-plane, thus the deflection angles at each grids are computed.
+        The image-plane is, by definition, a lens-plane, thus the deflection angles for each grid are computed.
 
-        The image_grid-plane coodinates are defined on the observed image_grid's uniform regular grid. Calculating its light \
-        profiles therefore exploits this uniformity to perform more efficient and precise calculations.
+        The image-plane coodinates are defined on the observed image's uniform regular grid. Calculating its model \
+        images from its light profiles exploits this uniformity to perform more efficient and precise calculations via \
+        an iterative sub-gridding approach.
 
         The light profiles of galaxies at higher redshifts (and therefore in different lens-planes) can be assigned to \
-        the ImagePlane instead. This occurs when:
+        the ImagePlane. This occurs when:
 
         1) The efficiency and precision offered by computing the light profile on a uniform grid is preferred and \
-        won't lead noticeable inaccuracy. For example, computing the light profile of main lens galaxy, ignoring \
+        won't lead noticeable inaccuracy. For example, computing the light profile of the main lens galaxy, ignoring \
         minor lensing effects due to a low mass foreground substructure.
 
-        2) If evaluating the light profile in its lens-plane is inaccurate. For example, when modeling the \
+        2) When evaluating the light profile in its lens-plane is inaccurate. For example, when modeling the \
         point-source images of a lensed quasar, effects like micro-lensing means lens-plane modeling will be inaccurate.
 
         Parameters
         ----------
         galaxies : [Galaxy]
             The galaxies in the image_grid-plane.
-        grids : PlaneCoordinates
-            The x and y image_grid in the plane. Includes all image_grid e.g. the image_grid, sub_grid-grid, sparse_grid-grid, etc.
+        grids : grids.RayTracingGrids
+            The grids of (x,y) coordinates in the plane, including the image grid, sub-grid, blurring grid, etc.
         """
 
         super(ImagePlane, self).__init__(galaxies, grids)
@@ -128,8 +131,8 @@ class ImagePlane(LensPlane):
 class SourcePlane(Plane):
 
     def __init__(self, galaxies, grids):
-        """Represents a source-plane, a set of galaxies and grids at the highest redshift in the lens \
-        ray-tracing calculation.
+        """Represents a source-plane, a set of galaxies and grids at the highest redshift in the ray-tracing \
+        calculation.
 
         A source-plane is the final ray-tracing plane, thus the deflection angles due to the plane's galaxies are \
         not calculated.
@@ -138,7 +141,7 @@ class SourcePlane(Plane):
         ----------
         galaxies : [Galaxy]
             The galaxies in the source-plane.
-        grids : PlaneCoordinates
-            The x and y image_grid in the plane. Includes all image_grid e.g. the image_grid, sub_grid-grid, sparse_grid-grid, etc.
+        grids : grids.RayTracingGrids
+            The grids of (x,y) coordinates in the plane, including the image grid, sub-grid, blurring grid, etc.
         """
         super(SourcePlane, self).__init__(galaxies, grids)
