052565d07ccfd026ed9d9cfceeb97f742f0079e1
Richard Hayes <richardhayes777@gmail.com>
2018-05-11 05:36:50 +0100

    comment format

--- scripts/grid_demo.py
+++ scripts/grid_demo.py
@@ -35,9 +35,9 @@ source_galaxy = galaxy.Galaxy(light_profiles=[sersic])
 print('--- VALUES OF SOURCE GALXY AT COORDINATES (1.0, 1.0) ---')
 print('intensity of source galaxy = ', source_galaxy.intensity_at_coordinates(coordinates[0]), '\n')
 
-# Using just one set of coordinates and having to specify their index isn't ideal. Lets set up the coordinates as a \
-# grid_coords, which is an abstract object containing a set of coordinates which we will then pass through the ray-tracing
-# module.
+# Using just one set of coordinates and having to specify their index isn't ideal. Lets set up the coordinates as a
+# grid_coords, which is an abstract object containing a set of coordinates which we will then pass through the
+# ray-tracing module.
 # (checkout the grids module for more info, basically we can define whatever grids we fancy for ray-tracing.
 coordinates = np.array([[1.0, 1.0],
                         [2.0, 2.0]])
@@ -45,8 +45,8 @@ image_grid = grids.GridCoordsImage(grid_coords=coordinates)
 print('--- VALUES OF GRID COORDINATES [(1.0, 1.0), (2.0, 2.0)] ---')
 print('grid_coords coordinates = ', image_grid, '\n')
 
-# We can have multiple grids (which are different to the image grid_coords we're using here). Thus, before ray-tracing, we
-# combine all of our grids into a grid_coords collection. This doesn't change any of aspect of this tutorial.
+# We can have multiple grids (which are different to the image grid_coords we're using here). Thus, before ray-tracing,
+# we combine all of our grids into a grid_coords collection. This doesn't change any of aspect of this tutorial.
 grid_collection = grids.GridCoordsCollection(image=image_grid)
 print('--- VALUES OF GRID COLLECTION COORDINATES [(1.0, 1.0), (2.0, 2.0)] ---')
 print('grid_coords collection coordinates = ', grid_collection.image, '\n')
@@ -91,8 +91,8 @@ galaxy_ray_trace = ray_trace.generate_image_of_galaxies()
 print('galaxy image (intensities) from source plane = ', galaxy_source_plane)
 print('galaxy image (intensities) from all of ray-tracing (e.g. image plane + source plane) = ', galaxy_ray_trace)
 
-# We will also be able to attach a pixelization to a galaxy, which is used to reconstruct the image via a pixel grid_coords.
-# source_galaxy = galaxy.Galaxy(pixelization=AdaptivePixelization)
+# We will also be able to attach a pixelization to a galaxy, which is used to reconstruct the image via a pixel
+# grid_coords. source_galaxy = galaxy.Galaxy(pixelization=AdaptivePixelization)
 
 # Given that the galaxy's (and therefore pixelizations) are attached to a plane, for each plane we can retrieve its
 # mapping matrix. The mapping matrix is what is called f in Warren & Dye 2003 and tells us the mapping between every
@@ -105,22 +105,22 @@ print('galaxy image (intensities) from all of ray-tracing (e.g. image plane + so
 
 ### POTENTIAL GRID ###
 
-# The potential grid_coords is basically a rectangular grid_coords of coordinates (e.g. of size (20, 20)). We could store these
-# coordinates as another attribute of the GridCoordsCollection class, such that when we call TraceImageAndSource, the
-# image_plane has an additional set of coordinates ray_trace.image.plane.grids.potential_grid. In the image plane, the
-# galaxy potential at these coordinates will be automatically computed.
+# The potential grid_coords is basically a rectangular grid_coords of coordinates (e.g. of size (20, 20)). We could
+# store these coordinates as another attribute of the GridCoordsCollection class, such that when we call
+# TraceImageAndSource, the image_plane has an additional set of coordinates ray_trace.image.plane.grids.potential_grid.
+# In the image plane, the galaxy potential at these coordinates will be automatically computed.
 
-# The potential grid_coords is computed in the image-plane, so it doesn't need to know the traced image coordinates in the \
-# source-plane. Thus, we'll make sure the potential grid_coords coordinates do not have their deflection angles computed and
-# that they are not traced to the source plane.
+# The potential grid_coords is computed in the image-plane, so it doesn't need to know the traced image coordinates in
+# the source-plane. Thus, we'll make sure the potential grid_coords coordinates do not have their deflection angles
+# computed and that they are not traced to the source plane.
 
-# Now our image plane knows the potential grid_coords coordinates (and the galaxies in image plane, from which to compute their
-# potentials), We could extract the potential grid_coords much like the model images / pixelization above:
+# Now our image plane knows the potential grid_coords coordinates (and the galaxies in image plane, from which to
+# compute their potentials), We could extract the potential grid_coords much like the model images / pixelization above:
 # e.g. ray_trace.compute_potential_grid()
 
 # My guess is that this will do something along the lines of:
 
 # 1) Using the image plane, compute the potential on the potential_grid.
 # 2) Using the source plane, determine the mapping matrix or whatever quantity to needed for the potential grid_coords.
-# 3) Use the above quantities to set up the potential grid_coords mapping matrix, which is then passed to the analyis_image
-# module for fitting the data.
+# 3) Use the above quantities to set up the potential grid_coords mapping matrix, which is then passed to the
+# analyis_image module for fitting the data.
