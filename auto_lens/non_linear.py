49851c75500b066cbee20bf33b0d4d62d3581ea1
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-05-22 14:02:02 +0100

    Rich update

--- auto_lens/non_linear.py
+++ auto_lens/non_linear.py
@@ -162,23 +162,38 @@ class MultiNestOptimizer(NonLinearDirectory):
 
         model_info_check.close()
 
-    def setup_results(self):
-        return MultiNestResults(self.path, self.obj_name, self.model_mapper)
+    def setup_results_intermediate(self):
+        return MultiNestResultsIntermediate(self.path, self.obj_name, self.model_mapper)
 
+    def setup_results_final(self):
+        return MultiNestResultsFinal(self.path, self.obj_name, self.model_mapper)
 
-class MultiNestResults(NonLinearDirectory):
+class MultiNestResultsIntermediate(NonLinearDirectory):
 
-    def __init__(self, path, obj_name, model_mapper, limit=0.9973):
+    def __init__(self, path, obj_name, model_mapper):
+        """Class which stores the intermediate results of a MultiNest analysis, e.g. before the non-linear sampling \
+        is complete.
 
-        super(MultiNestResults, self).__init__(path, obj_name, model_mapper)
+        This corresponds to the most likely and most probable models, and allows the model images, residuals, \
+        chi-sq image, etc. to be inspected before a MultiNest analysis is complete.
+
+        Parameters
+        -----------
+        path : str
+            The path where the non_linear results are stored.
+        obj_name : str
+            Unique identifier of the data being analysed (e.g. the name of the data set)
+        model_mapper : CalibrationModel.ModelMapper
+            Maps the model priors to a set of parameters (a model instance)
+        """
+
+        super(MultiNestResultsIntermediate, self).__init__(path, obj_name, model_mapper)
 
         self.files = MultiNestFiles(self.obj_name, self.results_path)
-        self.pdf = getdist.mcsamples.loadMCSamples(self.files.weighted_samples)
 
-        self.setup_most_likely_and_probable_models()
-        self.setup_1d_upper_and_lower_limits(limit)
+        self.setup_most_likely_and_probable()
 
-    def setup_most_likely_and_probable_models(self):
+    def setup_most_likely_and_probable(self):
         """Setup the most likely and probable models. This is performed as both 1D vectors of all parameters and a \
         model_mapper instance of each model.
 
@@ -188,56 +203,22 @@ class MultiNestResults(NonLinearDirectory):
         The most likely model is defined as the model which gives the highest likelihood, regardless of the inferred
         posterior distribution.
         """
-        self._most_probable_model = self.read_most_probable_model()
-        self._most_likely_model = self.read_most_likely_model()
+        self._most_probable = self.read_most_probable()
+        self._most_likely = self.read_most_likely()
 
-        self.most_probable_model = self.model_mapper.from_physical_vector(self._most_probable_model)
-        self.most_likely_model = self.model_mapper.from_physical_vector(self._most_likely_model)
+        self.most_probable = self.model_mapper.from_physical_vector(self._most_probable)
+        self.most_likely = self.model_mapper.from_physical_vector(self._most_likely)
 
-    def setup_1d_upper_and_lower_limits(self, limit):
-        """Setup 1D vectors of the upper and lower limits of the multinest results.
-
-        These are generated at an input limfrac, which gives the percentage of 1d posterior weighted samples within \
-        each parameter estimate
-
-        Parameters
-        -----------
-        limit : float
-            The limit on the sample to calculate.
-        """
-        self.densities_1d = list(map(lambda p : self.pdf.get1DDensity(p), self.pdf.getParamNames().names))
-
-        limits = list(map(lambda p : p.getLimits(limit), self.densities_1d))
-
-        self._lower_limits_1d = list(map(lambda p : p[0], limits))
-        self._upper_limits_1d = list(map(lambda p : p[1], limits))
-
-    def setup_weighted_sample_model(self, index):
-        """Setup a model instance of a weighted sample, including its weight and likelihood.
-
-        Parameters
-        -----------
-        index : int
-            The index of the weighted sample to return.
-        """
-        model, weight, likelihood = self.read_weighted_sample_model(index)
-
-        self._weighted_sample_model = model
-
-        self.weighted_sample_model = self.model_mapper.from_physical_vector(model)
-        self.weighted_sample_weight = weight
-        self.weighted_sample_likelihood = likelihood
-
-    def read_most_probable_model(self):
+    def read_most_probable(self):
         return self.read_vector_from_summary(self.files.summary, self.total_parameters, 0)
 
-    def read_most_likely_model(self):
+    def read_most_likely(self):
         return self.read_vector_from_summary(self.files.summary, self.total_parameters, 28)
 
     @staticmethod
     def read_vector_from_summary(filename_summary, total_parameters, offset):
         """
-        Read the most probable or most likely model values from the 'summary.txt' file which results from a \
+        Read the most probable or most likely model values from the 'obj_summary.txt' file which results from a \
         multinest analysis.
 
         This file stores the parameters of the most probable model in the first half of entries and the most likely
@@ -265,6 +246,71 @@ class MultiNestResults(NonLinearDirectory):
         summary.close()
 
         return vector
+
+
+class MultiNestResultsFinal(MultiNestResultsIntermediate):
+
+    def __init__(self, path, obj_name, model_mapper, limit=0.9973):
+        """Class which stores the final results of a MultiNest analysis, including the intermediate results \
+        produced by the parent class *MultiNestResultsFinal* (these are the most likely / probably models).
+
+        The final results use the library *GetDist* to compute and visualize the probably distribution function \
+        (PDF) of parameter space. This uses the weighted-samples output by MultiNest and allows the marginalized PDF's \
+        of parameters in 1D and 2D to be plotted.
+
+        Confidence limits on parameters are also calculated, by marginalized parameters over 1 or 2 dimensions.
+
+        Parameters
+        -----------
+        path : str
+            The path where the non_linear results are stored.
+        obj_name : str
+            Unique identifier of the data being analysed (e.g. the name of the data set)
+        model_mapper : CalibrationModel.ModelMapper
+            Maps the model priors to a set of parameters (a model instance)
+        limit : float
+            The fraction of a PDF used to estimate errors.
+        """
+
+        super(MultiNestResultsFinal, self).__init__(path, obj_name, model_mapper)
+
+        self.pdf = getdist.mcsamples.loadMCSamples(self.files.weighted_samples)
+
+        self.setup_1d_upper_and_lower_limits(limit)
+
+    def setup_1d_upper_and_lower_limits(self, limit):
+        """Setup 1D vectors of the upper and lower limits of the multinest results.
+
+        These are generated at an input limfrac, which gives the percentage of 1d posterior weighted samples within \
+        each parameter estimate
+
+        Parameters
+        -----------
+        limit : float
+            The fraction of a PDF used to estimate errors.
+        """
+        self.densities_1d = list(map(lambda p : self.pdf.get1DDensity(p), self.pdf.getParamNames().names))
+
+        limits = list(map(lambda p : p.getLimits(limit), self.densities_1d))
+
+        self._lower_limits_1d = list(map(lambda p : p[0], limits))
+        self._upper_limits_1d = list(map(lambda p : p[1], limits))
+
+    def setup_weighted_sample_model(self, index):
+        """Setup a model instance of a weighted sample, including its weight and likelihood.
+
+        Parameters
+        -----------
+        index : int
+            The index of the weighted sample to return.
+        """
+        model, weight, likelihood = self.read_weighted_sample_model(index)
+
+        self._weighted_sample_model = model
+
+        self.weighted_sample_model = self.model_mapper.from_physical_vector(model)
+        self.weighted_sample_weight = weight
+        self.weighted_sample_likelihood = likelihood
 
     def read_weighted_sample_model(self, index):
         """From a weighted sample return the model, weight and likelihood hood.
