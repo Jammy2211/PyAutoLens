89c065c4a0770eed06bbf0f74c98afe85284d230
Richard Hayes <richardhayes777@gmail.com>
2018-05-22 09:21:42 +0100

    note and format

--- scripts/simlens.py
+++ scripts/simlens.py
@@ -1,24 +1,26 @@
 import numpy as np
 import os
 import sys
+
 sys.path.append("../")
 
 from auto_lens.imaging import imaging, simulate
 from astropy.io import fits
 
-def numpy_array_to_fits(array, path, filename):
 
+def numpy_array_to_fits(array, path, filename):
     new_hdr = fits.Header()
     hdu = fits.PrimaryHDU(array, new_hdr)
     hdu.writeto(path + filename + '.fits')
+
 
 data_path = "{}/../data/".format(os.path.dirname(os.path.realpath(__file__)))
 
 pixel_scale = 0.05
 dimensions = (256, 256)
 
-psf = imaging.PSF.from_fits(path=data_path+'slacs/', filename='slacs_1_post.fits', hdu=3, pixel_scale=pixel_scale)
-psf.trim(new_dimensions=(15,15))
+psf = imaging.PSF.from_fits(path=data_path + 'slacs/', filename='slacs_1_post.fits', hdu=3, pixel_scale=pixel_scale)
+psf.trim(new_dimensions=(15, 15))
 
 exposure_time = imaging.ExposureTime.from_one_value(exposure_time=5e8, pixel_dimensions=dimensions,
                                                     pixel_scale=pixel_scale)
@@ -32,8 +34,8 @@ image = simulate.SimulateImage.from_fits(path=data_path, filename='SimLens.fits'
 
 print(np.max(image.data_original), np.min(image.data_original))
 print(np.max(image.data), np.min(image.data))
-#print(np.max(image.noise), np.min(image.noise))
-#print(np.max(image.signal_to_noise_ratio))
+# print(np.max(image.noise), np.min(image.noise))
+# print(np.max(image.signal_to_noise_ratio))
 
 image.plot()
 
