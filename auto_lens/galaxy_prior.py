efa17e78a6d3aec3356dbbb77cb8de93a6b4a9c7
Richard Hayes <richardhayes777@gmail.com>
2018-06-05 17:15:34 +0100

    basic, partially tested galaxy prior

---
+++ auto_lens/galaxy_prior.py
@@ -0,0 +1,28 @@
+import random
+import string
+from auto_lens import galaxy
+
+
+class GalaxyPrior:
+    def __init__(self, light_profile_classes=None, mass_profile_classes=None):
+        self.id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+
+        self.light_profile_classes = light_profile_classes if light_profile_classes is not None else []
+        self.mass_profile_classes = mass_profile_classes if mass_profile_classes is not None else []
+
+    def attach_to_model_mapper(self, model_mapper):
+        for num, light_profile_class in enumerate(self.light_profile_classes):
+            model_mapper.add_class("{}_light_profile_{}".format(self.id, num), light_profile_class)
+
+        for num, mass_profile_class in enumerate(self.mass_profile_classes):
+            model_mapper.add_class("{}_mass_profile_{}".format(self.id, num), mass_profile_class)
+
+    def galaxy_for_model_instance(self, model_instance):
+        light_profiles = []
+        mass_profiles = []
+        for num in range(len(self.light_profile_classes)):
+            light_profiles.append(getattr(model_instance, "{}_light_profile_{}".format(self.id, num)))
+        for num in range(len(self.mass_profile_classes)):
+            mass_profiles.append(getattr(model_instance, "{}_mass_profile_{}".format(self.id, num)))
+
+        return galaxy.Galaxy(light_profiles=light_profiles, mass_profiles=mass_profiles)
