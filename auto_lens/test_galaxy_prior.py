0c88a88af57d60526293855738d2f64d2635ae7f
Richard Hayes <richardhayes777@gmail.com>
2018-06-06 08:18:49 +0100

    fixtures

--- auto_lens/test_galaxy_prior.py
+++ auto_lens/test_galaxy_prior.py
@@ -1,5 +1,6 @@
 from auto_lens import galaxy_prior as gp
 from auto_lens.profiles import mass_profiles, light_profiles
+import pytest
 
 
 class MockModelMapper:
@@ -10,13 +11,19 @@ class MockModelMapper:
         self.classes[name] = cls
 
 
+@pytest.fixture(name="mapper")
+def make_mapper():
+    return MockModelMapper()
+
+
+@pytest.fixture(name="galaxy_prior")
+def make_galaxy_prior():
+    return gp.GalaxyPrior(light_profile_classes=[light_profiles.EllipticalDevVaucouleurs],
+                          mass_profile_classes=[mass_profiles.EllipticalCoredIsothermal])
+
+
 class TestGalaxyPrior:
-    def test_attach_to_model_mapper(self):
-        galaxy_prior = gp.GalaxyPrior(light_profile_classes=[light_profiles.EllipticalDevVaucouleurs],
-                                      mass_profile_classes=[mass_profiles.EllipticalCoredIsothermal])
-
-        mapper = MockModelMapper()
-
+    def test_attach_to_model_mapper(self, galaxy_prior, mapper):
         galaxy_prior.attach_to_model_mapper(mapper)
 
         assert len(mapper.classes) == 2
