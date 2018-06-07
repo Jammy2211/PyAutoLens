cb2747cd053b281eb00aee3e662887227f61e9b5
Richard Hayes <richardhayes777@gmail.com>
2018-06-07 11:06:05 +0100

    docs

--- auto_lens/analysis/pipline.py
+++ auto_lens/analysis/pipline.py
@@ -1,3 +1,22 @@
 class Analysis(object):
     def __init__(self, image, lens_galaxy_priors, source_galaxy_priors, pixelization, non_linear_optimizer):
+        """
+        A class encapsulating an analysis. An analysis takes an image and a set of galaxy priors describing an
+        assumed model and applies a pixelization and non linear optimizer to find the best possible fit between the
+        image and model.
+
+        Parameters
+        ----------
+        image: Image
+            An image of the galaxy to be fit
+        lens_galaxy_priors: [GalaxyPrior]
+            A list of prior instances describing the lens
+        source_galaxy_priors: [GalaxyPrior]
+            A list of prior instances describing the source
+        pixelization: Pixelization
+            A class determining how the source plane should be pixelized
+        non_linear_optimizer: NonLinearOptimizer
+            A wrapper around a library that searches an n-dimensional space by providing unit vector hypercubes to
+            the analysis.
+        """
         pass
