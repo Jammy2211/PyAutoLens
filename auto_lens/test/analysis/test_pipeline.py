d4503dec9610ea28a29f8925ee67f15e11922836
Richard Hayes <richardhayes777@gmail.com>
2018-06-07 14:19:12 +0100

    extracting mock classes to mock module

--- auto_lens/test/analysis/test_pipeline.py
+++ auto_lens/test/analysis/test_pipeline.py
@@ -1,38 +1,21 @@
 from auto_lens.analysis import pipeline
-
-
-class MockImage:
-    pass
-
-
-class MockGalaxyPrior:
-    def __init__(self):
-        self.model_mapper = None
-
-    def attach_to_model_mapper(self, model_mapper):
-        self.model_mapper = model_mapper
-
-
-class MockModelMapper:
-    pass
-
-
-class MockPixelization:
-    pass
-
-
-class MockNLO:
-    pass
+from auto_lens.analysis import galaxy_prior
+from auto_lens.test import mock
 
 
 class TestModelAnalysis:
     def test_setup(self):
-        lens_galaxy_prior = MockGalaxyPrior()
-        source_galaxy_prior = MockGalaxyPrior()
+        lens_galaxy_prior = galaxy_prior.GalaxyPrior()
+        source_galaxy_prior = galaxy_prior.GalaxyPrior()
 
-        pipeline.ModelAnalysis(image=MockImage(), lens_galaxy_priors=[lens_galaxy_prior],
-                               source_galaxy_priors=[source_galaxy_prior], pixelization=MockPixelization(),
-                               model_mapper=MockModelMapper(), non_linear_optimizer=MockNLO())
+        model_mapper = mock.MockModelMapper()
 
-        assert lens_galaxy_prior.model_mapper is not None
-        assert source_galaxy_prior.model_mapper is not None
+        pipeline.ModelAnalysis(image=mock.MockImage(), lens_galaxy_priors=[lens_galaxy_prior],
+                               source_galaxy_priors=[source_galaxy_prior], pixelization=mock.MockPixelization(),
+                               model_mapper=model_mapper, non_linear_optimizer=mock.MockNLO())
+
+        assert len(model_mapper.classes) == 2
+
+    def test_run(self):
+        pass
+
