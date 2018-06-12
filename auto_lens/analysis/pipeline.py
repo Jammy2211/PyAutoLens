391247c357e9568cc5e38d5ba5c3041a1ff1be54
Richard Hayes <richardhayes777@gmail.com>
2018-06-12 15:53:10 +0100

    created run object

--- auto_lens/analysis/pipeline.py
+++ auto_lens/analysis/pipeline.py
@@ -6,9 +6,8 @@ from auto_lens.analysis import ray_tracing
 
 
 class ModelAnalysis(object):
-    def __init__(self, image, mask, lens_galaxy_priors, source_galaxy_priors, pixelization,
-                 model_mapper=mm.ModelMapper(), non_linear_optimizer=non_linear.MultiNestWrapper(),
-                 likelihood_for_tracer=fitting.likelihood_for_tracer):
+    def __init__(self, lens_galaxy_priors, source_galaxy_priors, pixelization,
+                 model_mapper=mm.ModelMapper(), non_linear_optimizer=non_linear.MultiNestWrapper()):
         """
         A class encapsulating an analysis. An analysis takes an image and a set of galaxy priors describing an
         assumed model and applies a pixelization and non linear optimizer to find the best possible fit between the
@@ -16,10 +15,6 @@ class ModelAnalysis(object):
 
         Parameters
         ----------
-        image: Image
-            An image of the galaxy to be fit
-        mask: Mask
-            A mask eliminating the areas of the image that are not relevant to the analysis
         lens_galaxy_priors: [GalaxyPrior]
             A list of prior instances describing the lens
         source_galaxy_priors: [GalaxyPrior]
@@ -33,56 +28,16 @@ class ModelAnalysis(object):
             the analysis.
         """
 
-        self.image = image
-        self.mask = mask
         self.lens_galaxy_priors = lens_galaxy_priors
         self.source_galaxy_priors = source_galaxy_priors
         self.pixelization = pixelization
         self.non_linear_optimizer = non_linear_optimizer
         self.model_mapper = model_mapper
-        self.likelihood_for_tracer = likelihood_for_tracer
-
-        # TODO: is this step correct? How is the mask chosen?
-        self.image_grid_collection = grids.GridCoordsCollection.from_mask(mask)
 
         for galaxy_prior in lens_galaxy_priors + source_galaxy_priors:
             galaxy_prior.attach_to_model_mapper(model_mapper)
 
-        self.lens_galaxies = None
-        self.source_galaxies = None
-        self.likelihood = None
-
-    def fitness_function(self, physical_values):
-        """
-        Generates a model instance from a set of physical values, uses this to construct galaxies and ultimately a
-        tracer that can be passed to analyse.likelihood_for_tracer.
-
-        Parameters
-        ----------
-        physical_values: [float]
-            Physical values from a non-linear optimiser
-
-        Returns
-        -------
-        likelihood: float
-            A value for the likelihood associated with this set of physical values in conjunction with the provided
-            model
-        """
-
-        # Recover classes from physical values
-        model_instance = self.model_mapper.from_physical_vector(physical_values)
-        # Construct galaxies from their priors
-        self.lens_galaxies = list(map(lambda galaxy_prior: galaxy_prior.galaxy_for_model_instance(model_instance),
-                                      self.lens_galaxy_priors))
-        self.source_galaxies = list(map(lambda galaxy_prior: galaxy_prior.galaxy_for_model_instance(model_instance),
-                                        self.source_galaxy_priors))
-        # Construct a ray tracer
-        tracer = ray_tracing.Tracer(self.lens_galaxies, self.source_galaxies, self.image_grid_collection)
-        # Determine likelihood:
-        self.likelihood = self.likelihood_for_tracer(tracer)
-        return self.likelihood
-
-    def run(self):
+    def run(self, image, mask):
         """
         Run the analysis, iteratively analysing each set of values provided by the non-linear optimiser until it
         terminates.
@@ -92,28 +47,74 @@ class ModelAnalysis(object):
         result: Result
             The result of the analysis, comprising a likelihood and the final set of galaxies generated
         """
+        image_grid_collection = grids.GridCoordsCollection.from_mask(mask)
+
+        run = self.__class__.Run(image, image_grid_collection, self.model_mapper, self.lens_galaxy_priors,
+                                 self.source_galaxy_priors)
 
         # Run the optimiser using the fitness function. The fitness function is applied iteratively until the optimiser
         # terminates
-        self.non_linear_optimizer.run(self.fitness_function, self.model_mapper.priors_ordered_by_id)
-        return self.__class__.Result(self)
+        self.non_linear_optimizer.run(run.fitness_function, self.model_mapper.priors_ordered_by_id)
+        return self.__class__.Result(run)
+
+    class Run(object):
+        def __init__(self, image, image_grid_collection, model_mapper, lens_galaxy_priors, source_galaxy_priors):
+            self.image = image
+            self.model_mapper = model_mapper
+            self.lens_galaxy_priors = lens_galaxy_priors
+            self.source_galaxy_priors = source_galaxy_priors
+            self.lens_galaxies = []
+            self.source_galaxies = []
+            self.image_grid_collection = image_grid_collection
+            self.likelihood = None
+
+        def fitness_function(self, physical_values):
+            """
+            Generates a model instance from a set of physical values, uses this to construct galaxies and ultimately a
+            tracer that can be passed to analyse.likelihood_for_tracer.
+
+            Parameters
+            ----------
+            physical_values: [float]
+                Physical values from a non-linear optimiser
+
+            Returns
+            -------
+            likelihood: float
+                A value for the likelihood associated with this set of physical values in conjunction with the provided
+                model
+            """
+
+            # Recover classes from physical values
+            model_instance = self.model_mapper.from_physical_vector(physical_values)
+            # Construct galaxies from their priors
+            self.lens_galaxies = list(map(lambda galaxy_prior: galaxy_prior.galaxy_for_model_instance(model_instance),
+                                          self.lens_galaxy_priors))
+            self.source_galaxies = list(map(lambda galaxy_prior: galaxy_prior.galaxy_for_model_instance(model_instance),
+                                            self.source_galaxy_priors))
+
+            # Construct a ray tracer
+            tracer = ray_tracing.Tracer(self.lens_galaxies, self.source_galaxies, self.image_grid_collection)
+            # Determine likelihood:
+            self.likelihood = fitting.likelihood_for_image_and_tracer(self.image, tracer)
+            return self.likelihood
 
     class Result(object):
-        def __init__(self, analysis):
+        def __init__(self, run):
             """
             The result of an analysis
 
             Parameters
             ----------
-            analysis: ModelAnalysis
-                An analysis
+            run: Run
+                An analysis run
             """
             # The final likelihood found
-            self.likelihood = analysis.likelihood
+            self.likelihood = run.likelihood
             # The final lens galaxies generated
-            self.lens_galaxies = analysis.lens_galaxies
+            self.lens_galaxies = run.lens_galaxies
             # The final source galaxies generated
-            self.source_galaxies = analysis.source_galaxies
+            self.source_galaxies = run.source_galaxies
 
 
 class MainPipeline(object):
