b2737e4c771b922523c0c67116a6e1923f28244d
Richard Hayes <richardhayes777@gmail.com>
2018-06-13 13:03:47 +0100

    fixed pipeline tests by defining mock config/using mock pixelization

--- auto_lens/test/analysis/test_pipeline.py
+++ auto_lens/test/analysis/test_pipeline.py
@@ -1,7 +1,6 @@
 from auto_lens.analysis import pipeline as pl
 from auto_lens.analysis import galaxy_prior
 from auto_lens.analysis import model_mapper as mm
-from auto_lens.pixelization import pixelization as px
 from auto_lens import instrumentation as inst
 import pytest
 import os
@@ -17,6 +16,12 @@ class MockResult:
         self.instrumentation = instrumentation
         self.lens_galaxies = lens_galaxies
         self.source_galaxies = source_galaxies
+
+
+class MockPixelization:
+    def __init__(self, number_clusters, regularization_coefficient=1):
+        self.number_clusters = number_clusters
+        self.regularization_coefficient = regularization_coefficient
 
 
 class MockModelAnalysis:
@@ -46,7 +51,7 @@ class MockHyperparameterAnalysis(object):
         self.mask = mask
         self.lens_galaxies = lens_galaxies
         self.source_galaxies = source_galaxies
-        return MockResult(pixelization=px.VoronoiPixelization(0), instrumentation=inst.Instrumentation(0))
+        return MockResult(pixelization=MockPixelization(0), instrumentation=inst.Instrumentation(0))
 
 
 class MockImage:
@@ -95,8 +100,9 @@ class MockMask:
 
 @pytest.fixture(name='test_config')
 def make_test_config():
-    return mm.Config(
-        config_folder_path="{}/../{}".format(os.path.dirname(os.path.realpath(__file__)), "test_files/config"))
+    path = "{}/../{}".format(os.path.dirname(os.path.realpath(__file__)), "test_files/config")
+    print(path)
+    return mm.Config(config_folder_path=path)
 
 
 @pytest.fixture(name="lens_galaxy_prior")
@@ -129,7 +135,7 @@ class TestModelAnalysis:
         assert len(model_mapper.prior_models) == 2
 
     def test_run(self, model_analysis):
-        result = model_analysis.run(MockImage(), MockMask(), px.VoronoiPixelization(0), inst.Instrumentation(0))
+        result = model_analysis.run(MockImage(), MockMask(), MockPixelization(0), inst.Instrumentation(0))
         assert len(model_analysis.non_linear_optimizer.priors) == 2
 
         assert result.likelihood == 1
@@ -139,12 +145,12 @@ class TestModelAnalysis:
 
 class TestHyperparameterAnalysis:
     def test_setup(self, model_mapper):
-        pl.HyperparameterAnalysis(px.VoronoiPixelization, inst.Instrumentation, model_mapper, MockNLO([0.5, 0.5, 0.5]))
+        pl.HyperparameterAnalysis(MockPixelization, inst.Instrumentation, model_mapper, MockNLO([0.5, 0.5, 0.5]))
 
         assert len(model_mapper.prior_models) == 2
 
     def test_run(self, model_mapper):
-        hyperparameter_analysis = pl.HyperparameterAnalysis(px.VoronoiPixelization, inst.Instrumentation, model_mapper,
+        hyperparameter_analysis = pl.HyperparameterAnalysis(MockPixelization, inst.Instrumentation, model_mapper,
                                                             MockNLO([0.5, 0.5, 0.5]))
 
         result = hyperparameter_analysis.run(MockImage(), MockMask(), [MockGalaxy()], [MockGalaxy()])
@@ -159,9 +165,9 @@ class TestMainPipeline:
     def test_main_pipeline(self):
         hyperparameter_analysis = MockHyperparameterAnalysis()
         model_analysis = MockModelAnalysis()
-        pipeline = pl.MainPipeline(model_analysis,
-                                   hyperparameter_analysis=hyperparameter_analysis)
-        results = pipeline.run(MockImage(), MockMask(), px.VoronoiPixelization(0), inst.Instrumentation(0))
+        # noinspection PyTypeChecker
+        pipeline = pl.MainPipeline(model_analysis, hyperparameter_analysis=hyperparameter_analysis)
+        results = pipeline.run(MockImage(), MockMask(), MockPixelization(0), inst.Instrumentation(0))
         assert len(results) == 2
         assert len(hyperparameter_analysis.source_galaxies) == 2
         assert len(hyperparameter_analysis.lens_galaxies) == 1
