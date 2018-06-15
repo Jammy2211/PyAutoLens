deb6058e5afb8d3ad0e886d51fda62c986f2b409
Richard Hayes <richardhayes777@gmail.com>
2018-06-15 14:15:15 +0100

    continuing pipeline

--- auto_lens/analysis/pipeline.py
+++ auto_lens/analysis/pipeline.py
@@ -34,23 +34,31 @@ def source_only_pipeline(image, mask, instrumentation):
        Source: Sersic
        NLO: LM
     """
-    mapper = mm.ModelMapper()
-    optimizer = non_linear.LevenbergMarquardt(mapper)
+    mapper_1 = mm.ModelMapper()
+    optimizer_1 = non_linear.LevenbergMarquardt(mapper_1)
 
-    source_galaxy_prior = galaxy_prior.GalaxyPrior("source_galaxy_prior", mapper,
+    source_galaxy_prior = galaxy_prior.GalaxyPrior("source_galaxy_prior", mapper_1,
                                                    light_profile=light_profiles.EllipticalSersic)
-    lens_galaxy_prior = galaxy_prior.GalaxyPrior("lens_galaxy_prior", mapper,
+    lens_galaxy_prior = galaxy_prior.GalaxyPrior("lens_galaxy_prior", mapper_1,
                                                  spherical_mass_profile=mass_profiles.SphericalIsothermal,
                                                  shear_mass_profile=mass_profiles.ExternalShear)
 
-    initialization_1 = analysis.Analysis(mapper, non_linear_optimizer=optimizer,
+    initialization_1 = analysis.Analysis(mapper_1, non_linear_optimizer=optimizer_1,
                                          source_galaxy_priors=[source_galaxy_prior],
                                          lens_galaxy_priors=[lens_galaxy_prior])
 
     result = initialization_1.run(image, mask, instrumentation=instrumentation)
     results.append(result)
 
-    prior_results = mapper.prior_results_for_gaussian_tuples(optimizer.compute_gaussian_priors(SIGMA_LIMIT))
+    prior_results = mapper_1.prior_results_for_gaussian_tuples(optimizer_1.compute_gaussian_priors(SIGMA_LIMIT))
+
+    """
+    2) Mass: SIE+Shear (priors from phase 1)
+       Source: 'smooth' pixelization (include regularization parameter(s) in the model)
+       NLO: LM
+    """
+    mapper_2 = mm.ModelMapper()
+    optimizer_2 = non_linear.LevenbergMarquardt(mapper_2)
 
 
 """
