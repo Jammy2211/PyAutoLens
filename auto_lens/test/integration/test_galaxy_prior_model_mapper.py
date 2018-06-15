3682a111a9f79c9dc2c7bdeeaccea9d49ac468f4
Richard Hayes <richardhayes777@gmail.com>
2018-06-15 12:26:10 +0100

    fixed tests to work with new model mapper paradigm

--- auto_lens/test/integration/test_galaxy_prior_model_mapper.py
+++ auto_lens/test/integration/test_galaxy_prior_model_mapper.py
@@ -12,19 +12,15 @@ class TestCase:
 
         # Create a galaxy prior for the source galaxy. Here we are describing only the light profile of the source
         # galaxy which comprises an elliptical exponential and elliptical sersic light profile.
-        source_galaxy_prior = gp.GalaxyPrior("source_galaxy",
+        source_galaxy_prior = gp.GalaxyPrior("source_galaxy", mapper,
                                              light_profile_one=light_profiles.EllipticalExponential,
                                              light_profile_2=light_profiles.EllipticalSersic)
 
         # Create a galaxy prior for the source galaxy. Here we are describing both the light and mass profiles. We've
         # also stipulated that the centres of any galaxies generated using the galaxy prior should match.
-        lens_galaxy_prior = gp.GalaxyPrior("lens_galaxy", light_profile=light_profiles.EllipticalExponential,
+        lens_galaxy_prior = gp.GalaxyPrior("lens_galaxy", mapper, light_profile=light_profiles.EllipticalExponential,
                                            mass_profile=mass_profiles.EllipticalExponentialMass,
                                            align_centres=True)
-
-        # Associate the galaxy priors with our model mapper.
-        source_galaxy_prior.attach_to_model_mapper(mapper)
-        lens_galaxy_prior.attach_to_model_mapper(mapper)
 
         # Create a model instance. All the instances of the profile classes are created here. Normally we would do this
         # using the output of a non linear search but in this case we are using the median values from the priors.
