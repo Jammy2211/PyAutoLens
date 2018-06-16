27cfb794f6add50e889edd4f886897ba393adb4f
Richard Hayes <richardhayes777@gmail.com>
2018-06-16 09:05:56 +0100

    sketch of how pipeline should work

--- auto_lens/analysis/pipeline.py
+++ auto_lens/analysis/pipeline.py
@@ -35,21 +35,16 @@ def source_only_pipeline(image, mask, instrumentation):
        Source: Sersic
        NLO: LM
     """
-    mapper_1 = mm.ModelMapper()
-    optimizer_1 = non_linear.LevenbergMarquardt(mapper_1)
+    optimizer_1 = non_linear.LevenbergMarquardt()
 
-    source_galaxy_prior = galaxy_prior.GalaxyPrior("source_galaxy_prior", mapper_1,
-                                                   light_profile=light_profiles.EllipticalSersic)
-    lens_galaxy_prior = galaxy_prior.GalaxyPrior("lens_galaxy_prior", mapper_1,
-                                                 spherical_mass_profile=mass_profiles.SphericalIsothermal,
+    source_galaxy_prior = galaxy_prior.GalaxyPrior(light_profile=light_profiles.EllipticalSersic)
+    lens_galaxy_prior = galaxy_prior.GalaxyPrior(spherical_mass_profile=mass_profiles.SphericalIsothermal,
                                                  shear_mass_profile=mass_profiles.ExternalShear)
 
-    initialization_1 = analysis.Analysis(mapper_1, non_linear_optimizer=optimizer_1,
-                                         source_galaxy_priors=[source_galaxy_prior],
-                                         lens_galaxy_priors=[lens_galaxy_prior])
+    optimizer_1.add("source_galaxies", [source_galaxy_prior])
+    optimizer_1.add("lens_galaxies", [lens_galaxy_prior])
 
-    result_1 = initialization_1.run(image, mask, instrumentation=instrumentation)
-    prior_result_1 = mapper_1.prior_results_for_gaussian_tuples(optimizer_1.compute_gaussian_priors(SIGMA_LIMIT))
+    result_1 = analysis.analyse(image, mask, optimizer_1, instrumentation=instrumentation)
 
     results.append(result_1)
 
@@ -58,22 +53,20 @@ def source_only_pipeline(image, mask, instrumentation):
        Source: 'smooth' pixelization (include regularization parameter(s) in the model)
        NLO: LM
     """
-    mapper_2 = mm.ModelMapper()
-    optimizer_2 = non_linear.LevenbergMarquardt(mapper_2)
+    optimizer_2 = non_linear.LevenbergMarquardt()
 
-    lens_galaxy_prior = galaxy_prior.GalaxyPrior("lens_galaxy_prior", mapper_2,
-                                                 spherical_mass_profile=mass_profiles.SphericalIsothermal,
+    lens_galaxy_prior = galaxy_prior.GalaxyPrior(spherical_mass_profile=mass_profiles.SphericalIsothermal,
                                                  shear_mass_profile=mass_profiles.ExternalShear)
+    source_galaxy_prior = galaxy_prior.GalaxyPrior(pixelization=pixelization.SquarePixelization)
 
-    lens_galaxy_prior.override_prior_models(spherical_mass_profile=prior_result_1.spherical_mass_profile,
-                                            shear_mass_profile=prior_result_1.shear_mass_profile)
+    optimizer_2.add("lens_galaxies", [lens_galaxy_prior])
+    optimizer_2.add("source_galaxies", [source_galaxy_prior])
 
-    initialization_2 = analysis.Analysis(mapper_2, non_linear_optimizer=optimizer_2,
-                                         lens_galaxy_priors=[lens_galaxy_prior],
-                                         pixelization_class=pixelization.SquarePixelization)
+    lens_galaxy_prior.spherical_mass_profile = result_1.priors.spherical_mass_profile
+    lens_galaxy_prior.shear_mass_profile = result_1.priors.shear_mass_profile
 
-    result_2 = initialization_2.run(image, mask, instrumentation=instrumentation)
-    prior_result_2 = mapper_1.prior_results_for_gaussian_tuples(optimizer_1.compute_gaussian_priors(SIGMA_LIMIT))
+    result_2 = analysis.analyse(image, mask, optimizer_2)
+
     results.append(result_2)
 
     """
@@ -82,38 +75,35 @@ def source_only_pipeline(image, mask, instrumentation):
         Source: 'noisy' pixelization
         NLO: MN
     """
-    mapper_2h = mm.ModelMapper()
-    optimizer_2h = non_linear.MultiNest(mapper_2h)
+    optimizer_2h = non_linear.MultiNest()
 
-    initialization_2h = analysis.Analysis(mapper_2h, non_linear_optimizer=optimizer_2h,
-                                          pixelization_class=pixelization.VoronoiPixelization,
-                                          instrumentation_class=inst.Instrumentation)
+    source_galaxy_prior = galaxy_prior.GalaxyPrior(pixelization=pixelization.VoronoiPixelization)
 
-    mapper_2h.pixelization.regularization = prior_result_2.pixelization.regularization
+    optimizer_2h.add("source_galaxy", source_galaxy_prior)
+    optimizer_2h.add("instrumentation", inst.Instrumentation)
 
-    result_2h = initialization_2h.run(image, mask, lens_galaxies=results.lens_galaxies,
-                                      source_galaxies=results.source_galaxies)
-    # prior_result_2h = mapper_1.prior_results_for_gaussian_tuples(optimizer_1.compute_gaussian_priors(SIGMA_LIMIT))
+    source_galaxy_prior.pixelization.regularization = result_2.priors.pixelization.regularization
+
+    result_2h = analysis.analyse(image, mask, optimizer_2h, lens_galaxies=result_2.lens_galaxies)
+
     results.append(result_2h)
 
     """
     a) Mass: SPLE+Shear (priors from Init phase 2)
        Source: 'noisy' pixelization (Fixed to init 2H hyper-parameters)
     """
-    mapper_a = mm.ModelMapper()
-    optimizer_a = non_linear.MultiNest(mapper_a)
+    optimizer_a = non_linear.MultiNest()
 
-    lens_galaxy_prior = galaxy_prior.GalaxyPrior("lens_galaxy_prior", mapper_a,
-                                                 spherical_power_law_mass_profile=mass_profiles.SphericalPowerLaw,
+    lens_galaxy_prior = galaxy_prior.GalaxyPrior(spherical_power_law_mass_profile=mass_profiles.SphericalPowerLaw,
                                                  shear_mass_profile=mass_profiles.ExternalShear)
 
-    lens_galaxy_prior.override_prior_models(shear_mass_profile=prior_result_2.shear_mass_profile)
-    lens_galaxy_prior.spherical_power_law_mass_profile.centre = prior_result_2.spherical_mass_profile.centre
+    optimizer_a.add("lens_galaxy", lens_galaxy_prior)
 
-    main_analysis = analysis.Analysis(mapper_a, non_linear_optimizer=optimizer_a,
-                                      lens_galaxy_priors=[lens_galaxy_prior])
-    result_a = main_analysis.run(image, mask, instrumentation=result_2h.instrumentation,
-                                 pixelization=result_2h.pixelization)
+    lens_galaxy_prior.shear_mass_profile = result_2.prior.shear_mass_profile
+    lens_galaxy_prior.spherical_power_law_mass_profile.centre = result_2.prior.spherical_mass_profile.centre
+
+    result_a = analysis.analyse(image, mask, optimizer_a, instrumentation=result_2h.instrumentation,
+                                pixelization=result_2h.pixelization)
     results.append(result_a)
 
     return results
