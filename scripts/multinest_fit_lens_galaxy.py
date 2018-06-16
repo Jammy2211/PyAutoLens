5949c85a40bd1499eb13806791658134272c7a57
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-06-16 14:57:57 +0100

    classmethods in mask now all return a Mask

--- scripts/multinest_fit_lens_galaxy.py
+++ scripts/multinest_fit_lens_galaxy.py
@@ -19,35 +19,6 @@ import pymultinest
 
 import matplotlib.pyplot as plt
 
-### Simulate Image ###
-
-### Setup mask + grid of this image ###
-
-ma = mask.Mask.circular(shape_arc_seconds=(4.0, 4.0), pixel_scale=0.1, radius_mask=1.6)
-ma = mask.Mask(array=ma, pixel_scale=0.1)
-
-image_grids = grids.GridCoordsCollection.from_mask(mask=ma, grid_size_sub=1, blurring_size=(3, 3))
-mappers = grids.GridMapperCollection.from_mask(mask=ma, blurring_size=(3, 3))
-
-### Setup the ray tracing model, and use to generate the 2D galaxy image ###
-
-gal = galaxy.Galaxy(light_profiles=[lp.EllipticalSersic(centre=(0.0, 0.0), axis_ratio=0.7,
-                                                        phi=90.0, intensity=0.5, effective_radius=1.0,
-                                                        sersic_index=4.0)])
-
-ray_trace = ray_tracing.Tracer(lens_galaxies=[gal], source_galaxies=[], image_plane_grids=image_grids)
-
-grid_galaxy_image = ray_trace.generate_image_of_galaxy_light_profiles()
-galaxy_image = mappers.data_to_pixel.map_to_2d(grid_galaxy_image)
-
-# plt.imshow(galaxy_image)
-# plt.show()
-
-### Setup the image as an image.
-
-sim_image = image.Image.simulate(array=galaxy_image)
-
-
 ### Fit Image With Same Model ###
 
 sim_grid_datas = grids.GridDataCollection.from_mask(mask=ma, image=sim_image, noise=0.1*np.ones(ma.shape),
