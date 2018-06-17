a67ec5ff3c6a25fab0782c17c368b50e5d1c5c35
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-06-17 10:57:03 +0100

    Updated local scripts for MultiNest use

--- scripts/multinest_fit_lens_sersic.py
+++ scripts/multinest_fit_lens_sersic.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append("../")
+import os
 
+from auto_lens.imaging import scaled_array
 from auto_lens.imaging import image
 from auto_lens.imaging import mask
 from auto_lens.imaging import grids
@@ -19,19 +21,29 @@ import pymultinest
 
 import matplotlib.pyplot as plt
 
-### Fit Image With Same Model ###
+lens_name = 'lens_sersic'
+data_dir = "../data/"+lens_name.format(os.path.dirname(os.path.realpath(__file__)))
 
-sim_grid_datas = grids.GridDataCollection.from_mask(mask=ma, image=sim_image, noise=0.1*np.ones(ma.shape),
-                                                    exposure_time=np.ones(ma.shape))
-mappers = grids.GridMapperCollection.from_mask(mask=ma)
+im = scaled_array.ScaledArray.from_fits(file_path=data_dir+'/image', hdu=0, pixel_scale=0.1)
+noise = scaled_array.ScaledArray.from_fits(file_path=data_dir+'/noise', hdu=0, pixel_scale=0.1)
+exposure_time = scaled_array.ScaledArray.from_fits(file_path=data_dir+'/exposure_time', hdu=0,
+                                                   pixel_scale=0.1)
+psf = image.PSF.from_fits(file_path=data_dir+'/psf', hdu=0, pixel_scale=0.1)
 
-frame = frame_convolution.FrameMaker(mask=ma)
-convolver = frame.convolver_for_kernel_shape(kernel_shape=(3, 3),
-                                             blurring_region_mask=ma.compute_blurring_mask(kernal_shape=(3, 3)))
-# This PSF leads to no blurring, so equivalent to being off.
-kernel_convolver = convolver.convolver_for_kernel(kernel=np.array([[0., 0., 0.],
-                                                                   [0., 1., 0.],
-                                                                   [0., 0., 0.]]))
+data = image.Image(array=im, effective_exposure_time=exposure_time, pixel_scale=0.1, psf=psf,
+                   background_noise=noise, poisson_noise=noise)
+
+msk = mask.Mask.circular(shape_arc_seconds=data.shape_arc_seconds, pixel_scale=data.pixel_scale, radius_mask=2.0)
+
+grid_coords = grids.GridCoordsCollection.from_mask(mask=msk, grid_size_sub=1, blurring_size=psf.shape)
+grid_data = grids.GridDataCollection.from_mask(mask=msk, image=data, noise=data.background_noise,
+                                               exposure_time=data.effective_exposure_time)
+mappers = grids.GridMapperCollection.from_mask(mask=msk)
+
+frame = frame_convolution.FrameMaker(mask=msk)
+convolver = frame.convolver_for_kernel_shape(kernel_shape=data.psf.shape,
+                                             blurring_region_mask=msk.compute_blurring_mask(kernal_shape=data.psf.shape))
+kernel_convolver = convolver.convolver_for_kernel(kernel=data.psf)
 
 # NON LINEAR ANALYSIS #
 
@@ -48,19 +60,19 @@ def prior(cube, ndim, n_params, model_mapper):
 
     return cube
 
-def likelihood(physical_cube, ndim, n_params, model_mapper, grid_datas, grid_mappers, kernel_convolver):
+def likelihood(physical_cube, ndim, n_params, model_mapper, grid_coords, grid_data, grid_mappers, kernel_convolver):
 
     physical_model = model_mapper.from_physical_vector(physical_cube)
 
     gal = galaxy.Galaxy(light_profiles=[physical_model.light_profile])
-    ray_trace = ray_tracing.Tracer(lens_galaxies=[gal], source_galaxies=[], image_plane_grids=image_grids)
+    ray_trace = ray_tracing.Tracer(lens_galaxies=[gal], source_galaxies=[], image_plane_grids=grid_coords)
 
-    return fitting.fit_data_with_model(grid_datas=grid_datas, grid_mappers=grid_mappers,
+    return fitting.fit_data_with_model(grid_data=grid_data, grid_mappers=grid_mappers,
                                        kernel_convolver=kernel_convolver, tracer=ray_trace)
 
 prior_pass = partial(prior, model_mapper=model_map)
-likelihood_pass = partial(likelihood, model_mapper=model_map, grid_datas=sim_grid_datas, grid_mappers=mappers,
-                          kernel_convolver=kernel_convolver)
+likelihood_pass = partial(likelihood, model_mapper=model_map, grid_coords=grid_coords, grid_data=grid_data,
+                          grid_mappers=mappers, kernel_convolver=kernel_convolver)
 
 pymultinest.run(likelihood_pass, prior_pass, n_dims=multinest.total_parameters, n_params=multinest.total_parameters,
                 n_clustering_params=None, wrapped_params=None, importance_nested_sampling=True, multimodal=True,
