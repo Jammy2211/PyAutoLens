accea0f8bdc2ffdc713472647c65d97ccde2128d
Richard Hayes <richardhayes777@gmail.com>
2018-06-21 17:03:46 +0100

    fixed image setup

--- scripts/pipeline_source_only.py
+++ scripts/pipeline_source_only.py
@@ -12,16 +12,16 @@ import os
 lens_name = 'source_sersic'
 data_dir = "../data/" + lens_name.format(os.path.dirname(os.path.realpath(__file__)))
 
-image = scaled_array.ScaledArray.from_fits(file_path=data_dir + '/image', hdu=0, pixel_scale=0.1)
+data = scaled_array.ScaledArray.from_fits(file_path=data_dir + '/image', hdu=0, pixel_scale=0.1)
 noise = scaled_array.ScaledArray.from_fits(file_path=data_dir + '/noise', hdu=0, pixel_scale=0.1)
 exposure_time = scaled_array.ScaledArray.from_fits(file_path=data_dir + '/exposure_time', hdu=0,
                                                    pixel_scale=0.1)
 psf = im.PSF.from_fits(file_path=data_dir + '/psf', hdu=0, pixel_scale=0.1)
 
-data = im.Image(array=image, effective_exposure_time=exposure_time, pixel_scale=0.1, psf=psf,
-                background_noise=noise, poisson_noise=noise)
+image = im.Image(array=data, effective_exposure_time=exposure_time, pixel_scale=0.1, psf=psf,
+                 background_noise=noise, poisson_noise=noise)
 
-mask = msk.Mask.circular(shape_arc_seconds=data.shape_arc_seconds, pixel_scale=data.pixel_scale, radius_mask=2.0)
+mask = msk.Mask.circular(shape_arc_seconds=image.shape_arc_seconds, pixel_scale=data.pixel_scale, radius_mask=2.0)
 
 # Run the primary pipeline
 results = pipeline.source_only_pipeline(image, mask)
