d6f1cddc7244712e2f9cd4b36ca3c28df5c9640a
Richard Hayes <richardhayes777@gmail.com>
2018-07-10 13:23:01 +0100

    to_eccentric_radius

--- test/profiling/intensity.py
+++ test/profiling/intensity.py
@@ -1,9 +1,13 @@
 import inspect
 import numpy as np
 import os
-from src.profiles import light_profiles
+import time
+from src.profiles import light_profiles, geometry_profiles
+from numpy.testing import assert_almost_equal
 
 path = os.path.dirname(os.path.realpath(__file__))
+
+repeats = 100
 
 
 def load(name):
@@ -31,20 +35,55 @@ def all_light_profiles(func):
     return wrapper
 
 
-def classic_method(profile):
+def tick_toc(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = None
+        for _ in range(repeats):
+            result = func(*args, **kwargs)
+        diff = time.time() - start
+        print("{}: {}".format(func.__name__, diff))
+        return result
+
+    return wrapper
+
+
+def classic_method(func):
     grid_values = np.zeros(grid.shape)
 
     for pixel_no, coordinate in enumerate(grid):
-        grid_values[pixel_no] = profile.intensity_at_coordinates(coordinate)
+        grid_values[pixel_no] = func(coordinate)
 
     return grid_values
 
 
+def grid_to_eccentric_radius():
+    profile = geometry_profiles.EllipticalProfile()
+
+    @tick_toc
+    def classic_grid_to_eccentric_radius():
+        grid_values = np.zeros(grid.shape[0])
+
+        for pixel_no, coordinate in enumerate(grid):
+            grid_values[pixel_no] = profile.coordinates_to_eccentric_radius(coordinate)
+
+        return grid_values
+
+    @tick_toc
+    def new_grid_to_eccentric_radius():
+        return profile.grid_to_eccentric_radius(grid)
+
+    classic = classic_grid_to_eccentric_radius()
+    new = new_grid_to_eccentric_radius()
+
+    assert_almost_equal(classic, new)
+
+
 @all_light_profiles
 def save_light_profile_intensity(profile):
-    result = classic_method(profile)
+    result = classic_method(profile.intensity_at_coordinates)
     save(profile.__class__.__name__, result)
 
 
 if __name__ == "__main__":
-    save_light_profile_intensity()
+    grid_to_eccentric_radius()
