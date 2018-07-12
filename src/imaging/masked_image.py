a84d5e72d89c4c06dc3d14e4f57709d78fe4299a
Richard Hayes <richardhayes777@gmail.com>
2018-07-12 14:59:22 +0100

    fixing integration test...

--- src/imaging/masked_image.py
+++ src/imaging/masked_image.py
@@ -3,12 +3,12 @@ from src.imaging import image as im
 
 class MaskedImage(im.AbstractImage):
     def __init__(self, mask, image):
-        super().__init__(image,
-                         mask.masked_1d_array_from_2d_array(image.effective_exposure_time),
-                         image.pixel_scale,
-                         image.psf,
-                         mask.masked_1d_array_from_2d_array(image.background_noise),
-                         mask.masked_1d_array_from_2d_array(image.poisson_noise))
+        super().__init__(array=mask.masked_1d_array_from_2d_array(image),
+                         effective_exposure_time=mask.masked_1d_array_from_2d_array(image.effective_exposure_time),
+                         pixel_scale=image.pixel_scale,
+                         psf=image.psf,
+                         background_noise=mask.masked_1d_array_from_2d_array(image.background_noise),
+                         poisson_noise=mask.masked_1d_array_from_2d_array(image.poisson_noise))
         self.border_pixel_indices = mask.border_pixel_indices
         self.coordinate_grid = mask.coordinate_grid
         self.blurring_mask = mask.blurring_mask_for_kernel_shape(image.psf.shape)
