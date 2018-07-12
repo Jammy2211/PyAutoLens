386e34df8166159b51ad77e31ee68bd23f2cbe59
Richard Hayes <richardhayes777@gmail.com>
2018-07-12 15:09:48 +0100

    fixed integration test

--- src/imaging/masked_image.py
+++ src/imaging/masked_image.py
@@ -1,9 +1,13 @@
 from src.imaging import image as im
+import numpy as np
 
 
 class MaskedImage(im.AbstractImage):
-    def __init__(self, mask, image):
-        super().__init__(array=mask.masked_1d_array_from_2d_array(image),
+    def __new__(cls, image, mask):
+        return np.array(mask.masked_1d_array_from_2d_array(image),).view(cls)
+
+    def __init__(self, image, mask):
+        super().__init__(array=image,
                          effective_exposure_time=mask.masked_1d_array_from_2d_array(image.effective_exposure_time),
                          pixel_scale=image.pixel_scale,
                          psf=image.psf,
