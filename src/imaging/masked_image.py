2ee4fac385f60e4c99cfcb6d7095fe50eef02ba2
Richard Hayes <richardhayes777@gmail.com>
2018-07-20 12:28:48 +0100

    more docs

--- src/imaging/masked_image.py
+++ src/imaging/masked_image.py
@@ -1,4 +1,5 @@
 from src.imaging import image as im
+from src.imaging import mask as msk
 from src.pixelization import frame_convolution
 import numpy as np
 
@@ -8,12 +9,24 @@ class MaskedImage(im.AbstractImage):
         return np.array(mask.map_to_1d(image), ).view(cls)
 
     def __init__(self, image, mask):
+        """
+        An image that has been masked. Only data within the mask is kept. This data is kept in 1D with a corresponding
+        array mapping data back to 2D.
+
+        Parameters
+        ----------
+        image: im.Image
+            A 2D image
+        mask: msk.Mask
+            A mask to be applied to the image
+        """
         super().__init__(array=image,
                          effective_exposure_time=mask.map_to_1d(image.effective_exposure_time),
                          pixel_scale=image.pixel_scale,
                          psf=image.psf,
                          background_noise=mask.map_to_1d(image.background_noise),
                          poisson_noise=mask.map_to_1d(image.poisson_noise))
+        #  TODO: Kernel convolver and blurring mask are here. Should they be?
         self.border_pixel_indices = mask.border_pixel_indices
         self.coordinate_grid = mask.coordinate_grid
         self.blurring_mask = mask.blurring_mask_for_kernel_shape(image.psf.shape)
