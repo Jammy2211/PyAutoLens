d1bcfc764d0b3514d502d9937d41eeca43a49624
Richard Hayes <richardhayes777@gmail.com>
2018-07-23 14:55:17 +0100

    working on analysis/customize paradigm

--- src/pipeline/phase.py
+++ src/pipeline/phase.py
@@ -6,28 +6,39 @@ from src.analysis import fitting
 
 
 class Phase(object):
-    def __init__(self, optimizer, sub_grid_size=1, blurring_shape=None):
+    def __init__(self, optimizer, sub_grid_size, blurring_shape):
         self.optimizer = optimizer
-        self.last_results = None
-        self.masked_image = None
-        self.coords_collection = None
         self.sub_grid_size = sub_grid_size
-        self.__blurring_shape = blurring_shape
-
-    @property
-    def blurring_shape(self):
-        return self.__blurring_shape \
-            if self.__blurring_shape is not None or self.masked_image is None else self.masked_image.psf.shape
-
-    @blurring_shape.setter
-    def blurring_shape(self, blurring_shape):
-        self.__blurring_shape = blurring_shape
+        self.blurring_shape = blurring_shape
 
     def run(self, **kwargs):
-        self.last_results = kwargs["last_results"]
-        self.masked_image = kwargs["masked_image"]
-        self.coords_collection = msk.CoordinateCollection.from_mask_subgrid_size_and_blurring_shape(
-            self.masked_image.mask, self.sub_grid_size, self.blurring_shape)
+        last_result = kwargs["last_result"]
+        kwargs["masked_image"] = self.customize_image(kwargs["masked_image"], last_result)
+        analysis = self.__class__.Analysis(**kwargs)
+        return self.optimizer.fit(analysis)
+
+    @property
+    def constant(self):
+        return self.optimizer.constant
+
+    @property
+    def variable(self):
+        return self.optimizer.variable
+
+    class Analysis(object):
+        def __init__(self, **kwargs):
+            self.last_results = kwargs["last_results"]
+            self.masked_image = kwargs["masked_image"]
+            self.coords_collection = msk.CoordinateCollection.from_mask_subgrid_size_and_blurring_shape(
+                self.masked_image.mask, kwargs["sub_grid_size"],
+                kwargs["blurring_shape"] if "blurring_shape" in kwargs else self.masked_image.psf.shape)
+
+        def fit(self, **kwargs):
+            raise NotImplementedError()
+
+    # noinspection PyMethodMayBeStatic,PyUnusedLocal
+    def customize_image(self, masked_image, last_result):
+        return masked_image
 
 
 class SourceLensPhase(Phase):
@@ -43,10 +54,11 @@ class SourceLensPhase(Phase):
         elif isinstance(lens_galaxy, galaxy_prior.GalaxyPrior):
             self.optimizer.variable.lens_galaxy = lens_galaxy
 
-    def run(self, **kwargs):
-        super(SourceLensPhase, self).run(**kwargs)
 
-    def fit(self, **kwargs):
-        tracer = ray_tracing.Tracer([kwargs["lens_galaxy"]], kwargs["source_galaxy"], self.coords_collection)
-        fitter = fitting.Fitter(self.masked_image, tracer)
-        return fitter.fit_data_with_profiles()
+class InitialSourceLensPhase(SourceLensPhase):
+    class Analysis(Phase.Analysis):
+
+        def fit(self, **kwargs):
+            tracer = ray_tracing.Tracer([kwargs["lens_galaxy"]], kwargs["source_galaxy"], self.coords_collection)
+            fitter = fitting.Fitter(self.masked_image, tracer)
+            return fitter.fit_data_with_profiles()
