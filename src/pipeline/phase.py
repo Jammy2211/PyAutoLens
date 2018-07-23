1b8d36b1b732be4537f6a1b02ea4b7cb153a4711
Richard Hayes <richardhayes777@gmail.com>
2018-07-23 14:08:03 +0100

    more tests, using real mask object...

--- src/pipeline/phase.py
+++ src/pipeline/phase.py
@@ -1,18 +1,35 @@
 from src.analysis import galaxy_prior
 from src.analysis import galaxy
+from src.analysis import ray_tracing
+from src.imaging import mask as msk
 
 
 class Phase(object):
-    def __init__(self, optimizer, sub_grid_size=1, blurring_shape=(1, 1)):
+    def __init__(self, optimizer, sub_grid_size=1, blurring_shape=None):
         self.optimizer = optimizer
         self.last_results = None
         self.masked_image = None
+        self.coords_collection = None
         self.sub_grid_size = sub_grid_size
-        self.blurring_shape = blurring_shape
+        self.__blurring_shape = blurring_shape
 
-    def run(self, masked_image, last_results):
-        self.last_results = last_results
-        self.masked_image = masked_image
+    @property
+    def blurring_shape(self):
+        return self.__blurring_shape \
+            if self.__blurring_shape is not None or self.masked_image is None else self.masked_image.psf.shape
+
+    @blurring_shape.setter
+    def blurring_shape(self, blurring_shape):
+        self.__blurring_shape = blurring_shape
+
+    def run(self, **kwargs):
+        self.last_results = kwargs["last_results"]
+        self.masked_image = kwargs["masked_image"]
+        self.coords_collection = msk.CoordinateCollection.from_mask_subgrid_size_and_blurring_shape(
+            self.masked_image.mask, self.sub_grid_size, self.blurring_shape)
+
+    def fit(self, **kwargs):
+        tracer = ray_tracing.Tracer([kwargs["lens_galaxy"]], kwargs["source_galaxy"], self.coords_collection)
 
 
 class SourceLensPhase(Phase):
@@ -27,3 +44,6 @@ class SourceLensPhase(Phase):
             self.optimizer.constant.lens_galaxy = lens_galaxy
         elif isinstance(lens_galaxy, galaxy_prior.GalaxyPrior):
             self.optimizer.variable.lens_galaxy = lens_galaxy
+
+    def run(self, **kwargs):
+        super(SourceLensPhase, self).run(**kwargs)
