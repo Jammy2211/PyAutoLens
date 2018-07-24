90bbf0882e4d2b1aafb54657046c7a5c9083eb59
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-07-24 13:39:50 +0100

    Jitted frame convolver.

--- test/profiling/frame_convolver_mapping_matrix.py
+++ test/profiling/frame_convolver_mapping_matrix.py
@@ -4,8 +4,6 @@ sys.path.append("../../")
 import numpy as np
 from src.imaging import mask
 from src.pixelization import frame_convolution
-from src.analysis import galaxy
-from src.profiles import mass_profiles
 import time
 import os
 import numba
@@ -17,17 +15,20 @@ def load(name):
 
 grid = load("deflection_data/grid")
 
-psf_shape = (21, 21)
+psf_shape = (11, 11)
 
-ma = mask.Mask.for_simulate(shape_arc_seconds=(4.0, 4.0), pixel_scale=0.05, psf_size=psf_shape)
+ma = mask.Mask.for_simulate(shape_arc_seconds=(4.0, 4.0), pixel_scale=0.1, psf_size=psf_shape)
 
-grid_data = ma.data_collection_from_image_noise_and_exposure_time(image=np.ones(ma.shape), noise=np.ones(ma.shape),
-                                                                   exposure_time=np.ones(ma.shape))
+data = ma.masked_1d_array_from_2d_array(np.ones(ma.shape))
+
+mapping = np.ones((len(data), 60))
+
 frame = frame_convolution.FrameMaker(mask=ma)
 convolver = frame.convolver_for_kernel_shape(kernel_shape=psf_shape)
 # This PSF leads to no blurring_coords, so equivalent to being off.
 kernel_convolver = convolver.convolver_for_kernel(kernel=np.ones(psf_shape))
 
+kernel_convolver.convolve_mapping_matrix_jit(mapping)
 repeats = 1
 
 def tick_toc(func):
@@ -44,7 +45,13 @@ def tick_toc(func):
 @tick_toc
 def current_solution():
 
-    kernel_convolver.convolve_array(grid_data.image)
+    kernel_convolver.convolve_mapping_matrix(mapping)
+
+
+@tick_toc
+def jitted_solution():
+    kernel_convolver.convolve_mapping_matrix_jit(mapping)
 
 if __name__ == "__main__":
     current_solution()
+    jitted_solution()
