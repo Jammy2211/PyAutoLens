5956d346c13ccb5f916990449bca4ac17ef1db66
Richard Hayes <richardhayes777@gmail.com>
2018-07-31 11:18:15 +0100

    fixed pipeline

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -91,7 +91,7 @@ def make_profile_pipeline():
             return masked_image - previous_results.last.lens_galaxy_image
 
         def pass_priors(self, previous_results):
-            self.lens_galaxy.sie.centre = previous_results.variable.elliptical_sersic.centre
+            self.lens_galaxy.sie.centre = previous_results.last.variable.elliptical_sersic.centre
 
     # 2) Lens Light : None
     #    Mass: SIE (use lens light profile centre from previous phase as prior on mass profile centre)
