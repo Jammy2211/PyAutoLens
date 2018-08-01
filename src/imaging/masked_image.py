285b7a1244e6083f6c42f4b8fe10badbe6d2e642
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-08-01 11:28:54 +0100

    Refactored masked image to inclue grids / borders

--- src/imaging/masked_image.py
+++ src/imaging/masked_image.py
@@ -4,11 +4,11 @@ from src.imaging import convolution
 import numpy as np
 
 
-class MaskedImage(im.AbstractImage):
-    def __new__(cls, image, mask):
-        return np.array(mask.masked_1d_array_from_2d_array(image)).view(cls)
+class MaskedImage(im.Image):
+    def __new__(cls, image, mask, sub_grid_size=2, profile_psf_shape=None, pixelization_psf_shape=None):
+        return np.array(mask.map_to_1d(image)).view(cls)
 
-    def __init__(self, image, mask):
+    def __init__(self, image, mask, sub_grid_size=2, profile_psf_shape=None, pixelization_psf_shape=None):
         """
         An image that has been masked. Only data within the mask is kept. This data is kept in 1D with a corresponding
         array mapping data back to 2D.
@@ -19,29 +19,42 @@ class MaskedImage(im.AbstractImage):
             A 2D image
         mask: msk.Mask
             A mask to be applied to the image
-        """
-        super().__init__(array=image, pixel_scale=image.pixel_scale,
-                         noise=mask.masked_1d_array_from_2d_array(image.noise),
-                         psf=image.psf)
+        sub_grid_size : int
 
-        self.coordinate_grid = mask.coordinate_grid
+        """
+        super().__init__(array=image, pixel_scale=image.pixel_scale, noise=mask.map_to_1d(image.noise), psf=image.psf)
+
         self.image = image
         self.image_shape = image.shape
         self.mask = mask
-        self.blurring_mask = mask.blurring_mask_for_kernel_shape(image.psf.shape)
-        self.convolver_image = convolution.ConvolverImage(self.mask, self.blurring_mask, self.image.psf)
-        self.convolver_mapping_matrix = convolution.ConvolverMappingMatrix(self.mask, self.image.psf)
+
+        if profile_psf_shape is None:
+            profile_psf_shape = self.image.psf.shape
+        if pixelization_psf_shape is None:
+            pixelization_psf_shape = self.image.psf.shape
+
+        self.blurring_mask = mask.blurring_mask_for_kernel_shape(profile_psf_shape)
+        self.convolver_image = convolution.ConvolverImage(self.mask,
+                                                          self.blurring_mask, self.image.psf.trim(profile_psf_shape))
+        self.convolver_mapping_matrix = convolution.ConvolverMappingMatrix(self.mask,
+                                                                           self.image.psf.trim(pixelization_psf_shape))
+        self.grids = msk.GridCollection.from_mask_sub_grid_size_and_blurring_shape(mask=mask, sub_grid_size=sub_grid_size,
+                                                                                   blurring_shape=profile_psf_shape)
+
+        self.borders = msk.BorderCollection.from_mask_and_sub_grid_size(mask=mask, sub_grid_size=sub_grid_size)
 
     def __array_finalize__(self, obj):
         super(MaskedImage, self).__array_finalize__(obj)
         if isinstance(obj, MaskedImage):
-            self.coordinate_grid = obj.coordinate_grid
             self.image = obj.image
             self.image_shape = obj.image_shape
             self.mask = obj.mask
             self.blurring_mask = obj.blurring_mask
             self.convolver_image = obj.convolver_image
             self.convolver_mapping_matrix = obj.convolver_mapping_matrix
+            self.grids = obj.grids
+            self.borders = obj.borders
+
 
 
     def map_to_2d(self, data):
--- test/imaging/test_masked_image.py
+++ test/imaging/test_masked_image.py
@@ -9,19 +9,19 @@ import pytest
 @pytest.fixture(name='image')
 def make_image():
     psf = im.PSF(np.ones((1, 1)), 1)
-    return im.Image(np.ones((3, 3)), pixel_scale=1., psf=psf, noise=np.ones((3, 3)))
+    return im.Image(np.ones((3, 3)), pixel_scale=3., psf=psf, noise=np.ones((3, 3)))
 
 
 @pytest.fixture(name="mask")
 def make_mask():
     return msk.Mask(np.array([[True, False, True],
                               [False, False, False],
-                              [True, False, True]]))
+                              [True, False, True]]), pixel_scale=3.0)
 
 
 @pytest.fixture(name="sparse_mask")
 def make_sparse_mask(mask):
-    return msk.SparseMask(mask, 1)
+    return msk.SparseMask(mask, 1.0)
 
 
 @pytest.fixture(name="masked_image")
@@ -37,9 +37,21 @@ class TestMaskedImage(object):
     def test_masking(self, masked_image):
         assert masked_image.noise.shape == (5,)
 
-    def test_coordinate_grid(self, masked_image):
-        assert masked_image.coordinate_grid.shape == (5, 2)
-        assert (masked_image.coordinate_grid == np.array([[-1, 0], [0, -1], [0, 0], [0, 1], [1, 0]])).all()
+    def test_grids(self, masked_image):
+
+        assert masked_image.grids.image.shape == (5, 2)
+
+        assert (masked_image.grids.image == np.array([[-3, 0], [0, -3], [0, 0], [0, 3], [3, 0]])).all()
+        assert (masked_image.grids.sub == np.array([[-3.5, -0.5], [-3.5, 0.5], [-2.5, -0.5], [-2.5, 0.5],
+                                                    [-0.5, -3.5], [-0.5, -2.5], [0.5, -3.5], [0.5, -2.5],
+                                                    [-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5],
+                                                    [-0.5, 2.5], [-0.5, 3.5], [0.5, 2.5], [0.5, 3.5],
+                                                    [2.5, -0.5], [2.5, 0.5], [3.5, -0.5], [3.5, 0.5]])).all()
+
+    def test_borders(self, masked_image):
+
+        assert (masked_image.borders.image == np.array([0, 1, 2, 3, 4])).all()
+        assert (masked_image.borders.sub == np.array([0, 4, 8, 13, 18])).all()
 
     def test_blurring_mask(self, masked_image):
         assert masked_image.blurring_mask.all()
