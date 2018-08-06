677d9c0a400231452b83172bfd5dc92242ff396c
Richard Hayes <richardhayes777@gmail.com>
2018-08-06 17:04:22 +0100

    trying to extend cli code...

--- autolens/commands/base.py
+++ autolens/commands/base.py
@@ -11,3 +11,26 @@ class Base(object):
 
     def run(self):
         raise NotImplementedError('You must implement the run() method yourself!')
+
+
+def await_input():
+    import termios, fcntl, sys, os
+    fd = sys.stdin.fileno()
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    try:
+        while 1:
+            try:
+                c = sys.stdin.read(1)
+                return repr(c)
+            except IOError: pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
