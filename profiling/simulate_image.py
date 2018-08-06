4a3b8843e2d77cfd5713f08e87d65abc2c4b675d
Richard Hayes <richardhayes777@gmail.com>
2018-08-06 15:08:00 +0100

    removed tools package as it seems to conflict with packaging process

--- profiling/simulate_image.py
+++ profiling/simulate_image.py
@@ -10,7 +10,7 @@ from autolens.profiles import light_profiles as lp
 from autolens.profiles import mass_profiles as mp
 from autolens.analysis import ray_tracing
 from autolens.analysis import galaxy
-from autolens.tools import arrays
+from imaging import array_util
 
 path =  "{}/".format(os.path.dirname(os.path.realpath(__file__)))
 output_path = "{}/../profiling/data/".format(os.path.dirname(os.path.realpath(__file__)))
@@ -66,7 +66,7 @@ sim_image = image.Image.simulate(array=galaxy_image_2d, effective_exposure_time=
 if os.path.exists(output_path + lens_name) == False:
     os.makedirs(output_path + lens_name)
 
-arrays.numpy_array_to_fits(sim_image, file_path=output_path + lens_name + 'image')
-arrays.numpy_array_to_fits(np.ones(sim_image.shape), file_path=output_path + lens_name + 'noise')
-arrays.numpy_array_to_fits(np.ones(sim_image.shape), file_path=output_path + lens_name + 'exposure_time')
-arrays.numpy_array_to_fits(psf, file_path=output_path + lens_name + '/psf')
+array_util.numpy_array_to_fits(sim_image, file_path=output_path + lens_name + 'image')
+array_util.numpy_array_to_fits(np.ones(sim_image.shape), file_path=output_path + lens_name + 'noise')
+array_util.numpy_array_to_fits(np.ones(sim_image.shape), file_path=output_path + lens_name + 'exposure_time')
+array_util.numpy_array_to_fits(psf, file_path=output_path + lens_name + '/psf')
