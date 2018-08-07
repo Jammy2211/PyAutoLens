7646644e4b2be8563ba7def2e1a6e88e54242efe
Richard Hayes <richardhayes777@gmail.com>
2018-08-07 10:57:33 +0100

    nicely working config util

--- autolens/commands/download_config.py
+++ autolens/commands/download_config.py
@@ -11,6 +11,8 @@ class DownloadConfig(Base):
     """Reset Config!"""
 
     def run(self):
-        if input("Are you sure? This will reset the state of your config").lower() == 'y':
-            conf.remove_config()
-            conf.download_config()
+        if conf.is_config(current_directory):
+            if input("Are you sure? This will reset the state of your config. (y/n)\n").lower() != 'y':
+                return
+            conf.remove_config(current_directory)
+        conf.download_config(current_directory)
