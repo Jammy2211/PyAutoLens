c6a092ada608b3e9336a53db84f6747056f762c5
Richard Hayes <richardhayes777@gmail.com>
2018-08-07 11:39:46 +0100

    fixed cli by moving import]a

--- autolens/commands/pipeline.py
+++ autolens/commands/pipeline.py
@@ -1,16 +1,16 @@
 from autolens.commands.base import Base
-from autolens import pipeline
 
 
 class Pipeline(Base):
 
     def run(self):
+        from autolens import pipeline
         name = self.options['<name>']
         if name is not None:
             try:
                 self.run_pipeline(pipeline.pipeline_dict[name])
             except KeyError:
-                print("No pipeline called {} found".format(name))
+                print("No pipeline called '{}' found".format(name))
         else:
             print("Available Pipelines:")
             print("\n".join(list(pipeline.pipeline_dict.keys())))
