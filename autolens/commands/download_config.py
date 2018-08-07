d96d5837e5835bd315f82862a37fd49a7f18ebf3
Richard Hayes <richardhayes777@gmail.com>
2018-08-07 13:46:33 +0100

    improving config paths, added example

--- autolens/commands/download_config.py
+++ autolens/commands/download_config.py
@@ -3,13 +3,15 @@
 from .base import Base, current_directory
 from autolens import conf
 
+config_path = "{}/config".format(current_directory)
+
 
 class DownloadConfig(Base):
     """Reset Config!"""
 
     def run(self):
-        if conf.is_config(current_directory):
+        if conf.is_config(config_path):
             if input("Are you sure? This will reset the state of your config. (y/n)\n").lower() != 'y':
                 return
-            conf.remove_config(current_directory)
+            conf.remove_config(config_path)
         conf.download_config(current_directory)
