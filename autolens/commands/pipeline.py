de91a71cc7cc02cc9686af43fc6bf28ce6a8ec0d
Richard Hayes <richardhayes777@gmail.com>
2018-08-07 11:34:01 +0100

    shuffling pipeline code around for faster cli response

--- autolens/commands/pipeline.py
+++ autolens/commands/pipeline.py
@@ -4,13 +4,17 @@ from autolens.commands.base import Base
 class Pipeline(Base):
 
     def run(self):
-        print(self.options)
-        print("Available Pipelines:")
         from autolens.pipeline import pipeline
+        pipelines = {value.name: value for value in pipeline.__dict__.values() if isinstance(value, pipeline.Pipeline)}
+        name = self.options['<name>']
+        if name is not None:
+            try:
+                self.run_pipeline(pipelines[name])
+            except KeyError:
+                print("No pipeline called {} found".format(name))
+        else:
+            print("Available Pipelines:")
+            print("\n".join(list(pipelines.keys())))
 
-        pipelines = [value for value in pipeline.__dict__.values() if isinstance(value, pipeline.Pipeline)]
-        print("\n".join([p.name for p in pipelines]))
-
-
-if __name__ == "__main__":
-    Pipeline(None).run()
+    def run_pipeline(self, pipeline):
+        pass
