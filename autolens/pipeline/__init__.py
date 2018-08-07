95a967f7334aab429e75aa90a0630109c527bdeb
Richard Hayes <richardhayes777@gmail.com>
2018-08-07 15:56:22 +0100

    displaying coloured docs

--- autolens/pipeline/__init__.py
+++ autolens/pipeline/__init__.py
@@ -1,11 +1,19 @@
 from autolens.pipeline import profile_pipeline
 from autolens.pipeline import source_only_pipeline
+from collections import namedtuple
+
+PipelineTuple = namedtuple("PipelineTuple", ["make", "doc"])
 
 pipeline_dict = {}
 
 
 def add(module):
-    pipeline_dict[module.name] = module.make
+    """
+    Parameters
+    ----------
+    module: {name, make}
+    """
+    pipeline_dict[module.name] = PipelineTuple(module.make, module.__doc__)
 
 
 add(profile_pipeline)
