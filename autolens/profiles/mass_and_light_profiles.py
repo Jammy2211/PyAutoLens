c92b615df1b42716cba01384de132749f01abcaa
Richard Hayes <richardhayes777@gmail.com>
2018-08-13 12:10:34 +0100

    first example of a mass and light profile

--- autolens/profiles/mass_and_light_profiles.py
+++ autolens/profiles/mass_and_light_profiles.py
@@ -1,6 +1,10 @@
 from autolens.profiles import light_profiles, mass_profiles
 
 
-class EllipticalSersicMassAndLight(light_profiles.EllipticalSersicLightProfile, mass_profiles.EllipticalSersicMass):
-    def __init__(self):
-        pass
+class EllipticalSersicMassAndLightProfile(light_profiles.EllipticalSersicLightProfile,
+                                          mass_profiles.EllipticalSersicMass):
+    def __init__(self, centre=(0.0, 0.0), axis_ratio=1.0, phi=0.0, intensity=0.1, effective_radius=0.6,
+                 sersic_index=4.0):
+        super().__init__(centre, axis_ratio, phi, intensity, effective_radius, sersic_index)
+        super(mass_profiles.EllipticalSersicMass, self).__init__(centre, axis_ratio, phi, intensity, effective_radius,
+                                                                 sersic_index)
