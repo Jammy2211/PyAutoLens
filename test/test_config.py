8e392c90ccbba56d17635cdc5ea67be3c6abf1f0
Richard Hayes <richardhayes777@gmail.com>
2018-08-13 10:57:10 +0100

    removed test for hosting of config

--- test/test_config.py
+++ test/test_config.py
@@ -1,11 +1,6 @@
 from autolens import conf
-import requests
 
 
 class TestCase(object):
     def test_is_config(self):
         assert conf.is_config()
-
-    def test_is_hosted(self):
-        with requests.get(conf.CONFIG_URL) as response:
-            assert response.status_code == 200
