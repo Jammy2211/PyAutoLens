526ea8adf631d0b5f014cb3ca61a06fd3b4d23f5
Richard Hayes <richardhayes777@gmail.com>
2018-08-13 12:13:53 +0100

    test for mass profile component

--- test/profiles/test_mass_and_light_profiles.py
+++ test/profiles/test_mass_and_light_profiles.py
@@ -1,5 +1,6 @@
 from autolens.profiles import mass_and_light_profiles
 import pytest
+import numpy as np
 
 
 class TestCase(object):
@@ -11,3 +12,21 @@ class TestCase(object):
 
             intensity = sersic.intensity_at_radius(radius=1.0)
             assert intensity == pytest.approx(0.351797, 1e-3)
+
+        def test__flip_coordinates_lens_center__same_value(self):
+            sersic = mass_and_light_profiles.EllipticalSersicMassAndLightProfile(centre=(0.0, 0.0), axis_ratio=1.0,
+                                                                                 phi=0.0,
+                                                                                 intensity=1.0,
+                                                                                 effective_radius=1.0, sersic_index=4.0)
+
+            defls_0 = sersic.deflections_from_grid(grid=np.array([[1.0, 1.0]]))
+
+            sersic = mass_and_light_profiles.EllipticalSersicMassAndLightProfile(centre=(1.0, 1.0), axis_ratio=1.0,
+                                                                                 phi=0.0,
+                                                                                 intensity=1.0,
+                                                                                 effective_radius=1.0, sersic_index=4.0)
+
+            defls_1 = sersic.deflections_from_grid(grid=np.array([[0.0, 0.0]]))
+
+            assert defls_0[0, 0] == pytest.approx(-defls_1[0, 0], 1e-5)
+            assert defls_0[0, 1] == pytest.approx(-defls_1[0, 1], 1e-5)
