b4104253bc559efbe1035203905b56d6c3f3261b
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-08-21 10:14:36 +0100

    Added lens mass and source profile hyper phase

---
+++ test/integration/lens_mass_source_profile/lens_x1_src_x1_hyper.py
@@ -0,0 +1,86 @@
+from autolens.pipeline import pipeline as pl
+from autolens.pipeline import phase as ph
+from autolens.profiles import light_profiles as lp
+from autolens.profiles import mass_profiles as mp
+from autolens.analysis import galaxy_prior as gp
+from autolens.autopipe import non_linear as nl
+from autolens.autopipe import model_mapper as mm
+from autolens.analysis import galaxy
+from autolens import conf
+from test.integration import tools
+
+import numpy as np
+import shutil
+import os
+
+dirpath = os.path.dirname(os.path.realpath(__file__))
+dirpath = os.path.dirname(dirpath)
+output_path = '/gpfs/data/pdtw24/Lens/int/lens_mass_source_profile/'
+
+def test_lens_x1_src_x1_profile_hyper_pipeline():
+
+    pipeline_name = "l1_s1_hyp"
+    data_name = '/l1_s1_hyp'
+
+    try:
+        shutil.rmtree(dirpath+'/data'+data_name)
+    except FileNotFoundError:
+        pass
+
+    lens_mass = mp.EllipticalIsothermalMP(centre=(0.01, 0.01), axis_ratio=0.8, phi=80.0, einstein_radius=1.6)
+    
+    source_bulge = lp.EllipticalSersicLP(centre=(0.01, 0.01), axis_ratio=0.9, phi=90.0, intensity=1.0,
+                                  effective_radius=1.0, sersic_index=4.0)
+
+    source_disk = lp.EllipticalSersicLP(centre=(0.01, 0.01), axis_ratio=0.6, phi=90.0, intensity=20.0,
+                                 effective_radius=2.5, sersic_index=1.0)
+
+    lens_galaxy = galaxy.Galaxy(sie=lens_mass)
+    source_galaxy = galaxy.Galaxy(bulge=source_bulge, disk=source_disk)
+
+    tools.simulate_integration_image(data_name=data_name, pixel_scale=0.2, lens_galaxies=[lens_galaxy],
+                                     source_galaxies=[source_galaxy], target_signal_to_noise=30.0)
+
+    conf.instance.output_path = output_path
+
+    try:
+        shutil.rmtree(output_path + pipeline_name)
+    except FileNotFoundError:
+        pass
+
+    pipeline = make_lens_x1_src_x1_profile_hyper_pipeline(pipeline_name=pipeline_name)
+    image = tools.load_image(data_name=data_name, pixel_scale=0.2)
+
+    results = pipeline.run(image=image)
+    for result in results:
+        print(result)
+
+def make_lens_x1_src_x1_profile_hyper_pipeline(pipeline_name):
+
+    phase1 = ph.LensMassAndSourceProfilePhase(lens_galaxies=[gp.GalaxyPrior(sie=mp.EllipticalIsothermalMP)],
+                                              source_galaxies=[gp.GalaxyPrior(sersic=lp.EllipticalSersicLP)],
+                                              optimizer_class=nl.MultiNest, phase_name="{}/phase1".format(pipeline_name))
+
+    phase1.optimizer.n_live_points = 60
+    phase1.optimizer.sampling_efficiency = 0.8
+
+    phase1h = ph.LensMassAndSourceProfileHyperOnlyPhase(optimizer_class=nl.MultiNest, 
+                                                        phase_name="{}/phase1h".format(pipeline_name))
+
+    class SourceHyperPhase(ph.LensMassAndSourceProfileHyperPhase):
+        def pass_priors(self, previous_results):
+            phase1_results = previous_results[-1]
+            phase1h_results = previous_results[-1].hyper
+            self.source_galaxies = phase1_results.variable.source_galaxies
+            self.source_galaxies[0].hyper_galaxy = phase1h_results.constant.source_galaxies[0].hyper_galaxy
+
+    phase2 = SourceHyperPhase(optimizer_class=nl.MultiNest, phase_name="{}/phase2".format(pipeline_name))
+
+    phase2.optimizer.n_live_points = 40
+    phase2.optimizer.sampling_efficiency = 0.8
+
+    return pl.Pipeline(pipeline_name, phase1, phase1h, phase2)
+
+
+if __name__ == "__main__":
+    test_lens_x1_src_x1_profile_hyper_pipeline()
