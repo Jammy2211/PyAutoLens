5ed67e216557906cc90dbb6195da899d1f07d0e6
Richard Hayes <richardhayes777@gmail.com>
2018-09-11 12:02:24 +0100

    format and commented out test

--- test/autofit/test_non_linear.py
+++ test/autofit/test_non_linear.py
@@ -314,9 +314,9 @@ class TestNonLinearOptimizer(object):
         def test__1_class__correct_directory(self, mm_config, nlo_setup_path):
             conf.instance.output_path = nlo_setup_path + '1_class'
             mapper = model_mapper.ModelMapper(config=mm_config, mock_class=MockClassNLOx4)
-            nlo = non_linear.NonLinearOptimizer(model_mapper=mapper)
+            non_linear.NonLinearOptimizer(model_mapper=mapper)
 
-            assert os.path.exists(nlo_setup_path + '1_class') == True
+            assert os.path.exists(nlo_setup_path + '1_class')
 
     class TestTotalParameters:
 
@@ -485,7 +485,6 @@ class TestNonLinearOptimizer(object):
 
 
 class TestMultiNest(object):
-
     class TestReadFromSummary:
 
         def test__read_most_probable__1_class_4_params(self, mm_config, mn_summary_path):
@@ -867,53 +866,6 @@ class TestMultiNest(object):
             assert mn.model_at_upper_sigma_limit(sigma_limit=1.0) == pytest.approx([1.07, 2.07, 3.07, 4.07], 1e-2)
             assert mn.model_at_lower_sigma_limit(sigma_limit=1.0) == pytest.approx([0.93, 1.93, 2.93, 3.93], 1e-2)
 
-    # TODO: This test is inconsistent to due floating point error
-    # class TestOutputResults(object):
-    #
-    #     def test__1_class__output_results_which_are_limits_tests_above(self, mm_config, mn_results_path):
-    #         conf.instance.output_path = mn_results_path
-    #
-    #         create_gaussian_prior_summary_4_parameters(path=mn_results_path)
-    #         create_weighted_samples_4_parameters(path=mn_results_path)
-    #
-    #         mapper = model_mapper.ModelMapper(config=mm_config, mock_class=MockClassNLOx4)
-    #         mn = non_linear.MultiNest(model_mapper=mapper)
-    #         mn.save_model_info()
-    #         mn.output_results()
-    #
-    #         results_file = open(mn_results_path + '/mn.results')
-    #
-    #         results = results_file.readlines()
-    #
-    #         assert results[0] == r'Most likely model, Likelihood = 0.02' + '\n'
-    #         assert results[1] == '\n'
-    #         assert results[2] == r'mock_class_one                          9.0' + '\n'
-    #         assert results[3] == r'mock_class_two                          10.0' + '\n'
-    #         assert results[4] == r'mock_class_three                        11.0' + '\n'
-    #         assert results[5] == r'mock_class_four                         12.0' + '\n'
-    #         assert results[6] == '\n'
-    #         assert results[7] == r'Most probable model (3 sigma limits)' + '\n'
-    #         assert results[8] == '\n'
-    #         assert results[
-    #                    9] == r'mock_class_one                          1.0 (0.8802415615355199, 1.1195259985150692)' + '\n'
-    #         assert results[
-    #                    10] == r'mock_class_two                          2.0 (1.8804740014849308, 2.1197584384644803)' + '\n'
-    #         assert results[
-    #                    11] == r'mock_class_three                        3.0 (2.8804740014849326, 3.119758438464474)' + '\n'
-    #         assert results[
-    #                    12] == r'mock_class_four                         4.1 (3.8804740014849326, 4.119758438464474)' + '\n'
-    #         assert results[13] == '\n'
-    #         assert results[14] == r'Most probable model (1 sigma limits)' + '\n'
-    #         assert results[15] == '\n'
-    #         assert results[
-    #                    16] == 'mock_class_one                          1.0 (0.9275915805966737, 1.0719700725722299)' + '\n'
-    #         assert results[
-    #                    17] == 'mock_class_two                          2.0 (1.9280299274279094, 2.0724084194031906)' + '\n'
-    #         assert results[
-    #                    18] == 'mock_class_three                        3.0 (2.928029927428015, 3.0724084194030854)' + '\n'
-    #         assert results[
-    #                    19] == 'mock_class_four                         4.1 (3.928029927428014, 4.072408419403085)' + '\n'
-
 
 class TestConfig(object):
     def test_multinest_default(self):
@@ -1021,7 +973,7 @@ class TestRealClasses(object):
     #     assert paramnames_str[18] == r'mass_profile_1_kappa_s                  $\kappa_{\mathrm{d2}}$' + '\n'
     #     assert paramnames_str[19] == r'mass_profile_1_scale_radius             $Rs_{\mathrm{d2}}$' + '\n'
 
-    # TODO : Reimplement once settled on moddel info output
+    # TODO : Reimplement once settled on model info output
 
     # def test__output_model_info__2_models(self, mm_config, nlo_model_info_path):
     #     conf.instance.output_path = nlo_model_info_path
@@ -1209,6 +1161,7 @@ class TestRealClasses(object):
 class MockAnalysis(object):
     def __init__(self):
         self.kwargs = None
+        self.instance = None
 
     def fit(self, instance):
         self.instance = instance
@@ -1241,14 +1194,13 @@ def make_downhill_simplex(test_config, width_config):
 
 @pytest.fixture(name="multi_nest")
 def make_multi_nest(test_config, width_config):
-
     mn_fit_path = "{}/test_fit/".format(os.path.dirname(os.path.realpath(__file__)))
 
     conf.instance.output_path = mn_fit_path
 
     create_weighted_samples_4_parameters(mn_fit_path)
 
-    # noinspection PyUnusedLocal
+    # noinspection PyUnusedLocal,PyPep8Naming
     def run(fitness_function, prior, total_parameters, outputfiles_basename, n_clustering_params=None,
             wrapped_params=None, importance_nested_sampling=True, multimodal=True, const_efficiency_mode=False,
             n_live_points=400, evidence_tolerance=0.5, sampling_efficiency=0.8, n_iter_before_update=100,
@@ -1302,9 +1254,8 @@ class TestFitting(object):
     class TestMultiNest(object):
 
         def test__path_length_error__if_path_is_above_77_characters__raise_exception(self, multi_nest):
-
             with pytest.raises(exc.MultiNestException):
-                multi_nest.path = 78*'a'
+                multi_nest.path = 78 * 'a'
                 multi_nest.fit(MockAnalysis())
 
         def test_variable(self, multi_nest, test_config):
