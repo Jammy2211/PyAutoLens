ef0661985c5419370814286d33f28d31df9159d8
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-09-16 10:53:58 +0100

    More refactoring o the visualization module

--- residuals_plot_SLACS2.py
+++ residuals_plot_SLACS2.py
@@ -10,7 +10,7 @@ obs = scaled_array.ScaledArray.from_fits(file_path='/gpfs/data/pdtw24/PL_Data/Im
 obs = obs.trim(new_shape)
 
 array_plotters.plot_observed_image_array(array=obs, xticks=xticks, yticks=yticks, normalization='symmetric_log',
-                                         output_path='/home/jammy/Documents/', output_filename='SLACS1430_Obs', output_type='png')
+                                         output_path='/home/jammy/Documents/', output_filename='SLACS1430_Obs', output_format='png')
 
 residuals_one_sersic = scaled_array.ScaledArray.from_fits(file_path='/home/jammy/Documents/RichardImages/Residuals_fits/'
                                                                     'SLACSJ1430+4105_Decomp_Mass_x1Sersic.fits', hdu=0)
@@ -18,7 +18,7 @@ residuals_one_sersic = scaled_array.ScaledArray.from_fits(file_path='/home/jammy
 residuals_one_sersic = residuals_one_sersic.trim(new_shape)
 
 array_plotters.plot_residuals_array(array=residuals_one_sersic, xticks=xticks, yticks=yticks, norm_min=-0.1, norm_max=0.1,
-                                    output_path='/home/jammy/Documents/', output_filename='SLACS1430_x1Sersic', output_type='png')
+                                    output_path='/home/jammy/Documents/', output_filename='SLACS1430_x1Sersic', output_format='png')
 
 # stop
 
@@ -28,4 +28,4 @@ residuals_one_sersic = scaled_array.ScaledArray.from_fits(file_path='/home/jammy
 residuals_one_sersic = residuals_one_sersic.trim(new_shape)
 
 array_plotters.plot_residuals_array(array=residuals_one_sersic, xticks=xticks, yticks=yticks, norm_min=-0.1, norm_max=0.1,
-                                    output_path='/home/jammy/Documents/', output_filename='SLACS1430_x2Sersic', output_type='png')
+                                    output_path='/home/jammy/Documents/', output_filename='SLACS1430_x2Sersic', output_format='png')
