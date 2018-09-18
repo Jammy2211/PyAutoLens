405026e9477bf0d15d84c04b5b313a1cf91d2c43
Richard Hayes <richardhayes777@gmail.com>
2018-09-18 10:09:16 +0100

    very basic test

--- test/test_config.py
+++ test/test_config.py
@@ -1,6 +1,16 @@
 from autolens import conf
+from os import path
+
+
+directory = path.dirname(path.realpath(__file__))
 
 
 class TestCase(object):
     def test_is_config(self):
         assert conf.is_config()
+
+
+class TestLabel(object):
+    def test_basic(self):
+        label_config = conf.NamedConfig("{}/config/label.ini".format(directory))
+        assert label_config.get("label", "centre_0") == "x"
