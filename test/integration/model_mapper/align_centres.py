5b012c139e670c84de5244b02edeb1c919ab4fad
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 11:54:19 +0100

    format and import

--- test/integration/model_mapper/align_centres.py
+++ test/integration/model_mapper/align_centres.py
@@ -5,19 +5,16 @@ from autolens.profiles import mass_profiles as mp
 from autolens.lensing import galaxy_model as gm
 from autolens.autofit import non_linear as nl
 from autolens.lensing import galaxy
-from autolens import conf
 from test.integration import tools
 
-import numpy as np
-import shutil
 import os
 
 dirpath = os.path.dirname(os.path.realpath(__file__))
 dirpath = os.path.dirname(dirpath)
 output_path = '{}/integration_output'.format(dirpath)
 
-def pipeline():
 
+def pipeline():
     pipeline_name = "align_centres"
     data_name = '/align_centres'
 
@@ -38,19 +35,20 @@ def pipeline():
     for result in results:
         print(result)
 
-def make_pipeline(pipeline_name):
 
+def make_pipeline(pipeline_name):
     class MMPhase(ph.LensPlanePhase):
         pass
 
     phase1 = MMPhase(lens_galaxies=[gm.GalaxyModel(sersic0=lp.EllipticalSersic, sersic1=lp.EllipticalSersic,
                                                    sie=mp.EllipticalIsothermal, align_centres=True)],
-                               optimizer_class=nl.MultiNest, phase_name="{}/phase1".format(pipeline_name))
+                     optimizer_class=nl.MultiNest, phase_name="{}/phase1".format(pipeline_name))
 
     phase1.optimizer.n_live_points = 20
     phase1.optimizer.sampling_efficiency = 0.8
 
     return pl.PipelineImaging(pipeline_name, phase1)
+
 
 if __name__ == "__main__":
     pipeline()
