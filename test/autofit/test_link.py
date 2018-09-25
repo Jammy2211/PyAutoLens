352dddcd9e6927a56087718b7f5c99d93acc8f20
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 09:50:50 +0100

    create base64 dir

--- test/autofit/test_link.py
+++ test/autofit/test_link.py
@@ -5,3 +5,10 @@ from autolens.autofit import link
 class TestCase(object):
     def test_create_dir(self):
         assert os.path.exists(link.autolens_dir)
+
+    def test_consistent_dir(self):
+        directory = link.directory_for("/a/random/directory")
+        assert link.autolens_dir in directory
+        assert len(directory.split("/")[-1]) == link.SUB_DIR_LENGTH
+        assert directory == link.directory_for("/a/random/directory")
+        assert directory != link.directory_for("/b/random/directory")
