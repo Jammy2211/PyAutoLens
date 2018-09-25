da4e02884faed51d4dcc67f7e21874415fe669d8
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 17:14:29 +0100

    test demonstrating model mapper set attribute issue

--- test/integration/test_regression.py
+++ test/integration/test_regression.py
@@ -1,5 +1,6 @@
 import os
 
+from autolens.autofit import model_mapper as mm
 from autolens.autofit import non_linear as nl
 from autolens.lensing import galaxy
 from autolens.lensing import galaxy_model as gm
@@ -50,3 +51,45 @@ class TestPhaseModelMapper(object):
         assert len(lines) == 2
         assert "lens_galaxies_axis_ratio                UniformPrior, lower_limit = 0.5, upper_limit = 1.0" in lines
         assert "lens_galaxies_intensity                 UniformPrior, lower_limit = 0.5, upper_limit = 1.0" in lines
+
+    def test_constants_work(self):
+        pipeline_name = "const_float"
+        data_name = '/const_float'
+
+        tools.reset_paths(data_name, pipeline_name, output_path)
+
+        sersic = lp.EllipticalSersic(centre=(0.0, 0.0), axis_ratio=0.8, phi=90.0, intensity=1.0, effective_radius=1.3,
+                                     sersic_index=3.0)
+
+        lens_galaxy = galaxy.Galaxy(light_profile=sersic)
+
+        tools.simulate_integration_image(data_name=data_name, pixel_scale=0.5, lens_galaxies=[lens_galaxy],
+                                         source_galaxies=[], target_signal_to_noise=10.0)
+        image = tools.load_image(data_name=data_name, pixel_scale=0.5)
+
+        class MMPhase(ph.LensPlanePhase):
+
+            def pass_priors(self, previous_results):
+                self.lens_galaxies[0].sersic.axis_ratio = 0.2
+                self.lens_galaxies[0].sersic.phi = 90.0
+                self.lens_galaxies[0].sersic.intensity = 1.0
+                self.lens_galaxies[0].sersic.effective_radius = 1.3
+                self.lens_galaxies[0].sersic.sersic_index = 3.0
+
+        phase = MMPhase(lens_galaxies=[gm.GalaxyModel(sersic=lp.EllipticalSersic)],
+                        optimizer_class=nl.MultiNest, phase_name="{}/phase1".format(pipeline_name))
+
+        phase.optimizer.n_live_points = 20
+        phase.optimizer.sampling_efficiency = 0.8
+
+        phase.make_analysis(image)
+
+        sersic = phase.variable.lens_galaxies[0].sersic
+
+        assert isinstance(sersic, mm.PriorModel)
+
+        assert isinstance(sersic.axis_ratio, mm.Constant)
+        assert isinstance(sersic.phi, mm.Constant)
+        assert isinstance(sersic.intensity, mm.Constant)
+        assert isinstance(sersic.effective_radius, mm.Constant)
+        assert isinstance(sersic.sersic_index, mm.Constant)
