e5a0a67326c35bb2aa7eb9cec74db98d94d35505
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 14:02:40 +0100

    triangulation

--- test/autofit/test_link.py
+++ test/autofit/test_link.py
@@ -50,3 +50,13 @@ class TestCase(object):
         assert not os.path.exists(temp_folder_path)
         link.make_linked_folder(temp_folder_path)
         assert not os.path.exists(temp_file_path)
+
+    def test_not_clean_source(self):
+        path = link.make_linked_folder(temp_folder_path)
+        temp_file_path = "{}/{}".format(path, "temp")
+        open(temp_file_path, "a").close()
+        assert os.path.exists(temp_file_path)
+
+        link.make_linked_folder(temp_folder_path)
+        assert os.path.exists(temp_file_path)
+
