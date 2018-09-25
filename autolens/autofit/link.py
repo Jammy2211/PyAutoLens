82e90163f9e635ac390e709a01524fa206249607
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 09:50:50 +0100

    create base64 dir

--- autolens/autofit/link.py
+++ autolens/autofit/link.py
@@ -1,13 +1,21 @@
 import os
 import logging
 from os.path import expanduser
+from base64 import b64encode
+
+SUB_DIR_LENGTH = 5
+AUTOLENS_FOLDER = ".autolens"
 
 logger = logging.getLogger(__file__)
 
 home = expanduser("~")
-autolens_dir = "{}/.autolens".format(home)
+autolens_dir = "{}/{}".format(home, AUTOLENS_FOLDER)
 
 try:
     os.mkdir(autolens_dir)
 except Exception as e:
     logger.exception(e)
+
+
+def directory_for(directory):
+    return "{}/{}".format(autolens_dir, b64encode(bytes(directory, encoding="utf-8")).decode("utf-8")[:SUB_DIR_LENGTH])
