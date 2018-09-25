ce2b39920cfba775f84c689c4cc596aded3503e2
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 14:56:51 +0100

    better default

--- autolens/autofit/non_linear.py
+++ autolens/autofit/non_linear.py
@@ -61,12 +61,14 @@ class NonLinearOptimizer(object):
 
         self.named_config = conf.instance.non_linear
 
+        if name is None:
+            name = "phase"
+
         sym_path = "{}/{}/optimizer".format(conf.instance.output_path, name)
 
         if not os.path.exists(sym_path):
             os.makedirs("/".join(sym_path.split("/")[:-1]))
-        if name is None:
-            name = ""
+
         self.path = link.make_linked_folder(sym_path)
         self.chains_path = "{}/{}".format(self.path, 'chains')
         if not os.path.exists(self.chains_path):
