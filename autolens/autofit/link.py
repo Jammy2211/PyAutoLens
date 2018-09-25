1cd07d5546d0388f90d34fd30e8cd35233dbcecc
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 11:07:58 +0100

    linking folders

--- autolens/autofit/link.py
+++ autolens/autofit/link.py
@@ -3,7 +3,7 @@ import logging
 from os.path import expanduser
 from base64 import b64encode
 
-SUB_PATH_LENGTH = 5
+SUB_PATH_LENGTH = 10
 AUTOLENS_FOLDER = ".autolens"
 
 logger = logging.getLogger(__file__)
@@ -13,12 +13,18 @@ autolens_dir = "{}/{}".format(home, AUTOLENS_FOLDER)
 
 try:
     os.mkdir(autolens_dir)
-except FileExistsError as e:
-    pass
+except FileExistsError as ex:
+    logger.exception(ex)
 
 
 def path_for(path):
-    return "{}/{}".format(autolens_dir, b64encode(bytes(path, encoding="utf-8")).decode("utf-8")[:SUB_PATH_LENGTH])
+    print(path)
+    start = int(SUB_PATH_LENGTH / 2)
+    end = SUB_PATH_LENGTH - start
+    print("start = {}, end = {}".format(start, end))
+    b64_string = b64encode(bytes(path, encoding="utf-8")).decode("utf-8")
+    print(b64_string)
+    return "{}/{}".format(autolens_dir, b64_string[:start] + b64_string[-end:])
 
 
 def make_linked_file(path):
@@ -26,6 +32,27 @@ def make_linked_file(path):
     open(actual_path, 'a').close()
     try:
         os.symlink(actual_path, path)
-    except FileExistsError:
-        pass
+    except FileExistsError as e:
+        logger.exception(e)
+    return actual_path
+
+
+def make_linked_folder(path):
+    actual_path = path_for(path)
+    try:
+        os.mkdir(actual_path)
+    except FileExistsError as e:
+        logger.exception(e)
+    try:
+        split_path = list(filter(lambda component: component, path.split("/")))
+        for i in range(len(split_path) - 1):
+            try:
+                make_path = "/{}".format("/".join(split_path[:i + 1]))
+                print(make_path)
+                os.mkdir(make_path)
+            except (IsADirectoryError, FileExistsError) as e:
+                logger.exception(e)
+        os.symlink(actual_path, path)
+    except FileExistsError as e:
+        logger.exception(e)
     return actual_path
