231f005a921b377c38b07bd67c1a5f2dd161ae59
Richard Hayes <richardhayes777@gmail.com>
2018-09-25 14:07:04 +0100

    docs

--- autolens/autofit/link.py
+++ autolens/autofit/link.py
@@ -19,6 +19,20 @@ except FileExistsError as ex:
 
 
 def path_for(path):
+    """
+    Generate a path in the ~/.autolens directory by taking the provided path, base64 encoding it and extracting the
+    first and last five characters.
+
+    Parameters
+    ----------
+    path: str
+        The path where multinest output is apparently saved
+
+    Returns
+    -------
+    actual_path: str
+        The path where multinest output is actually saved
+    """
     start = int(SUB_PATH_LENGTH / 2)
     end = SUB_PATH_LENGTH - start
     b64_string = b64encode(bytes(path, encoding="utf-8")).decode("utf-8")
@@ -26,6 +40,23 @@ def path_for(path):
 
 
 def make_linked_folder(path):
+    """
+    Create a folder in the ~/.autolens directory and create a sym link to it at the provided path.
+
+    If both folders already exist then nothing is changed. If the source folder exists but the destination folder does
+    not then the source folder is removed and replaced so as to conform to the behaviour that the user would expect
+    should they delete the sym linked folder.
+
+    Parameters
+    ----------
+    path: str
+        The path where multinest output is apparently saved
+
+    Returns
+    -------
+    actual_path: str
+        The path where multinest output is actually saved
+    """
     actual_path = path_for(path)
     if os.path.exists(actual_path) and not os.path.exists(path):
         shutil.rmtree(actual_path)
