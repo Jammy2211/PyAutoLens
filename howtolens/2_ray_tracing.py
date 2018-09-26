c1f905b3195872f302d1b30bc52eb5866eb856fc
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-09-26 13:44:52 +0100

    Second ray tracing tutorial complete, involved passing arcsec per kpc to plotter

--- howtolens/2_ray_tracing.py
+++ howtolens/2_ray_tracing.py
@@ -5,6 +5,8 @@ from autolens.lensing import ray_tracing
 from autolens.imaging import mask
 from autolens.plotting import ray_tracing_plotters
 
+from astropy import cosmology
+
 # In this example, we'll reinforce what we learnt about ray-tracing in the last tutorial and introduce the following
 # new aspects about using the ray-tracing module:
 # 1) Short-hand notation for setting up profiles and galaxies, to make code clean and easy to read.
@@ -17,22 +19,26 @@ from autolens.plotting import ray_tracing_plotters
 lens_galaxy = g.Galaxy(light=lp.SphericalSersic(centre=(0.0, 0.0), intensity=2.0, effective_radius=0.5,
                                                 sersic_index=2.5),
                        mass=mp.EllipticalIsothermal(centre=(0.0, 0.0), axis_ratio=0.8, phi=90.0, einstein_radius=1.6),
-                       shear=mp.ExternalShear(magnitude=0.05, phi=45.0))
+                       shear=mp.ExternalShear(magnitude=0.05, phi=45.0),
+                       redshift=0.5)
 
 # As you can see, we can put as many light and mass profiles in the galaxy and they'll be correctly handled by the
 # ray-tracing once we pass it the galaxy!
 # Lets also create a small satellite galaxy nearby this lens.
 lens_satellite = g.Galaxy(light=lp.SphericalDevVaucouleurs(centre=(1.0, 0.0), intensity=2.0, effective_radius=0.2),
-                          mass=mp.SphericalIsothermal(centre=(1.0, 0.0), einstein_radius=0.4))
+                          mass=mp.SphericalIsothermal(centre=(1.0, 0.0), einstein_radius=0.4),
+                          redshift=0.5)
 
 # Finally, lets make two source galaxies. We don't need to use the terms 'light' and 'mass' to setup profiles, we should
 # choose the most descriptive terms for the galaxy, like we have below for the source's bulge and disk.
 source_galaxy_0 = g.Galaxy(bulge=lp.SphericalDevVaucouleurs(centre=(0.1, 0.2), intensity=0.3, effective_radius=0.3),
                            disk=lp.EllipticalExponential(centre=(0.1, 0.2), axis_ratio=0.8, phi=45.0, intensity=3.0,
-                                                       effective_radius=2.0))
+                                                       effective_radius=2.0),
+                           redshift=1.0)
 
 source_galaxy_1 = g.Galaxy(disk=lp.EllipticalExponential(centre=(-0.3, -0.5), axis_ratio=0.6, phi=80.0, intensity=8.0,
-                                                         effective_radius=1.0))
+                                                         effective_radius=1.0),
+                           redshift=1.0)
 
 # Again, we need to setup the grids we ray-trace using. Lets use a higher resolution grid then before!
 image_plane_grids = mask.ImagingGrids.from_shape_and_pixel_scale(shape=(200, 200), pixel_scale=0.025)
@@ -41,7 +47,7 @@ image_plane_grids = mask.ImagingGrids.from_shape_and_pixel_scale(shape=(200, 200
 # passing multiple lens galaxies and source galaxies, there is no limit to how many you pass.
 tracer = ray_tracing.TracerImageSourcePlanes(lens_galaxies=[lens_galaxy, lens_satellite],
                                              source_galaxies=[source_galaxy_0, source_galaxy_1],
-                                             image_plane_grids=image_plane_grids)
+                                             image_plane_grids=image_plane_grids, cosmology=cosmology.Planck15)
 
 # AutoLens has plotting tools specifically for a tracer, if we plot the tracer we'll see its surface-density,
 # gravitational potential, deflection-angle map, image-plane image and source-plane image.
