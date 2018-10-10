55447cc7a70a429b33f701c8b839ea5560cb2806
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-10-10 08:51:52 +0100

    Updated howtolens tutorials so that they run and visualize

--- autolens/plotting/plane_plotters.py
+++ autolens/plotting/plane_plotters.py
@@ -6,12 +6,12 @@ from autolens.plotting import array_plotters
 
 def plot_plane_grid(plane, xmin=None, xmax=None, ymin=None, ymax=None):
 
-    plt.figure(figsize=(10, 8))
-    plt.scatter(x=plane.grids.image[:, 0], y=plane.grids.image[:, 1], marker='.')
+    plt.figure(figsize=(12, 8))
+    plt.scatter(y=plane.grids.image[:, 0], x=plane.grids.image[:, 1], marker='.', s=1)
     array_plotters.set_title(title='Plane Grid', titlesize=36)
-    plt.xlabel('x (arcsec)', fontsize=36)
-    plt.ylabel('y (arcsec)', fontsize=36)
-    plt.tick_params(labelsize=40)
+    plt.xlabel('x (arcsec)', fontsize=20)
+    plt.ylabel('y (arcsec)', fontsize=20)
+    plt.tick_params(labelsize=20)
     if xmin is not None and xmax is not None and ymin is not None and ymax is not None:
         plt.axis([xmin, xmax, ymin, ymax])
     plt.show()
