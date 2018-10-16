2a32aa5f2d669c22f0fe2c9281a1ee80551b3c95
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-10-16 15:39:25 +0100

    Added colorbar frac / pad to plotting functions, so that points plot on images correctly.

--- autolens/plotting/plane_plotters.py
+++ autolens/plotting/plane_plotters.py
@@ -7,7 +7,8 @@ from autolens.plotting import plot_grid
 def plot_image_plane_image(plane, mask=None, positions=None, grid=None, as_subplot=False,
                            units='arcsec', kpc_per_arcsec=None,
                            xyticksize=16, norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
-                           figsize=(7, 7), aspect='equal', cmap='jet', cb_ticksize=16,
+                           figsize=(7, 7), aspect='equal', cmap='jet',
+                           cb_ticksize=10, cb_fraction=0.047, cb_pad=0.01,
                            title='Plane Image-Plane Image', titlesize=16, xlabelsize=16, ylabelsize=16,
                            output_path=None, output_format='show', output_filename='plane_image_plane_image'):
 
@@ -15,14 +16,16 @@ def plot_image_plane_image(plane, mask=None, positions=None, grid=None, as_subpl
                                       grid=grid, as_subplot=as_subplot,
                                       units=units, kpc_per_arcsec=kpc_per_arcsec, xyticksize=xyticksize,
                                       norm=norm, norm_min=norm_min, norm_max=norm_max, linthresh=linthresh,
-                                      linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap, cb_ticksize=cb_ticksize,
+                                      linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap,
+                                      cb_ticksize=cb_ticksize, cb_fraction=cb_fraction, cb_pad=cb_pad,
                                       title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                                       output_path=output_path, output_format=output_format, output_filename=output_filename)
 
 def plot_plane_image(plane, positions=None, plot_grid=False, as_subplot=False,
                      units='arcsec', kpc_per_arcsec=None,
                      xyticksize=16, norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
-                     figsize=(7, 7), aspect='equal', cmap='jet', cb_ticksize=16,
+                     figsize=(7, 7), aspect='equal', cmap='jet',
+                     cb_ticksize=10, cb_fraction=0.047, cb_pad=0.01,
                      title='Plane Image', titlesize=16, xlabelsize=16, ylabelsize=16,
                      output_path=None, output_format='show', output_filename='plane_image'):
 
@@ -30,63 +33,72 @@ def plot_plane_image(plane, positions=None, plot_grid=False, as_subplot=False,
                                 as_subplot=as_subplot,
                                 units=units, kpc_per_arcsec=kpc_per_arcsec, xyticksize=xyticksize,
                                 norm=norm, norm_min=norm_min, norm_max=norm_max, linthresh=linthresh,
-                                linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap, cb_ticksize=cb_ticksize,
+                                linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap,
+                                cb_ticksize=cb_ticksize, cb_fraction=cb_fraction, cb_pad=cb_pad,
                                 title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                                 output_path=output_path, output_format=output_format, output_filename=output_filename)
 
 def plot_surface_density(plane, as_subplot=False,
                          units='arcsec', kpc_per_arcsec=None,
                          xyticksize=16, norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
-                         figsize=(7, 7), aspect='equal', cmap='jet', cb_ticksize=16,
+                         figsize=(7, 7), aspect='equal', cmap='jet',
+                         cb_ticksize=10, cb_fraction=0.047, cb_pad=0.01,
                          title='Plane Surface Density', titlesize=16, xlabelsize=16, ylabelsize=16,
                          output_path=None, output_format='show', output_filename='plane_surface_density'):
 
     plot_array.plot_surface_density(surface_density=plane.surface_density, as_subplot=as_subplot,
                                     units=units, kpc_per_arcsec=kpc_per_arcsec, xyticksize=xyticksize,
                                     norm=norm, norm_min=norm_min, norm_max=norm_max, linthresh=linthresh,
-                                    linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap, cb_ticksize=cb_ticksize,
+                                    linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap,
+                                    cb_ticksize=cb_ticksize, cb_fraction=cb_fraction, cb_pad=cb_pad,
                                     title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                                     output_path=output_path, output_format=output_format, output_filename=output_filename)
 
 def plot_potential(plane, as_subplot=False,
                    units='arcsec', kpc_per_arcsec=None,
                    xyticksize=16, norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
-                   figsize=(7, 7), aspect='equal', cmap='jet', cb_ticksize=16,
+                   figsize=(7, 7), aspect='equal', cmap='jet',
+                   cb_ticksize=10, cb_fraction=0.047, cb_pad=0.01,
                    title='Plane Potential', titlesize=16, xlabelsize=16, ylabelsize=16,
                    output_path=None, output_format='show', output_filename='plane_potential'):
 
     plot_array.plot_potential(potential=plane.potential, as_subplot=as_subplot,
                               units=units, kpc_per_arcsec=kpc_per_arcsec, xyticksize=xyticksize,
                               norm=norm, norm_min=norm_min, norm_max=norm_max, linthresh=linthresh,
-                              linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap, cb_ticksize=cb_ticksize,
+                              linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap,
+                              cb_ticksize=cb_ticksize, cb_fraction=cb_fraction, cb_pad=cb_pad,
                               title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                               output_path=output_path, output_format=output_format, output_filename=output_filename)
     
 def plot_deflections_y(plane, as_subplot=False,
                        units='arcsec', kpc_per_arcsec=None,
                        xyticksize=16, norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
-                       figsize=(7, 7), aspect='equal', cmap='jet', cb_ticksize=16,
+                       figsize=(7, 7), aspect='equal', cmap='jet',
+                       cb_ticksize=10, cb_fraction=0.047, cb_pad=0.01,
                        title='Plane Deflections (y)', titlesize=16, xlabelsize=16, ylabelsize=16,
                        output_path=None, output_format='show', output_filename='plane_deflections_y'):
 
     plot_array.plot_deflections_y(deflections_y=plane.deflections_y, as_subplot=as_subplot,
                                   units=units, kpc_per_arcsec=kpc_per_arcsec, xyticksize=xyticksize,
                                   norm=norm, norm_min=norm_min, norm_max=norm_max, linthresh=linthresh,
-                                  linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap, cb_ticksize=cb_ticksize,
+                                  linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap,
+                                  cb_ticksize=cb_ticksize, cb_fraction=cb_fraction, cb_pad=cb_pad,
                                   title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                                   output_path=output_path, output_format=output_format, output_filename=output_filename)
     
 def plot_deflections_x(plane, as_subplot=False,
                        units='arcsec', kpc_per_arcsec=None,
                        xyticksize=16, norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
-                       figsize=(7, 7), aspect='equal', cmap='jet', cb_ticksize=16,
+                       figsize=(7, 7), aspect='equal', cmap='jet',
+                       cb_ticksize=10, cb_fraction=0.047, cb_pad=0.01,
                        title='Plane Deflections (x)', titlesize=16, xlabelsize=16, ylabelsize=16,
                        output_path=None, output_format='show', output_filename='plane_deflections_x'):
 
     plot_array.plot_deflections_x(deflections_x=plane.deflections_x, as_subplot=as_subplot,
                                   units=units, kpc_per_arcsec=kpc_per_arcsec, xyticksize=xyticksize,
                                   norm=norm, norm_min=norm_min, norm_max=norm_max, linthresh=linthresh,
-                                  linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap, cb_ticksize=cb_ticksize,
+                                  linscale=linscale, figsize=figsize, aspect=aspect, cmap=cmap,
+                                  cb_ticksize=cb_ticksize, cb_fraction=cb_fraction, cb_pad=cb_pad,
                                   title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                                   output_path=output_path, output_format=output_format, output_filename=output_filename)
 
