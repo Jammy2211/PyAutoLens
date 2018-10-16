5fc6f882f74060d9fcde18b1b5a8adc8c0ed1ed7
Jammy2211 <james.w.nightingale@durham.ac.uk>
2018-10-16 13:20:36 +0100

    Updated plot_grid tutorial to includes points of image pixels

--- autolens/plotting/plane_plotters.py
+++ autolens/plotting/plane_plotters.py
@@ -90,7 +90,7 @@ def plot_deflections_x(plane, as_subplot=False,
                                   title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
                                   output_path=output_path, output_format=output_format, output_filename=output_filename)
 
-def plot_image_and_source_plane_subplot(image_plane, source_plane, axis_limits=None,
+def plot_image_and_source_plane_subplot(image_plane, source_plane, points=None, axis_limits=None,
                     units='arcsec', kpc_per_arcsec=None,
                     output_path=None, output_format='show', output_filename='image_and_source_plane_grids'):
 
@@ -99,29 +99,31 @@ def plot_image_and_source_plane_subplot(image_plane, source_plane, axis_limits=N
     plt.figure(figsize=figsize)
     plt.subplot(rows, columns, 1)
 
-    plot_plane_grid(plane=image_plane, axis_limits=axis_limits, as_subplot=True,
+    plot_plane_grid(plane=image_plane, axis_limits=axis_limits, points=points, as_subplot=True,
                     units=units, kpc_per_arcsec=kpc_per_arcsec,
                     pointsize=3, xyticksize=16, titlesize=10, xlabelsize=10, ylabelsize=10,
+                    title='Image-plane Grid',
                     output_path=output_path, output_filename=output_filename, output_format=output_format)
 
     plt.subplot(rows, columns, 2)
 
-    plot_plane_grid(plane=source_plane, axis_limits=axis_limits, as_subplot=True,
+    plot_plane_grid(plane=source_plane, axis_limits=axis_limits, points=points, as_subplot=True,
                     units=units, kpc_per_arcsec=kpc_per_arcsec,
                     pointsize=3, xyticksize=16, titlesize=10, xlabelsize=10, ylabelsize=10,
+                    title='Source-plane Grid',
                     output_path=output_path, output_filename=output_filename, output_format=output_format)
 
     tools.output_subplot_array(output_path=output_path, output_filename=output_filename, output_format=output_format)
     plt.close()
 
 
-def plot_plane_grid(plane, axis_limits=None, as_subplot=False,
+def plot_plane_grid(plane, axis_limits=None, points=None, as_subplot=False,
                     units='arcsec', kpc_per_arcsec=None,
                     figsize=(12, 8), pointsize=3, xyticksize=16,
                     title='Plane Grid', titlesize=16, xlabelsize=16, ylabelsize=16,
                     output_path=None, output_format='show', output_filename='plane_grid'):
 
-    plot_grid.plot_grid(grid=plane.grids.image, axis_limits=axis_limits, as_subplot=as_subplot,
+    plot_grid.plot_grid(grid=plane.grids.image, points=points, axis_limits=axis_limits, as_subplot=as_subplot,
                          units=units, kpc_per_arcsec=kpc_per_arcsec,
                          figsize=figsize, pointsize=pointsize, xyticksize=xyticksize,
                          title=title, titlesize=titlesize, xlabelsize=xlabelsize, ylabelsize=ylabelsize,
