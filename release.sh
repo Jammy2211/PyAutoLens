e168e6272fd04cc63a03af3eb8b4555727fd3450
Richard Hayes <richardhayes777@gmail.com>
2018-10-22 17:21:36 +0100

    added lines for docker build and push to release script

--- release.sh
+++ release.sh
@@ -2,3 +2,6 @@
 
 python setup.py sdist bdist_wheel
 twine upload dist/* --skip-existing --username $PYPI_USERNAME --password $PYPI_PASSWORD
+
+docker build -t rhayes777/autolens .
+docker push rhayes777/autolens:latest
