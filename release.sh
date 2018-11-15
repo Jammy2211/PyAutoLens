88b18c107c0afb8dfd79e3d1dd4a9545da7560d6
Richard Hayes <richardhayes777@gmail.com>
2018-11-15 14:12:15 +0000

    added docker login step

--- release.sh
+++ release.sh
@@ -3,5 +3,6 @@
 python setup.py sdist bdist_wheel
 twine upload dist/* --skip-existing --username $PYPI_USERNAME --password $PYPI_PASSWORD
 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t rhayes777/autolens .
 docker push rhayes777/autolens:latest
