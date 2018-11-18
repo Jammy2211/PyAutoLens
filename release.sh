c5c42b98380737aa1d43a3a49ca62b70d1bf7896
Richard Hayes <richardhayes777@gmail.com>
2018-11-18 16:51:37 +0000

    fixes to release script

--- release.sh
+++ release.sh
@@ -3,20 +3,25 @@
 set -e
 
 BRANCH=`git branch | grep \* | cut -d ' ' -f2`
-BRANCH_TYPE=`echo $BRANCH | cut -d ' ' -f1`
+BRANCH_TYPE=`echo $BRANCH | cut -d '/' -f1`
 VERSION=`echo $BRANCH | cut -d '/' -f2`
 
-if [ $BRANCH_TYPE!="release" ]  
+if [ $BRANCH_TYPE != "release" ]  
 then
 	echo "Must be on a release branch. Type git-flow release start a new release."
 	exit 1
 fi
 
-echo "__version__ = "`git branch | grep \* | cut -d ' ' -f2 | cut -d '/' -f2` > autolens/__init__.py
+echo "__version__ = '"`git branch | grep \* | cut -d ' ' -f2 | cut -d '/' -f2`"'" > autolens/__init__.py
+
+git add autolens/__init__.py
+git commit -m "Incremented version number"
 
 python setup.py sdist bdist_wheel
 twine upload dist/* --skip-existing --username $PYPI_USERNAME --password $PYPI_PASSWORD
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t rhayes777/autolens .
-docker push rhayes777/autolens:latest
+docker push rhayes777/autofit:latest
+
+git flow release finish '0.1.0'
