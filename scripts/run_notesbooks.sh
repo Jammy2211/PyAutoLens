ec0fa4e108f048749f461aa48e34cd4984324327
Richard Hayes <richardhayes777@gmail.com>
2018-12-05 14:58:58 +0000

    logging error when running notebooks

--- scripts/run_notesbooks.sh
+++ scripts/run_notesbooks.sh
@@ -7,6 +7,6 @@ do
 	echo "Converting $filename"
 	jupyter nbconvert --to script $notebook --output temp 
 	echo "Running $filename"
-	python $dir/temp.py 
+	python $dir/temp.py 2> >(tee -a error.log >&2) 
 	rm $dir/temp.py
 done
