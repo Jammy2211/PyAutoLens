0a694f7380a23544fde40a70d14078e4dd99b543
Jammy2211 <james.w.nightingale@durham.ac.uk>
2019-01-09 18:57:46 +0000

    Fixed input of a mask to run function and added unit test to check behaviour.

--- workspace/tools/mask_maker.py
+++ workspace/tools/mask_maker.py
@@ -8,19 +8,20 @@ import os
 # pipeline is run and passed to that pipeline so as to become the default masked used by a phase (if a mask
 # function is not passed to that phase).
 
-# The 'lens name' is the name of the lens in the data folder, e.g. if you run this, code the mask will be output as
-# '/workspace/data/example/mask.fits)
-lens_name = 'example'
+# The 'lens name' is the name of the lens in the data folder, e.g. the mask will be output as 
+# '/workspace/data/example_sim/mask.fits' (this file is already in the workspace and is remade running this script)
+lens_name = 'example_sim'
+pixel_scale = 0.1 # If you use this tool for your own data, you *must* double check this pixel scale is correct!
 
 # First, load the CCD imaging data, so that the mask can be plotted over the strong lens image.
 path = '{}/../'.format(os.path.dirname(os.path.realpath(__file__)))
-image = ccd.load_image(image_path=path+'/data/'+lens_name+'/image.fits', image_hdu=0, pixel_scale=0.03)
+image = ccd.load_image(image_path=path+'/data/'+lens_name+'/image.fits', image_hdu=0, pixel_scale=pixel_scale)
 
 # Now, create a mask for this data, using the mask function's we're used to. I'll use a circular-annular mask here,
 # but I've commented over options you might want to use (feel free to experiment!)
 
 mask = msk.Mask.circular_annular(shape=image.shape, pixel_scale=image.pixel_scale,
-                                 inner_radius_arcsec=0.2, outer_radius_arcsec=3.0, centre=(0.0, 0.0))
+                                 inner_radius_arcsec=0.5, outer_radius_arcsec=2.5, centre=(0.0, 0.0))
 
 # mask = msk.Mask.circular(shape=image.shape, pixel_scale=image.pixel_scale,
 #                          radius_arcsec=3.0, centre=(0.0, 0.0))
@@ -38,4 +39,4 @@ data_plotters.plot_image(image=image, mask=mask)
 
 # Now we're happy with the mask, lets output it to the data folder of the lens, so that we can load it from a .fits
 # file in our pipelines!
-msk.output_mask_to_fits(mask=mask, mask_path=path+'/data/'+lens_name+'/mask.fits')
+msk.output_mask_to_fits(mask=mask, mask_path=path+'/data/'+lens_name+'/mask.fits', overwrite=True)
