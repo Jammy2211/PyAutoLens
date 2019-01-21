1f714b7e0598de78815491e0a55483868d3b6b85
Richard Hayes <richardhayes777@gmail.com>
2019-01-21 16:14:21 +0000

    updated autofit requirement

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
                       'numba',
                       'matplotlib',
                       'colorama',
-		      'autofit==0.7.0'
+		      'autofit==0.7.2'
                       ],
     extras_require={
         'test': ['coverage', 'pytest', 'pytest-cov'],
