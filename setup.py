50c2ac5ff1fd40b5cac501ef19ea5b44752b38ed
Richard Hayes <richardhayes777@gmail.com>
2019-01-25 15:44:53 +0000

    autofit==0.8.6

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
                       'numba',
                       'matplotlib',
                       'colorama',
-		      'autofit==0.8.5'
+		      'autofit==0.8.6'
                       ],
     extras_require={
         'test': ['coverage', 'pytest', 'pytest-cov'],
