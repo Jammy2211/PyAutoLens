1319f78fad97e36a6d041ce9e847dc6a4575e016
Richard Hayes <richardhayes777@gmail.com>
2019-02-26 16:41:43 +0000

    docs

--- autolens/data/array/grids.py
+++ autolens/data/array/grids.py
@@ -1044,6 +1044,25 @@ class Interpolator(object):
 
 
 def grid_interpolate(func):
+    """
+    Decorate a profile method that accepts a coordinate grid and returns a data grid.
+
+    If an interpolator attribute is associated with the input grid then that interpolator is used to down sample the
+    coordinate grid prior to calling the function and up sample the result of the function.
+
+    If no interpolator attribute is associated with the input grid then the function is called as normal.
+
+    Parameters
+    ----------
+    func
+        Some method that accepts a grid
+
+    Returns
+    -------
+    decorated_function
+        The function with optional interpolation
+    """
+
     @wraps(func)
     def wrapper(profile, grid, *args, **kwargs):
         if hasattr(grid, "interpolator"):
