4e9dd24f6d2b9b819afb375aa7a58ea5ddc5b591
Richard Hayes <richardhayes777@gmail.com>
2019-03-04 11:02:18 +0000

    docs

--- autolens/model/profiles/geometry_profiles.py
+++ autolens/model/profiles/geometry_profiles.py
@@ -43,15 +43,18 @@ def transform_grid(func):
 
 def cache(func):
     """
-    Cache
+    Caches results of a call to a grid function. If a grid that evaluates to the same byte value is passed into the same
+    function of the same instance as previously then the cached result is returned.
 
     Parameters
     ----------
     func
+        Some instance method that takes a grid as its argument
 
     Returns
     -------
-
+    result
+        Some result, either newly calculated or recovered from the cache
     """
 
     def wrapper(instance: GeometryProfile, grid: np.ndarray):
