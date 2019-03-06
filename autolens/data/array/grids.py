54e2288ea4df3e21925c494f2da7072271b514d9
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-03-06 17:57:06 +0000

    Added function to SubGRid which maps a sub_array_1d to a sub_array_2d

--- autolens/data/array/grids.py
+++ autolens/data/array/grids.py
@@ -622,8 +622,9 @@ class SubGrid(RegularGrid):
             The 1D sub_array which is mapped to its masked 2D sub-array.
         """
         sub_shape = (self.mask.shape[0]*self.sub_grid_size, self.mask.shape[1]*self.sub_grid_size)
+        sub_one_to_two = self.mask.masked_sub_grid_index_to_sub_pixel(sub_grid_size=self.sub_grid_size)
         return mapping_util.map_masked_1d_array_to_2d_array_from_array_1d_shape_and_one_to_two(
-            array_1d=sub_array_1d, shape=sub_shape, one_to_two=self.mask.masked_grid_index_to_pixel)
+            array_1d=sub_array_1d, shape=sub_shape, one_to_two=sub_one_to_two)
 
     # def scaled_array_from_sub_array_1d(self, sub_array_1d):
     #     """ Map a 1D array the same dimension as the grid to its original masked 2D array and return it as a scaled \
