1d538ee6007c788f0d5a9c1ffb2c43883d73060c
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-03-06 18:04:14 +0000

    Added function to SubGrid which created a scaled_array_from a sub_array_2d

--- autolens/data/array/grids.py
+++ autolens/data/array/grids.py
@@ -626,18 +626,18 @@ class SubGrid(RegularGrid):
         return mapping_util.map_masked_1d_array_to_2d_array_from_array_1d_shape_and_one_to_two(
             array_1d=sub_array_1d, shape=sub_shape, one_to_two=sub_one_to_two)
 
-    # def scaled_array_from_sub_array_1d(self, sub_array_1d):
-    #     """ Map a 1D array the same dimension as the grid to its original masked 2D array and return it as a scaled \
-    #     array.
-    #
-    #     Parameters
-    #     -----------
-    #     array_1d : ndarray
-    #         The 1D array of which is mapped to a 2D scaled array.
-    #     """
-    #     return scaled_array.ScaledSquarePixelArray(array=self.array_2d_from_array_1d(array_1d),
-    #                                                pixel_scale=self.mask.pixel_scale,
-    #                                                origin=self.mask.origin)
+    def scaled_sub_array_from_sub_array_1d(self, sub_array_1d):
+        """ Map a 1D sub-array the same dimension as the sub-grid to its original masked 2D sub-array and return it as
+        a scaled array.
+
+        Parameters
+        -----------
+        sub_array_1d : ndarray
+            The 1D sub-array of which is mapped to a 2D scaled sub-array the dimensions.
+        """
+        return scaled_array.ScaledSquarePixelArray(array=self.sub_array_2d_from_sub_array_1d(sub_array_1d=sub_array_1d),
+                                                   pixel_scale=self.mask.pixel_scale / self.sub_grid_size,
+                                                   origin=self.mask.origin)
 
     def __array_finalize__(self, obj):
         super().__array_finalize__(obj)
