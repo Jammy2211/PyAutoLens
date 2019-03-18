82cddbeb50e0771d95970de753d276bb6972e94f
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-03-18 18:30:28 +0000

    Implemented binning up mask routine in array util

--- autolens/data/array/util/array_util.py
+++ autolens/data/array/util/array_util.py
@@ -101,7 +101,7 @@ def extracted_array_2d_from_array_2d_and_coordinates(array_2d, y0, y1, x0, x1):
 
 
 @decorator_util.jit()
-def resized_array_2d_from_array_2d_and_resized_shape(array_2d, resized_shape, origin=(-1, -1)):
+def resized_array_2d_from_array_2d_and_resized_shape(array_2d, resized_shape, origin=(-1, -1), pad_value=0.0):
     """Resize an array to a new size around a central pixel.
 
     If the origin (e.g. the central pixel) of the resized array is not specified, the central pixel of the array is \
@@ -171,7 +171,7 @@ def resized_array_2d_from_array_2d_and_resized_shape(array_2d, resized_shape, or
                     resized_array[y_resized, x_resized] = array_2d[y, x]
             else:
                 if y_resized >= 0 and y_resized < resized_shape[0] and x_resized >= 0 and x_resized < resized_shape[1]:
-                    resized_array[y_resized, x_resized] = 0.0
+                    resized_array[y_resized, x_resized] = pad_value
 
     return resized_array
 
@@ -188,7 +188,7 @@ def replace_noise_map_2d_values_where_image_2d_values_are_negative(image_2d, noi
     return noise_map_2d
 
 @decorator_util.jit()
-def pad_2d_array_for_binning_up_with_bin_up_factor(array_2d, bin_up_factor):
+def pad_2d_array_for_binning_up_with_bin_up_factor(array_2d, bin_up_factor, pad_value=0.0):
 
     shape = array_2d.shape
     shape_remainder = (shape[0] % bin_up_factor, shape[1] % bin_up_factor)
@@ -203,7 +203,8 @@ def pad_2d_array_for_binning_up_with_bin_up_factor(array_2d, bin_up_factor):
         shape_pad = (0, 0)
 
     binned_up_shape = (shape[0] + shape_pad[0], shape[1] + shape_pad[1])
-    return resized_array_2d_from_array_2d_and_resized_shape(array_2d=array_2d, resized_shape=binned_up_shape)
+    return resized_array_2d_from_array_2d_and_resized_shape(array_2d=array_2d, resized_shape=binned_up_shape,
+                                                            pad_value=pad_value)
 
 @decorator_util.jit()
 def bin_up_array_2d_using_mean(array_2d, bin_up_factor):
@@ -350,6 +351,58 @@ def bin_up_array_2d_using_sum(array_2d, bin_up_factor):
                     padded_y = y*bin_up_factor + y1
                     padded_x = x*bin_up_factor + x1
                     value += padded_array_2d[padded_y, padded_x]
+
+            binned_array_2d[y,x] = value
+
+    return binned_array_2d
+
+@decorator_util.jit()
+def bin_up_mask_2d(mask_2d, bin_up_factor):
+    """Bin up an array to coarser resolution, by binning up groups of pixels and using their sum value to determine \
+     the value of the new pixel.
+
+    If an array of shape (8,8) is input and the bin up size is 2, this would return a new array of size (4,4) where \
+    every pixel was the sum of each collection of 2x2 pixels on the (8,8) array.
+
+    If binning up the array leads to an edge being cut (e.g. a (9,9) array binned up by 2), an array is first \
+    extracted around the centre of that array.
+
+
+    Parameters
+    ----------
+    mask_2d : ndarray
+        The 2D array that is resized.
+    new_shape : (int, int)
+        The (y,x) new pixel dimension of the trimmed array.
+    origin : (int, int)
+        The oigin of the resized array, e.g. the central pixel around which the array is extracted.
+
+    Returns
+    -------
+    ndarray
+        The resized 2D array from the input 2D array.
+
+    Examples
+    --------
+    array_2d = np.ones((5,5))
+    resize_array = resize_array_2d(array_2d=array_2d, new_shape=(2,2), origin=(2, 2))
+    """
+
+    padded_array_2d = pad_2d_array_for_binning_up_with_bin_up_factor(array_2d=mask_2d, bin_up_factor=bin_up_factor,
+                                                                     pad_value=True)
+
+    binned_array_2d = np.zeros(shape=(padded_array_2d.shape[0] // bin_up_factor,
+                                      padded_array_2d.shape[1] // bin_up_factor))
+
+    for y in range(binned_array_2d.shape[0]):
+        for x in range(binned_array_2d.shape[1]):
+            value = True
+            for y1 in range(bin_up_factor):
+                for x1 in range(bin_up_factor):
+                    padded_y = y*bin_up_factor + y1
+                    padded_x = x*bin_up_factor + x1
+                    if padded_array_2d[padded_y, padded_x] == False:
+                        value = False
 
             binned_array_2d[y,x] = value
 
