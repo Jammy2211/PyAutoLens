a2fbc91d7d4cbac2003f3705d99b55e329e170cf
Richard Hayes <richardhayes777@gmail.com>
2019-03-22 15:29:21 +0000

    filter aggregations

--- autolens/aggregator.py
+++ autolens/aggregator.py
@@ -5,12 +5,18 @@ class Aggregation(object):
     def __init__(self, file_path):
         self.file_path = file_path
         with open(file_path) as f:
-            lines = f.readlines()
-            pairs = [line.replace("\n", "").split("=") for line in lines]
+            self.text = f.read()
+            pairs = [line.split("=") for line in self.text.split("\n")]
             value_dict = {pair[0]: pair[1] for pair in pairs}
             self.pipeline = value_dict["pipeline"]
             self.phase = value_dict["phase"]
             self.lens = value_dict["lens"]
+
+    def __str__(self):
+        return self.text
+
+    def __repr__(self):
+        return str(self)
 
 
 class Aggregator(object):
@@ -21,3 +27,7 @@ class Aggregator(object):
         for root, _, filenames in os.walk(directory):
             for filename in filter(lambda f: f == ".metadata", filenames):
                 self.aggregations.append(Aggregation(os.path.join(root, filename)))
+
+    def aggregations_with(self, **kwargs):
+        return [aggregation for aggregation in self.aggregations if
+                all([getattr(aggregation, key) == value for key, value in kwargs.items()])]
