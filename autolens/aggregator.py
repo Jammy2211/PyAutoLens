147edec8b5fb469135157dbb2055803dec9f0166
Richard Hayes <richardhayes777@gmail.com>
2019-03-22 16:13:36 +0000

    docs

--- autolens/aggregator.py
+++ autolens/aggregator.py
@@ -1,8 +1,30 @@
+#!/usr/bin/env python
+
+"""
+Filter and collate phase data in all subdirectories.
+
+Usage:
+
+./aggregator.py (root_directory) [pipeline=pipeline phase=phase lens=lens]
+
+Example:
+
+./aggregator.py ../output pipeline=lens_mass_x1_source_x1_positions
+"""
+
 import os
 
 
 class PhaseOutput(object):
-    def __init__(self, directory):
+    def __init__(self, directory: str):
+        """
+        Represents the output of a single phase. Comprises a .metadata file and other data files.
+
+        Parameters
+        ----------
+        directory
+            The directory of the phase
+        """
         self.directory = directory
         self.file_path = os.path.join(directory, ".metadata")
         with open(self.file_path) as f:
@@ -14,23 +36,39 @@ class PhaseOutput(object):
             self.lens = value_dict["lens"]
 
     @property
-    def model_results(self):
+    def model_results(self) -> str:
+        """
+        Reads the model.results file
+        """
         with open(os.path.join(self.directory, "model.results")) as f:
             return f.read()
 
     @property
-    def header(self):
+    def header(self) -> str:
+        """
+        A header created by joining the pipeline, phase and lens names
+        """
         return "/".join((self.pipeline, self.phase, self.lens))
 
     def __str__(self):
         return self.text
 
     def __repr__(self):
-        return str(self)
+        return "<PhaseOutput {}>".format(self)
 
 
 class Aggregator(object):
-    def __init__(self, directory):
+    def __init__(self, directory: str):
+        """
+        Class to aggregate phase results for all subdirectories in a given directory.
+
+        The whole directory structure is traversed and a Phase object created for each directory that contains a
+        .metadata file.
+
+        Parameters
+        ----------
+        directory
+        """
         self.directory = directory
         self.phases = []
 
@@ -38,11 +76,33 @@ class Aggregator(object):
             if ".metadata" in filenames:
                 self.phases.append(PhaseOutput(root))
 
-    def phases_with(self, **kwargs):
+    def phases_with(self, **kwargs) -> [PhaseOutput]:
+        """
+        Filters phases. If no arguments are passed all phases are returned. Arguments must be key value pairs, with
+        phase, lens or pipeline as the key.
+
+        Parameters
+        ----------
+        kwargs
+            Filters, e.g. pipeline=pipeline1
+        """
         return [phase for phase in self.phases if
                 all([getattr(phase, key) == value for key, value in kwargs.items()])]
 
-    def model_results(self, **kwargs):
+    def model_results(self, **kwargs) -> str:
+        """
+        Collates model results from all phases in the directory or some subset if filters are applied.
+
+        Parameters
+        ----------
+        kwargs
+            Filters, e.g. pipeline=pipeline1
+
+        Returns
+        -------
+        model_results
+            A string joining headers and results for all included phases.
+        """
         return "\n\n".join("{}\n\n{}".format(phase.header, phase.model_results) for phase in
                            self.phases_with(**kwargs))
 
@@ -54,7 +114,7 @@ if __name__ == "__main__":
     try:
         root_directory = argv[1]
     except IndexError:
-        print("Usage\n\naggregator.py (root_directory) [pipeline=pipeline phase=phase lens=lens]")
+        print("Usage:\n\naggregator.py (root_directory) [pipeline=pipeline phase=phase lens=lens]")
         exit(1)
     filter_dict = {pair[0]: pair[1] for pair in [line.split("=") for line in argv[2:]]}
 
