0c153bf898e09ddb7ca380862ef8025c5125c509
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-04-02 14:27:11 +0100

    Phase tagging added to pipeline.py, pipelines.

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
                       'numba',
                       'matplotlib==3.0.3',
                       'colorama',
-                      'autofit==0.18.1',
+                      'autofit==0.18.4',
                       'Cython==0.28.0',
                       'pyquad'
                       ],
