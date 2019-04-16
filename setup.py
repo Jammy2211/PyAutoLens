dac00467c36fc40ac0ab7f0e0b7d97838904241e
Richard Hayes <richardhayes777@gmail.com>
2019-04-16 17:42:33 +0100

    override pickle assertion

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
                       'numba',
                       'matplotlib==3.0.3',
                       'colorama',
-                      'autofit==0.18.8',
+                      'autofit==0.19.1',
                       'Cython==0.28.0',
                       'pyquad'
                       ],
