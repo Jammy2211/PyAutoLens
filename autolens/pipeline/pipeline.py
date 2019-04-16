84f3e8a9e610d0d20236b3a1627ed6dfb127327f
Richard Hayes <richardhayes777@gmail.com>
2019-04-16 17:42:33 +0100

    override pickle assertion

--- autolens/pipeline/pipeline.py
+++ autolens/pipeline/pipeline.py
@@ -2,16 +2,18 @@ from autofit.tools.pipeline import Pipeline
 
 
 class PipelineImaging(Pipeline):
-    def run(self, data, mask=None, positions=None, data_name=None):
+    def run(self, data, mask=None, positions=None, data_name=None, assert_optimizer_pickle_matches=True):
         def runner(phase, results):
-            return phase.run(data=data, results=results, mask=mask, positions=positions)
+            return phase.run(data=data, results=results, mask=mask, positions=positions,
+                             assert_optimizer_pickle_matches=assert_optimizer_pickle_matches)
 
         return self.run_function(runner, data_name)
 
 
 class PipelinePositions(Pipeline):
-    def run(self, positions, pixel_scale):
+    def run(self, positions, pixel_scale, assert_optimizer_pickle_matches=True):
         def runner(phase, results):
-            return phase.run(positions=positions, pixel_scale=pixel_scale, results=results)
+            return phase.run(positions=positions, pixel_scale=pixel_scale, results=results,
+                             assert_optimizer_pickle_matches=assert_optimizer_pickle_matches)
 
         return self.run_function(runner)
