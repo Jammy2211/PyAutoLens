6561dc196d3cad422a24d2bd65947e312e1fc538
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-05-15 12:05:53 +0100

    Added method to map a masked 1D grid to a 2D grid in mapping_util.

--- autolens/data/array/util/mapping_util.py
+++ autolens/data/array/util/mapping_util.py
@@ -238,6 +238,51 @@ def map_masked_1d_array_to_2d_array_from_array_1d_shape_and_one_to_two(array_1d,
 
 
 @decorator_util.jit()
+def map_masked_1d_grid_to_2d_grid_from_grid_1d_shape_and_one_to_two(grid_1d, shape, one_to_two):
+    """For a 1D array that was computed by mapping unmasked values from a 2D array of shape (rows, columns), map its \
+    values back to the original 2D array where masked values are set to zero.
+
+    This uses a 1D array 'one_to_two' where each index gives the 2D pixel indexes of the 1D array's unmasked pixels, \
+    for example:
+
+    - If one_to_two[0] = [0,0], the first value of the 1D array maps to the pixel [0,0] of the 2D array.
+    - If one_to_two[1] = [0,1], the second value of the 1D array maps to the pixel [0,1] of the 2D array.
+    - If one_to_two[4] = [1,1], the fifth value of the 1D array maps to the pixel [1,1] of the 2D array.
+
+    Parameters
+     ----------
+    grid_1d : ndarray
+        The 1D array of values which are mapped to a 2D array.
+    shape : (int, int)
+        The shape of the 2D array which the pixels are defined on.
+    one_to_two : ndarray
+        An array describing the 2D array index that every 1D array index maps too.
+
+    Returns
+    --------
+    ndarray
+        A 2D array of values mapped from the 1D array with dimensions shape.
+
+    Examples
+    --------
+    one_to_two = np.array([[0,1], [1,0], [1,1], [1,2], [2,1]])
+
+    array_1d = np.array([[2.0, 4.0, 5.0, 6.0, 8.0])
+
+    array_2d = map_masked_1d_array_to_2d_array_from_array_1d_shape_and_one_to_two(array_1d=array_1d, shape=(3,3),
+                                                                                  one_to_two=one_to_two)
+    """
+
+    grid_2d = np.zeros((shape[0], shape[1], 2))
+
+    for index in range(len(one_to_two)):
+        grid_2d[one_to_two[index, 0], one_to_two[index, 1], 0] = grid_1d[index, 0]
+        grid_2d[one_to_two[index, 0], one_to_two[index, 1], 1] = grid_1d[index, 1]
+
+    return grid_2d
+
+
+@decorator_util.jit()
 def map_unmasked_1d_array_to_2d_array_from_array_1d_and_shape(array_1d, shape):
     """For a 1D array that was flattened from a 2D array of shape (rows, columns), map its values back to the \
     original 2D array.
