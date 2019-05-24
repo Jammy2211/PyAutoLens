80fb50549ad7f86dbfacdecde2318f4b0681ecd6
Richard Hayes <richardhayes777@gmail.com>
2019-05-24 16:20:21 +0100

    lens source name galaxy tuples

--- test/unit/pipeline/test_hyper.py
+++ test/unit/pipeline/test_hyper.py
@@ -1,17 +1,27 @@
+import pytest
 from astropy import cosmology as cosmo
 
 from autofit.mapper import model
 from autolens.model import galaxy as g
 from autolens.model.profiles import light_profiles as lp
+from autolens.model.profiles import mass_profiles as mp
 from autolens.pipeline import phase as ph
 
 
-class TestImagePassing(object):
-    def test_galaxy_dict(self):
-        lens_galaxies = model.ModelInstance()
-        galaxy = g.Galaxy(light=lp.EllipticalLightProfile(), redshift=1.0)
-        lens_galaxies.lens = galaxy
+@pytest.fixture(name="lens_galaxy")
+def make_lens_galaxy():
+    return g.Galaxy(mass=mp.EllipticalMassProfile(), redshift=1.0)
 
+
+@pytest.fixture(name="lens_galaxies")
+def make_lens_galaxies(lens_galaxy):
+    lens_galaxies = model.ModelInstance()
+    lens_galaxies.lens = lens_galaxy
+    return lens_galaxies
+
+
+class TestImagePassing(object):
+    def test_lens_galaxy_dict(self, lens_galaxy, lens_galaxies):
         instance = model.ModelInstance()
         instance.lens_galaxies = lens_galaxies
 
@@ -19,4 +29,22 @@ class TestImagePassing(object):
                                           ph.LensPlanePhase.Analysis(lens_data=None, cosmology=cosmo.Planck15,
                                                                      positions_threshold=1.0), None)
 
-        assert result.name_galaxy_tuples == [("lens", galaxy)]
+        assert result.name_galaxy_tuples == [("lens", lens_galaxy)]
+
+    def test_lens_source_galaxy_dict(self, lens_galaxy):
+        source_galaxies = model.ModelInstance()
+        lens_galaxies = model.ModelInstance()
+        source_galaxy = g.Galaxy(light=lp.EllipticalLightProfile(), redshift=1.0)
+        source_galaxies.source = source_galaxy
+        lens_galaxies.lens = lens_galaxy
+
+        instance = model.ModelInstance()
+        instance.source_galaxies = source_galaxies
+        instance.lens_galaxies = lens_galaxies
+
+        result = ph.LensSourcePlanePhase.Result(instance, 1.0, None, None,
+                                                ph.LensSourcePlanePhase.Analysis(lens_data=None,
+                                                                                 cosmology=cosmo.Planck15,
+                                                                                 positions_threshold=1.0), None)
+
+        assert result.name_galaxy_tuples == [("lens", lens_galaxy), ("source", source_galaxy)]
