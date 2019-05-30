39defccfa3c8c6d442305ca46a301184a9eba159
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-05-30 16:39:33 +0100

    Made Mock objects of image, noise map and psf for lens fitting tests.

--- test/unit/mock/mock_lens_data.py
+++ test/unit/mock/mock_lens_data.py
@@ -7,36 +7,19 @@ from autolens.data.array import mask as msk
 from test.unit.mock.mock_grids import MockBorders
 from autolens.model.inversion import convolution as inversion_convolution
 
+from test.unit.mock.mock_ccd import MockImage, MockNoiseMap, MockPSF
+from test.unit.mock.mock_mask import MockMask, MockBlurringMask
+
 class MockLensData(object):
 
     def __init__(self):
 
         self.pixel_scale = 2.0
 
-        self.mask_2d = msk.Mask(np.array([[True, True, True, True, True, True],
-                                          [True, True, True, True, True, True],
-                                          [True, False, False, False, True, True],
-                                          [True, False, False, False, True, True],
-                                          [True, False, False, False, True, True],
-                                          [True, True, True, True, True, True]]), pixel_scale=self.pixel_scale)
-
-        self.image_2d = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                                  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                                  [0.0, 1.0, 2.0, 3.0, 0.0, 0.0],
-                                  [0.0, 4.0, 5.0, 6.0, 0.0, 0.0],
-                                  [0.0, 7.0, 8.0, 9.0, 0.0, 0.0],
-                                  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
-
-        self.noise_map_2d = np.array([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                                      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                                      [0.0, 1.0, 1.0, 1.0, 0.0, 0.0],
-                                      [0.0, 1.0, 1.0, 1.0, 0.0, 0.0],
-                                      [0.0, 1.0, 1.0, 1.0, 0.0, 0.0],
-                                      [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
-
-        self.psf = ccd.PSF(array=np.array([[1.0, 5.0, 9.0],
-                                           [2.0, 5.0, 1.0],
-                                           [3.0, 4.0, 0.0]]), pixel_scale=self.pixel_scale)
+        self.mask_2d = MockMask()
+        self.image_2d = MockImage()
+        self.noise_map_2d = MockNoiseMap()
+        self.psf = MockPSF()
 
         self.sub_grid_size = 2
 
@@ -52,12 +35,7 @@ class MockLensData(object):
         self.noise_map_1d = self.mask_2d.map_2d_array_to_masked_1d_array(array_2d=self.noise_map_2d)
         self.mask_1d = self.mask_2d.map_2d_array_to_masked_1d_array(array_2d=self.mask_2d)
 
-        self.blurring_mask = msk.Mask(array=np.array([[True,  True,  True,  True,  True, True],
-                                                     [False, False, False, False, False, True],
-                                                     [False,  True,  True,  True, False, True],
-                                                     [False,  True,  True,  True, False, True],
-                                                     [False,  True,  True,  True, False, True],
-                                                     [False, False, False, False, False,  True]]), pixel_scale=self.pixel_scale)
+        self.blurring_mask = MockBlurringMask()
 
         self.convolver_image = convolution.ConvolverImage(mask=self.mask_2d, blurring_mask=self.blurring_mask,
                                                           psf=self.psf)
