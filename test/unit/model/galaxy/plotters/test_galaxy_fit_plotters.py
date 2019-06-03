95038f68356d7bf3b1251cc5dc81fba803bf14bd
Richard Hayes <richardhayes777@gmail.com>
2019-06-03 09:25:20 +0100

    fixed imports

--- test/unit/model/galaxy/plotters/test_galaxy_fit_plotters.py
+++ test/unit/model/galaxy/plotters/test_galaxy_fit_plotters.py
@@ -1,19 +1,6 @@
-import numpy as np
-
 from autolens.model.galaxy.plotters import galaxy_fit_plotters
 from test.fixtures import *
 
-from test.unit.fixtures.data.ccd import positions_5x5
-from test.unit.fixtures.data.mask import mask_5x5
-from test.unit.fixtures.data.grids import regular_grid_5x5, sub_grid_5x5, blurring_grid_5x5, grid_stack_5x5
-from test.unit.fixtures.model.profiles import lp_0, mp_0
-from test.unit.fixtures.model.galaxy import gal_x1_lp, gal_x1_mp
-from test.unit.fixtures.model.galaxy_data import gal_fit_data_5x5_intensities, gal_fit_data_5x5_convergence, \
-    gal_fit_data_5x5_potential, gal_fit_data_5x5_deflections_y, gal_fit_data_5x5_deflections_x
-from test.unit.fixtures.model.galaxy_fit import gal_fit_5x5_intensities, gal_fit_5x5_convergence, \
-    gal_fit_5x5_potential, gal_fit_5x5_deflections_y, gal_fit_5x5_deflections_x
-
-import numpy as np
 
 @pytest.fixture(name='galaxy_fitting_plotter_path')
 def make_galaxy_fitting_plotter_setup():
@@ -22,7 +9,6 @@ def make_galaxy_fitting_plotter_setup():
 
 def test__fit_sub_plot__galaxy_intensities(gal_fit_5x5_intensities, positions_5x5, plot_patch,
                                            galaxy_fitting_plotter_path):
-
     galaxy_fit_plotters.plot_fit_subplot(fit=gal_fit_5x5_intensities,
                                          should_plot_mask=True, extract_array_from_mask=True, zoom_around_mask=True,
                                          positions=positions_5x5, cb_tick_values=[1.0], cb_tick_labels=['1.0'],
@@ -34,7 +20,6 @@ def test__fit_sub_plot__galaxy_intensities(gal_fit_5x5_intensities, positions_5x
 def test__fit_sub_plot__galaxy_convergence(gal_fit_5x5_convergence, mask_5x5, positions_5x5,
                                            plot_patch,
                                            galaxy_fitting_plotter_path):
-
     galaxy_fit_plotters.plot_fit_subplot(fit=gal_fit_5x5_convergence,
                                          should_plot_mask=True, extract_array_from_mask=True, zoom_around_mask=True,
                                          positions=positions_5x5, cb_tick_values=[1.0], cb_tick_labels=['1.0'],
@@ -45,7 +30,6 @@ def test__fit_sub_plot__galaxy_convergence(gal_fit_5x5_convergence, mask_5x5, po
 
 def test__fit_sub_plot__galaxy_potential(gal_fit_5x5_potential, mask_5x5, positions_5x5, plot_patch,
                                          galaxy_fitting_plotter_path):
-
     galaxy_fit_plotters.plot_fit_subplot(fit=gal_fit_5x5_potential,
                                          should_plot_mask=True, extract_array_from_mask=True, zoom_around_mask=True,
                                          positions=positions_5x5, cb_tick_values=[1.0], cb_tick_labels=['1.0'],
@@ -56,7 +40,6 @@ def test__fit_sub_plot__galaxy_potential(gal_fit_5x5_potential, mask_5x5, positi
 
 def test__fit_sub_plot__galaxy_deflections_y_(gal_fit_5x5_deflections_y, mask_5x5, positions_5x5, plot_patch,
                                               galaxy_fitting_plotter_path):
-
     galaxy_fit_plotters.plot_fit_subplot(fit=gal_fit_5x5_deflections_y,
                                          should_plot_mask=True, extract_array_from_mask=True, zoom_around_mask=True,
                                          positions=positions_5x5, cb_tick_values=[1.0], cb_tick_labels=['1.0'],
@@ -67,7 +50,6 @@ def test__fit_sub_plot__galaxy_deflections_y_(gal_fit_5x5_deflections_y, mask_5x
 
 def test__fit_sub_plot__galaxy_deflections_x(gal_fit_5x5_deflections_x, mask_5x5, positions_5x5, plot_patch,
                                              galaxy_fitting_plotter_path):
-
     galaxy_fit_plotters.plot_fit_subplot(fit=gal_fit_5x5_deflections_x,
                                          should_plot_mask=True, extract_array_from_mask=True, zoom_around_mask=True,
                                          positions=positions_5x5, cb_tick_values=[1.0], cb_tick_labels=['1.0'],
