ca3e11f6843d1bff77f428559471507b9d90afe7
Richard Hayes <richardhayes777@gmail.com>
2019-06-03 15:08:15 +0100

    bumped AutoFit

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
                       'numba',
                       'matplotlib==3.0.3',
                       'colorama',
-                      'autofit==0.24.0',
+                      'autofit==0.24.4',
                       'Cython==0.28.0',
                       'pyquad'
                       ],
