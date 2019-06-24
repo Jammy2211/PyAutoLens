2f705e3754dec9646427839a51af8fc833b9ea56
Richard Hayes <richardhayes777@gmail.com>
2019-06-24 17:53:50 +0100

    redundant assert optimizer argument

--- autolens/pipeline/pipeline.py
+++ autolens/pipeline/pipeline.py
@@ -2,25 +2,22 @@ import autofit as af
 
 
 class PipelineImaging(af.Pipeline):
-    def run(self, data, mask=None, positions=None, data_name=None,
-            assert_optimizer_pickle_matches=False):
+    def run(self, data, mask=None, positions=None, data_name=None):
         def runner(phase, results):
             return phase.run(data=data, results=results, mask=mask, positions=positions)
 
         return self.run_function(
             runner,
-            data_name,
-            assert_optimizer_pickle_matches=assert_optimizer_pickle_matches
+            data_name
         )
 
 
 class PipelinePositions(af.Pipeline):
-    def run(self, positions, pixel_scale, assert_optimizer_pickle_matches=True):
+    def run(self, positions, pixel_scale):
         def runner(phase, results):
             return phase.run(positions=positions, pixel_scale=pixel_scale,
                              results=results)
 
         return self.run_function(
-            runner,
-            assert_optimizer_pickle_matches=assert_optimizer_pickle_matches
+            runner
         )
