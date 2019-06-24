e364cb7f0aafc27ecc257b4b903f4de272447071
Richard Hayes <richardhayes777@gmail.com>
2019-06-24 15:19:55 +0100

    autofit==26

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
                       'numba',
                       'matplotlib==3.0.3',
                       'colorama',
-                      'autofit==0.25.5',
+                      'autofit==0.26.0',
                       'Cython==0.28.0',
                       'pyquad'
                       ],
