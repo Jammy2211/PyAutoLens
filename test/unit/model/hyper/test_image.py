83094638e3807b2f0f08c64f37141bcb903d34b1
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-07-04 15:03:41 +0100

    Added hyper image sky / hyper background noise scaling to lens_fit.

--- test/unit/model/hyper/test_image.py
+++ test/unit/model/hyper/test_image.py
@@ -2,37 +2,34 @@ import numpy as np
 
 from autolens.model.hyper import image as hi
 
-class TestHyperImage(object):
+class TestHyperImageSky(object):
 
-    class TestSkyScale(object):
+    def test__scale_sky_in_image__increases_all_image_values(self):
 
-        def test__scale_sky_in_image__increases_all_image_values(self):
-            image = np.array([1.0, 2.0, 3.0])
+        image = np.array([1.0, 2.0, 3.0])
 
-            hyper_image = hi.HyperImage(background_sky_scale=10.0)
+        hyper_sky = hi.HyperImageSky(background_sky_scale=10.0)
 
-            scaled_image = hyper_image.sky_scaled_hyper_image_from_image(image=image)
+        scaled_image = hyper_sky.hyper_image_from_image(image=image)
 
-            assert (scaled_image == np.array([11.0, 12.0, 13.0])).all()
+        assert (scaled_image == np.array([11.0, 12.0, 13.0])).all()
 
-    class TestScaledNoise(object):
+class TestHyperNoiseMapBackground(object):
 
-        def test__scaled_background_noise__adds_to_input_noise(self):
-            noise = np.array([1.0, 2.0, 3.0])
-            background_noise = np.ones((3))
+    def test__scaled_background_noise__adds_to_input_noise(self):
 
-            hyper_image = hi.HyperImage(background_noise_scale=2.0)
+        noise_map = np.array([1.0, 2.0, 3.0])
 
-            scaled_noise = hyper_image.hyper_noise_from_background_noise(noise=noise, background_noise=background_noise)
+        hyper_background_noise_map = hi.HyperNoiseMapBackground(background_noise_scale=2.0)
 
-            assert (scaled_noise == np.array([3.0, 4.0, 5.0])).all()
+        hyper_noise_map = hyper_background_noise_map.hyper_noise_map_from_noise_map(
+            noise_map=noise_map)
 
-        def test__variable_background_noise__adds_to_input_noise_different_for_each_value(self):
-            noise = np.array([1.0, 2.0, 3.0])
-            background_noise = np.array([5.0, 1.0, 3.0])
+        assert (hyper_noise_map == np.array([3.0, 4.0, 5.0])).all()
 
-            hyper_image = hi.HyperImage(background_noise_scale=3.0)
+        hyper_noise_map_background = hi.HyperNoiseMapBackground(background_noise_scale=3.0)
 
-            scaled_noise = hyper_image.hyper_noise_from_background_noise(noise=noise, background_noise=background_noise)
+        scaled_noise = hyper_noise_map_background.hyper_noise_map_from_noise_map(
+            noise_map=noise_map)
 
-            assert (scaled_noise == np.array([16.0, 5.0, 12.0])).all()
+        assert (scaled_noise == np.array([4.0, 5.0, 6.0])).all()
