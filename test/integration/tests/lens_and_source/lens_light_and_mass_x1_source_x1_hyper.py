fbc31e189322d9f9ce421abf637b8c9df27f1c47
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-07-15 11:29:45 +0100

    Hacking fix to LensDataFit by passing grid_stack

--- test/integration/tests/lens_and_source/lens_light_and_mass_x1_source_x1_hyper.py
+++ test/integration/tests/lens_and_source/lens_light_and_mass_x1_source_x1_hyper.py
@@ -7,7 +7,6 @@ from autolens.model.galaxy import galaxy as g
 from autolens.model.galaxy import galaxy_model as gm
 from autolens.model.profiles import light_profiles as lp, mass_profiles as mp
 from autolens.pipeline.phase import phase_imaging
-from autolens.pipeline.phase import phase_extensions
 from autolens.pipeline import pipeline as pl
 from test.integration import integration_util
 from test.simulation import simulation_util
@@ -37,22 +36,26 @@ def make_pipeline(test_name):
 
     phase1 = phase_imaging.LensSourcePlanePhase(
         phase_name='phase_1', phase_folders=[test_type, test_name],
-        lens_galaxies=dict(lens=gm.GalaxyModel(redshift=0.5, light=lp.SphericalDevVaucouleurs, mass=mp.EllipticalIsothermal)),
-        source_galaxies=dict(source=gm.GalaxyModel(redshift=1.0, light=lp.EllipticalSersic)),
+        lens_galaxies=dict(
+            lens=gm.GalaxyModel(
+                redshift=0.5,
+                light=lp.SphericalDevVaucouleurs,
+                mass=mp.EllipticalIsothermal)),
+        source_galaxies=dict(
+            source=gm.GalaxyModel(
+                redshift=1.0,
+                light=lp.EllipticalSersic)),
         optimizer_class=af.MultiNest)
 
     phase1.optimizer.const_efficiency_mode = True
     phase1.optimizer.n_live_points = 60
     phase1.optimizer.sampling_efficiency = 0.8
 
-    phase1h = phase_extensions.HyperGalaxyPhase(phase_name='phase_1_hyper', phase_folders=[test_type, test_name])
+    phase1 = phase1.extend_with_hyper_and_inversion_phases(hyper_galaxy=True)
 
     class HyperLensSourcePlanePhase(phase_imaging.LensSourcePlanePhase):
 
         def pass_priors(self, results):
-
-            self.lens_galaxies.lens.hyper_galaxy = results.from_phase('phase_1_hyper').\
-                constant.lens_galaxies.lens.hyper_galaxy
 
             self.lens_galaxies.lens.light = results.from_phase('phase_1').\
                 variable.lens_galaxies.lens.light
@@ -60,24 +63,35 @@ def make_pipeline(test_name):
             self.lens_galaxies.lens.mass = results.from_phase('phase_1').\
                 variable.lens_galaxies.lens.mass
 
-            self.source_galaxies.source.hyper_galaxy = results.from_phase('phase_1_hyper').\
-                constant.source_galaxies.source.hyper_galaxy
-
             self.source_galaxies.source.light = results.from_phase('phase_1').\
                 variable.source_galaxies.source.light
 
+            self.lens_galaxies.lens.hyper_galaxy = results.last.hyper_galaxy. \
+                constant.lens_galaxies.lens.hyper_galaxy
+            
+            self.source_galaxies.source.hyper_galaxy = results.last.hyper_galaxy. \
+                constant.source_galaxies.source.hyper_galaxy
+
     phase2 = HyperLensSourcePlanePhase(
         phase_name='phase_2', phase_folders=[test_type, test_name],
-        lens_galaxies=dict(lens=gm.GalaxyModel(redshift=0.5, light=lp.SphericalDevVaucouleurs,
-                           mass=mp.EllipticalIsothermal, hyper_galaxy=g.HyperGalaxy)),
-        source_galaxies=dict(source=gm.GalaxyModel(redshift=1.0, light=lp.EllipticalSersic, hyper_galaxy=g.HyperGalaxy)),
+        lens_galaxies=dict(
+            lens=gm.GalaxyModel(
+                redshift=0.5,
+                light=lp.SphericalDevVaucouleurs,
+                mass=mp.EllipticalIsothermal,
+                hyper_galaxy=g.HyperGalaxy)),
+        source_galaxies=dict(
+            source=gm.GalaxyModel(
+                redshift=1.0,
+                light=lp.EllipticalSersic,
+                hyper_galaxy=g.HyperGalaxy)),
         optimizer_class=af.MultiNest)
 
     phase2.optimizer.const_efficiency_mode = True
     phase2.optimizer.n_live_points = 40
     phase2.optimizer.sampling_efficiency = 0.8
 
-    return pl.PipelineImaging(test_name, phase1, phase1h, phase2)
+    return pl.PipelineImaging(test_name, phase1, phase2)
 
 
 if __name__ == "__main__":
