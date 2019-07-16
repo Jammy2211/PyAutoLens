d671ee2a13931af2265bb58bfab3c4cbe3a0811c
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-07-16 22:22:48 +0100

    Positions, mask, now passed around HyperPhases so that they are used throughout.

--- autolens/pipeline/phase/phase_extensions/inversion_phase.py
+++ autolens/pipeline/phase/phase_extensions/inversion_phase.py
@@ -37,7 +37,7 @@ class VariableFixingHyperPhase(HyperPhase):
 
         return phase
 
-    def run_hyper(self, data, results=None, **kwargs):
+    def run_hyper(self, data, results=None, mask=None, positions=None, **kwargs):
         """
         Run the phase, overriding the optimizer's variable instance with one created to
         only fit pixelization hyperparameters.
@@ -51,7 +51,7 @@ class VariableFixingHyperPhase(HyperPhase):
         phase = self.make_hyper_phase()
         phase.optimizer.variable = variable
 
-        return phase.run(data, results=results, **kwargs)
+        return phase.run(data, results=results, mask=mask, positions=positions, **kwargs)
 
     def add_defaults(self, variable: af.ModelMapper):
         """
