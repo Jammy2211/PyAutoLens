a4312fb8a6359d814240817da635a86cb2704e81
Richard Hayes <richardhayes777@gmail.com>
2019-07-23 14:08:28 +0100

    moved test runner code to runner module

--- test/integration/tests/mock_nlo/test_lens_only.py
+++ test/integration/tests/mock_nlo/test_lens_only.py
@@ -1,46 +1,7 @@
-import math
-
-import autofit as af
 from test.integration.tests.lens_only import lens_x1_galaxy
 from test.integration.tests.lens_only import lens_x1_galaxy_hyper
 from test.integration.tests.lens_only import lens_x1_galaxy_link_param
-from test.integration.tests.lens_only import runner
-
-
-class MockNLO(af.NonLinearOptimizer):
-    def fit(self, analysis):
-        instance = self.variable.instance_from_prior_medians()
-        fit = analysis.fit(
-            instance
-        )
-        return af.Result(
-            instance,
-            fit,
-            self.variable,
-            gaussian_tuples=[
-                (
-                    prior.mean,
-                    prior.width
-                    if math.isfinite(
-                        prior.width
-                    )
-                    else 1.0
-                )
-                for prior
-                in sorted(
-                    self.variable.priors,
-                    key=lambda prior: prior.id
-                )
-            ]
-        )
-
-
-def run_a_mock(module):
-    runner.run(
-        module,
-        test_name=f"{module.test_name}_mock",
-        optimizer_class=MockNLO
-    )
+from test.integration.tests.lens_only.runner import run_a_mock
 
 
 class TestCase:
