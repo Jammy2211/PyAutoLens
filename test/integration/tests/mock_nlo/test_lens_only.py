444a5480d71c1cf2ed261e3d6f98e6b9b7780b52
Richard Hayes <richardhayes777@gmail.com>
2019-07-23 13:54:00 +0100

    added lens_x1_galaxy_link_param test

--- test/integration/tests/mock_nlo/test_lens_only.py
+++ test/integration/tests/mock_nlo/test_lens_only.py
@@ -3,6 +3,7 @@ import math
 import autofit as af
 from test.integration.tests.lens_only import lens_x1_galaxy
 from test.integration.tests.lens_only import lens_x1_galaxy_hyper
+from test.integration.tests.lens_only import lens_x1_galaxy_link_param
 from test.integration.tests.lens_only import runner
 
 
@@ -48,3 +49,6 @@ class TestCase:
 
     def test_lens_x1_galaxy_hyper(self):
         run_a_mock(lens_x1_galaxy_hyper)
+
+    def test_lens_x1_galaxy_link_param(self):
+        run_a_mock(lens_x1_galaxy_link_param)
