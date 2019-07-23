dc3274ba2f812b7df262e01f178545e4866a6d56
Richard Hayes <richardhayes777@gmail.com>
2019-07-23 14:06:24 +0100

    adapting integration test that demonstrated hyper galaxy prior model instantiation issue

--- test/integration/tests/lens_and_source_inversion/lens_mass_x1_source_x1_adaptive_weighted_hyper_constant_pass.py
+++ test/integration/tests/lens_and_source_inversion/lens_mass_x1_source_x1_adaptive_weighted_hyper_constant_pass.py
@@ -1,46 +1,33 @@
-import os
-
 import autofit as af
 from autolens.model.galaxy import galaxy_model as gm
 from autolens.model.inversion import pixelizations as pix, regularization as reg
-from autolens.pipeline.phase import phase_imaging
-from autolens.pipeline import pipeline as pl
 from autolens.model.profiles import light_profiles as lp
 from autolens.model.profiles import mass_profiles as mp
-from test.integration import integration_util
-from test.simulation import simulation_util
+from autolens.pipeline import pipeline as pl
+from autolens.pipeline.phase import phase_imaging
+from test.integration.tests.lens_only import runner
 
 test_type = "lens_and_source_inversion"
 test_name = "lens_mass_x1_source_x1_adaptive_weighted_hyper_constant_pass"
-
-test_path = "{}/../../".format(os.path.dirname(os.path.realpath(__file__)))
-output_path = test_path + "output/"
-config_path = test_path + "config"
-af.conf.instance = af.conf.Config(config_path=config_path, output_path=output_path)
+data_type = "no_lens_light_and_source_smooth"
+data_resolution = "LSST"
 
 
-def pipeline():
-
-    integration_util.reset_paths(test_name=test_name, output_path=output_path)
-    ccd_data = simulation_util.load_test_ccd_data(
-        data_type="no_lens_light_and_source_smooth", data_resolution="LSST"
-    )
-    pipeline = make_pipeline(test_name=test_name)
-    pipeline.run(data=ccd_data)
-
-
-def make_pipeline(test_name):
-
+def make_pipeline(
+        name,
+        phase_folders,
+        optimizer_class=af.MultiNest
+):
     phase1 = phase_imaging.LensSourcePlanePhase(
         phase_name="phase_1",
-        phase_folders=[test_type, test_name],
+        phase_folders=phase_folders,
         lens_galaxies=dict(
             lens=gm.GalaxyModel(redshift=0.5, mass=mp.EllipticalIsothermal)
         ),
         source_galaxies=dict(
             source=gm.GalaxyModel(redshift=1.0, light=lp.EllipticalSersic)
         ),
-        optimizer_class=af.MultiNest,
+        optimizer_class=optimizer_class
     )
 
     phase1.optimizer.const_efficiency_mode = True
@@ -51,8 +38,7 @@ def make_pipeline(test_name):
 
     class InversionPhase(phase_imaging.LensSourcePlanePhase):
         def pass_priors(self, results):
-
-            ## Lens Mass, SIE -> SIE, Shear -> Shear ###
+            # Lens Mass, SIE -> SIE, Shear -> Shear #
 
             self.lens_galaxies.lens = results.last.constant.lens_galaxies.lens
 
@@ -66,7 +52,7 @@ def make_pipeline(test_name):
 
     phase2 = InversionPhase(
         phase_name="phase_2",
-        phase_folders=[test_type, test_name],
+        phase_folders=phase_folders,
         lens_galaxies=dict(
             lens=gm.GalaxyModel(redshift=0.5, mass=mp.EllipticalIsothermal)
         ),
@@ -78,19 +64,21 @@ def make_pipeline(test_name):
             )
         ),
         inversion_pixel_limit=50,
-        optimizer_class=af.MultiNest,
+        optimizer_class=optimizer_class
     )
 
     phase2.optimizer.const_efficiency_mode = True
     phase2.optimizer.n_live_points = 30
     phase2.optimizer.sampling_efficiency = 0.8
 
-    phase2 = phase2.extend_with_multiple_hyper_phases(hyper_galaxy=True, inversion=True)
+    phase2 = phase2.extend_with_multiple_hyper_phases(
+        hyper_galaxy=True,
+        inversion=True
+    )
 
     class InversionPhase(phase_imaging.LensSourcePlanePhase):
         def pass_priors(self, results):
-
-            ## Lens Mass, SIE -> SIE, Shear -> Shear ###
+            # Lens Mass, SIE -> SIE, Shear -> Shear #
 
             self.lens_galaxies.lens = results.from_phase(
                 "phase_1"
@@ -108,7 +96,7 @@ def make_pipeline(test_name):
 
     phase3 = InversionPhase(
         phase_name="phase_3",
-        phase_folders=[test_type, test_name],
+        phase_folders=phase_folders,
         lens_galaxies=dict(
             lens=gm.GalaxyModel(redshift=0.5, mass=mp.EllipticalIsothermal)
         ),
@@ -120,17 +108,24 @@ def make_pipeline(test_name):
             )
         ),
         inversion_pixel_limit=50,
-        optimizer_class=af.MultiNest,
+        optimizer_class=optimizer_class
     )
 
     phase3.optimizer.const_efficiency_mode = True
     phase3.optimizer.n_live_points = 40
     phase3.optimizer.sampling_efficiency = 0.8
 
-    phase3 = phase3.extend_with_multiple_hyper_phases(hyper_galaxy=True, inversion=True)
+    phase3 = phase3.extend_with_multiple_hyper_phases(
+        hyper_galaxy=True,
+        inversion=True
+    )
 
-    return pl.PipelineImaging(test_name, phase1, phase2, phase3)
+    return pl.PipelineImaging(name, phase1, phase2, phase3)
 
 
 if __name__ == "__main__":
-    pipeline()
+    import sys
+
+    runner.run(
+        sys.modules[__name__]
+    )
