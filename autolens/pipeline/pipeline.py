19d9386a37f6cb643fc6534d173d53869c122ecf
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-07-30 10:49:56 +0100

    Added pipeline settings for controlling high level behaviour of pipelines.

--- autolens/pipeline/pipeline.py
+++ autolens/pipeline/pipeline.py
@@ -1,5 +1,66 @@
 import autofit as af
 from autolens import exc
+from autolens.model.inversion import pixelizations as pix
+from autolens.model.inversion import regularization as reg
+
+
+class PipelineSettings(object):
+    def __init__(
+        self,
+        include_shear=False,
+        fix_lens_light=False,
+        pixelization=pix.VoronoiMagnification,
+        regularization=reg.Constant,
+        align_bulge_disk_centre=False,
+        align_bulge_disk_phi=False,
+        align_bulge_disk_axis_ratio=False,
+        align_light_dark_centre=True,
+        align_bulge_dark_centre=True,
+    ):
+
+        self.include_shear = include_shear
+        self.fix_lens_light = fix_lens_light
+        self.pixelization = pixelization
+        self.regularization = regularization
+        self.align_bulge_disk_centre = align_bulge_disk_centre
+        self.align_bulge_disk_phi = align_bulge_disk_phi
+        self.align_bulge_disk_axis_ratio = align_bulge_disk_axis_ratio
+        self.align_light_dark_centre = align_light_dark_centre
+        self.align_bulge_dark_centre = align_bulge_dark_centre
+
+
+class PipelineSettingsHyper(PipelineSettings):
+    def __init__(
+        self,
+        include_shear=False,
+        fix_lens_light=False,
+        pixelization=pix.VoronoiBrightnessImage,
+        regularization=reg.AdaptiveBrightness,
+        align_bulge_disk_centre=False,
+        align_bulge_disk_phi=False,
+        align_bulge_disk_axis_ratio=False,
+        align_light_dark_centre=True,
+        align_bulge_dark_centre=True,
+        hyper_galaxies=True,
+        hyper_background_sky=False,
+        hyper_background_noise=False,
+    ):
+
+        super(PipelineSettingsHyper, self).__init__(
+            include_shear=include_shear,
+            fix_lens_light=fix_lens_light,
+            pixelization=pixelization,
+            regularization=regularization,
+            align_bulge_disk_centre=align_bulge_disk_centre,
+            align_bulge_disk_phi=align_bulge_disk_phi,
+            align_bulge_disk_axis_ratio=align_bulge_disk_axis_ratio,
+            align_light_dark_centre=align_light_dark_centre,
+            align_bulge_dark_centre=align_bulge_dark_centre,
+        )
+
+        self.hyper_galaxies = (hyper_galaxies,)
+        self.hyper_background_sky = (hyper_background_sky,)
+        self.hyper_background_noise = hyper_background_noise
 
 
 class PipelineImaging(af.Pipeline):
