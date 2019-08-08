eae906459f458c26a95a3d355b9506e923f8fb2e
Richard Hayes <richardhayes777@gmail.com>
2019-08-08 15:54:59 +0100

    only run combined when there is more than one hyper phase included

--- autolens/pipeline/phase/phase_extensions/__init__.py
+++ autolens/pipeline/phase/phase_extensions/__init__.py
@@ -76,7 +76,17 @@ class CombinedHyperPhase(HyperPhase):
             hyper_result = phase.run_hyper(data=data, results=results, **kwargs)
             setattr(result, phase.hyper_name, hyper_result)
 
-        setattr(result, self.hyper_name, self.run_hyper(data=data, results=results))
+        if len(self.hyper_phases) == 1:
+            setattr(
+                result,
+                self.hyper_name,
+                getattr(
+                    result,
+                    self.hyper_phases[0].hyper_name
+                )
+            )
+        else:
+            setattr(result, self.hyper_name, self.run_hyper(data=data, results=results))
         return result
 
     def combine_variables(self, result) -> af.ModelMapper:
