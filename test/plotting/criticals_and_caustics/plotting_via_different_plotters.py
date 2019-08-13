d383c1b101849ffebfbe979ebfc05b2ced43ed21
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-08-13 11:56:30 +0100

    Built mapping between marching squares grid and critical curves into grid module

--- test/plotting/criticals_and_caustics/plotting_via_different_plotters.py
+++ test/plotting/criticals_and_caustics/plotting_via_different_plotters.py
@@ -6,18 +6,18 @@ from autolens.model.profiles.plotters import profile_plotters
 from autolens.model.galaxy.plotters import galaxy_plotters
 
 grid_stack = grids.GridStack.from_shape_pixel_scale_and_sub_grid_size(
-    shape=(100, 100), pixel_scale=0.05, sub_grid_size=1
+    shape=(100, 100), pixel_scale=0.05, sub_grid_size=4
 )
 
 sis_mass_profile = mass_profiles.EllipticalIsothermal(
-    centre=(0.1, 0.1), einstein_radius=1.6, axis_ratio=0.7
+    centre=(1.0, 1.0), einstein_radius=1.6, axis_ratio=0.7
 )
 
 profile_plotters.plot_convergence(
     mass_profile=sis_mass_profile,
     grid=grid_stack.sub,
     plot_critical_curves=True,
-    plot_caustics=True,
+    plot_caustics=False,
 )
 
 # galaxy = g.Galaxy(mass=sis_mass_profile, redshift=1)
