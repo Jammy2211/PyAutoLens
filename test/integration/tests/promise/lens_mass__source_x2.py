78eba7665d707c5bd582ec95ea8d4089b4faedce
Richard Hayes <richardhayes777@gmail.com>
2019-08-27 18:26:51 +0100

    fixing integration

--- test/integration/tests/promise/lens_mass__source_x2.py
+++ test/integration/tests/promise/lens_mass__source_x2.py
@@ -1,8 +1,6 @@
 import autofit as af
-from autolens.model.galaxy import galaxy_model as gm
-from autolens.model.profiles import light_profiles as lp, mass_profiles as mp
-from autolens.pipeline import pipeline as pl
-from autolens.pipeline.phase import phase_imaging
+
+import autolens as al
 from test.integration.tests import runner
 
 test_type = "lens__source"
@@ -12,12 +10,12 @@ data_resolution = "LSST"
 
 
 def make_pipeline(name, phase_folders, optimizer_class=af.MultiNest):
-    phase1 = phase_imaging.PhaseImaging(
+    phase1 = al.PhaseImaging(
         phase_name="phase_1",
         phase_folders=phase_folders,
         galaxies=dict(
-            lens=gm.GalaxyModel(redshift=0.5, mass=al.mass_profiles.EllipticalIsothermal),
-            source_0=gm.GalaxyModel(redshift=1.0, sersic=al.EllipticalSersic),
+            lens=al.GalaxyModel(redshift=0.5, mass=al.mass_profiles.EllipticalIsothermal),
+            source_0=al.GalaxyModel(redshift=1.0, sersic=al.light_profiles.EllipticalSersic),
         ),
         optimizer_class=optimizer_class,
     )
@@ -26,13 +24,13 @@ def make_pipeline(name, phase_folders, optimizer_class=af.MultiNest):
     phase1.optimizer.n_live_points = 60
     phase1.optimizer.sampling_efficiency = 0.7
 
-    phase2 = phase_imaging.PhaseImaging(
+    phase2 = al.PhaseImaging(
         phase_name="phase_2",
         phase_folders=phase_folders,
         galaxies=dict(
             lens=phase1.result.variable.galaxies.lens,
             source_0=phase1.result.variable.galaxies.source_0,
-            source_1=gm.GalaxyModel(redshift=1.0, sersic=al.EllipticalSersic),
+            source_1=al.GalaxyModel(redshift=1.0, sersic=al.light_profiles.EllipticalSersic),
         ),
         optimizer_class=optimizer_class,
     )
@@ -41,7 +39,7 @@ def make_pipeline(name, phase_folders, optimizer_class=af.MultiNest):
     phase2.optimizer.n_live_points = 60
     phase2.optimizer.sampling_efficiency = 0.7
 
-    return pl.PipelineImaging(name, phase1, phase2)
+    return al.PipelineImaging(name, phase1, phase2)
 
 
 if __name__ == "__main__":
