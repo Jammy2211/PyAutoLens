fd0651958bb01de44222efbf32f77575b243fa1e
Richard Hayes <richardhayes777@gmail.com>
2019-08-27 15:05:51 +0100

    simulate and fit

--- autolens/__init__.py
+++ autolens/__init__.py
@@ -1,29 +1,36 @@
 from autolens import text_util
-from autolens.array import grids, mask
 from autolens.array.grids import Grid
 from autolens.array.mapping_util import grid_mapping_util
 from autolens.array.mapping_util import mask_mapping_util
 from autolens.array.mapping_util import mask_mapping_util, sparse_mapping_util
 from autolens.array.mapping_util import sparse_mapping_util
+from autolens.array.mask import Mask
 from autolens.array.util import array_util, grid_util, binning_util
 from autolens.array.util import binning_util, grid_util, mask_util
 from autolens.array.util import grid_util, mask_util
 from autolens.data import fourier_transform as ft
 from autolens.data.instrument import interferometer
+from autolens.data.instrument.abstract_data import PSF
+from autolens.data.instrument.ccd import CCDData, NoiseMap, PoissonNoiseMap, SimulatedCCDData, \
+    generate_poisson_noise, load_ccd_data_from_fits
 from autolens.data.plotters import ccd_plotters
 from autolens.data.plotters import data_plotters
 from autolens.data.plotters import interferometer_plotters
 from autolens.lens import ray_tracing, lens_fit
 from autolens.lens import ray_tracing as rt
+from autolens.lens.lens_data import LensData
+from autolens.lens.lens_fit import LensDataFit, LensTracerFit, InversionFit, LensInversionFit, \
+    LensProfileInversionFit, LensPositionFit, LensProfileFit
 from autolens.lens.plotters import lens_fit_plotters
 from autolens.lens.plotters import lens_plotter_util
 from autolens.lens.plotters import plane_plotters
 from autolens.lens.plotters import ray_tracing_plotters
-from autolens.model.galaxy import galaxy as g, galaxy_data as gd
-from autolens.model.galaxy import galaxy as g, galaxy_fit
-from autolens.model.galaxy import galaxy as g, galaxy_model as gm
-from autolens.model.galaxy import galaxy as g, galaxy_model as gp
+from autolens.lens.ray_tracing import Tracer
 from autolens.model.galaxy import galaxy_data as gd
+from autolens.model.galaxy.galaxy import Galaxy
+from autolens.model.galaxy.galaxy import HyperGalaxy
+from autolens.model.galaxy.galaxy import Redshift
+from autolens.model.galaxy.galaxy_model import GalaxyModel
 from autolens.model.galaxy.plotters import galaxy_fit_plotters
 from autolens.model.galaxy.plotters import galaxy_plotters
 from autolens.model.inversion import mappers
@@ -35,14 +42,16 @@ from autolens.model.inversion.util import inversion_util
 from autolens.model.inversion.util import mapper_util
 from autolens.model.profiles import geometry_profiles as gp
 from autolens.model.profiles.light_profiles import LightProfile, EllipticalLightProfile, EllipticalGaussian, \
-    SphericalGaussian, AbstractEllipticalSersic, EllipticalSersic, SphericalSersic as SphericalSersicLight, EllipticalExponential, \
+    SphericalGaussian, AbstractEllipticalSersic, EllipticalSersic, SphericalSersic as SphericalSersicLight, \
+    EllipticalExponential, \
     SphericalExponential, EllipticalDevVaucouleurs, SphericalDevVaucouleurs, EllipticalCoreSersic, \
     SphericalCoreSersic
 from autolens.model.profiles.mass_profiles import PointMass, EllipticalCoredPowerLaw, SphericalCoredPowerLaw, \
     EllipticalCoredIsothermal, SphericalCoredIsothermal, EllipticalPowerLaw, SphericalPowerLaw, \
     EllipticalIsothermal, SphericalIsothermal, EllipticalIsothermalKormann, EllipticalGeneralizedNFW, \
     SphericalGeneralizedNFW, SphericalTruncatedNFW, SphericalTruncatedNFWChallenge, EllipticalNFW, SphericalNFW, \
-    EllipticalSersic, SphericalSersic as SphericalSersicMass, EllipticalExponential, SphericalExponential, EllipticalDevVaucouleurs, \
+    EllipticalSersic, SphericalSersic as SphericalSersicMass, EllipticalExponential, SphericalExponential, \
+    EllipticalDevVaucouleurs, \
     SphericalDevVaucouleurs, EllipticalSersicRadialGradient, SphericalSersicRadialGradient, MassProfile, \
     EllipticalMassProfile, ExternalShear, MassSheet
 from autolens.model.profiles.plotters import profile_plotters
