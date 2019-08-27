fa9f7a354c04945108061a0985ef0fe0b2d68d5a
Richard Hayes <richardhayes777@gmail.com>
2019-08-27 09:08:30 +0100

    preparing integration test for promises

---
+++ test/integration/tests/promise/test_mock.py
@@ -0,0 +1,7 @@
+from test.integration.tests.promise import lens_mass__source_x2
+from test.integration.tests.runner import run_a_mock
+
+
+class TestCase:
+    def test_lens_mass__source_x2(self):
+        run_a_mock(lens_mass__source_x2)
