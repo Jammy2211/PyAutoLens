285a64a8c880c4158fadec3ea807d0e1d2111806
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-08-29 20:18:56 +0100

    sub pixel border now uses further pixel from mask centre and can be fitlered

---
+++ test/integration/tests/model_mapper/passing_parameter_priors.py
@@ -0,0 +1,51 @@
+import autofit as af
+import autolens as al
+from test.integration.tests import runner
+
+test_type = "model_mapper"
+test_name = "passing_none"
+data_type = "lens_light_dev_vaucouleurs"
+data_resolution = "LSST"
+
+
+def make_pipeline(name, phase_folders, optimizer_class=af.MultiNest):
+
+    phase1 = al.PhaseImaging(
+        phase_name="phase_1",
+        phase_folders=phase_folders,
+        galaxies=dict(
+            lens=al.GalaxyModel(
+                redshift=0.5, light=al.light_profiles.EllipticalSersic, light_1=None
+            )
+        ),
+        optimizer_class=optimizer_class,
+    )
+
+    phase1.optimizer.const_efficiency_mode = True
+    phase1.optimizer.n_live_points = 20
+    phase1.optimizer.sampling_efficiency = 0.8
+
+    phase2 = al.PhaseImaging(
+        phase_name="phase_2",
+        phase_folders=phase_folders,
+        galaxies=dict(
+            lens=al.GalaxyModel(
+                redshift=0.5,
+                light=al.light_profiles.EllipticalSersic,
+                light_1=phase1.result.variable.galaxies.lens.light_1,
+            )
+        ),
+        optimizer_class=optimizer_class,
+    )
+
+    phase2.optimizer.const_efficiency_mode = True
+    phase2.optimizer.n_live_points = 20
+    phase2.optimizer.sampling_efficiency = 0.8
+
+    return al.PipelineImaging(name, phase1, phase2)
+
+
+if __name__ == "__main__":
+    import sys
+
+    runner.run(sys.modules[__name__])
