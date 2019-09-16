27ee0f8b8bee0145cad5af2e53315169a2449e1c
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-09-16 14:42:57 +0700

    Added interferometer transformer method to tracer.

--- autolens/lens/plane.py
+++ autolens/lens/plane.py
@@ -842,12 +842,12 @@ class AbstractPlaneData(AbstractPlaneLensing):
 
     def visibilities_from_grid_and_transformer(self, grid, transformer):
 
-        profile_image_plane_image_1d = self.profile_image_from_grid(
+        profile_image_1d = self.profile_image_from_grid(
             grid=grid, return_in_2d=False, return_binned=True
         )
 
         return transformer.visibilities_from_image_1d(
-            image_1d=profile_image_plane_image_1d
+            image_1d=profile_image_1d
         )
 
     def pixelization_grid_from_grid(self, grid):
