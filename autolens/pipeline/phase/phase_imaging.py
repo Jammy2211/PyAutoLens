5c837176d24572953822e568f655ed99bfcbb770
Richard Hayes <richardhayes777@gmail.com>
2019-09-23 14:46:02 +0100

    attempted to separate fitting logic from phase logic

--- autolens/pipeline/phase/phase_imaging.py
+++ autolens/pipeline/phase/phase_imaging.py
@@ -8,8 +8,91 @@ from autolens.lens import lens_fit
 from autolens.model.galaxy import galaxy as g
 from autolens.pipeline import phase_tagging
 from autolens.pipeline.phase import phase_extensions
-from autolens.pipeline.phase.phase_data import PhaseData
+from autolens.pipeline.phase.phase_data import PhaseData, MetaDataFit
 from autolens.plotters import visualizer
+
+
+class MetaImagingFit(MetaDataFit):
+    def __init__(
+            self,
+            variable,
+            sub_size=2,
+            is_hyper_phase=False,
+            signal_to_noise_limit=None,
+            positions_threshold=None,
+            mask_function=None,
+            inner_mask_radii=None,
+            pixel_scale_interpolation_grid=None,
+            pixel_scale_binned_cluster_grid=None,
+            inversion_uses_border=True,
+            inversion_pixel_limit=None,
+            psf_shape=None,
+            bin_up_factor=None
+    ):
+        super().__init__(
+            variable=variable,
+            sub_size=sub_size,
+            is_hyper_phase=is_hyper_phase,
+            signal_to_noise_limit=signal_to_noise_limit,
+            positions_threshold=positions_threshold,
+            mask_function=mask_function,
+            inner_mask_radii=inner_mask_radii,
+            pixel_scale_interpolation_grid=pixel_scale_interpolation_grid,
+            pixel_scale_binned_cluster_grid=pixel_scale_binned_cluster_grid,
+            inversion_uses_border=inversion_uses_border,
+            inversion_pixel_limit=inversion_pixel_limit,
+        )
+        self.psf_shape = psf_shape
+        self.bin_up_factor = bin_up_factor
+
+    def data_fit_from(
+            self,
+            data,
+            mask,
+            positions,
+            results,
+            modified_image
+    ):
+        mask = self.setup_phase_mask(data=data, mask=mask)
+
+        self.check_positions(positions=positions)
+
+        if self.uses_cluster_inversion:
+            pixel_scale_binned_grid = self.pixel_scale_binned_grid_from_mask(mask=mask)
+        else:
+            pixel_scale_binned_grid = None
+
+        preload_pixelization_grids_of_planes = self.preload_pixelization_grids_of_planes_from_results(
+            results=results
+        )
+
+        lens_imaging_data = ld.LensImagingData(
+            imaging_data=data,
+            mask=mask,
+            trimmed_psf_shape=self.psf_shape,
+            positions=positions,
+            positions_threshold=self.positions_threshold,
+            pixel_scale_interpolation_grid=self.pixel_scale_interpolation_grid,
+            pixel_scale_binned_grid=pixel_scale_binned_grid,
+            hyper_noise_map_max=self.hyper_noise_map_max,
+            inversion_pixel_limit=self.inversion_pixel_limit,
+            inversion_uses_border=self.inversion_uses_border,
+            preload_pixelization_grids_of_planes=preload_pixelization_grids_of_planes,
+        )
+
+        lens_imaging_data = lens_imaging_data.new_lens_imaging_data_with_modified_image(
+            modified_image=modified_image
+        )
+
+        if self.signal_to_noise_limit is not None:
+            lens_imaging_data = lens_imaging_data.new_lens_imaging_data_with_signal_to_noise_limit(
+                signal_to_noise_limit=self.signal_to_noise_limit
+            )
+
+        if self.bin_up_factor is not None:
+            lens_imaging_data = lens_imaging_data.new_lens_imaging_data_with_binned_up_imaging_data_and_mask(
+                bin_up_factor=self.bin_up_factor
+            )
 
 
 class PhaseImaging(PhaseData):
@@ -18,26 +101,26 @@ class PhaseImaging(PhaseData):
     hyper_background_noise = af.PhaseProperty("hyper_background_noise")
 
     def __init__(
-        self,
-        phase_name,
-        phase_folders=tuple(),
-        galaxies=None,
-        hyper_image_sky=None,
-        hyper_background_noise=None,
-        optimizer_class=af.MultiNest,
-        cosmology=cosmo.Planck15,
-        sub_size=2,
-        signal_to_noise_limit=None,
-        bin_up_factor=None,
-        psf_shape=None,
-        positions_threshold=None,
-        mask_function=None,
-        inner_mask_radii=None,
-        pixel_scale_interpolation_grid=None,
-        pixel_scale_binned_cluster_grid=None,
-        inversion_uses_border=True,
-        inversion_pixel_limit=None,
-        auto_link_priors=False,
+            self,
+            phase_name,
+            phase_folders=tuple(),
+            galaxies=None,
+            hyper_image_sky=None,
+            hyper_background_noise=None,
+            optimizer_class=af.MultiNest,
+            cosmology=cosmo.Planck15,
+            sub_size=2,
+            signal_to_noise_limit=None,
+            bin_up_factor=None,
+            psf_shape=None,
+            positions_threshold=None,
+            mask_function=None,
+            inner_mask_radii=None,
+            pixel_scale_interpolation_grid=None,
+            pixel_scale_binned_cluster_grid=None,
+            inversion_uses_border=True,
+            inversion_pixel_limit=None,
+            auto_link_priors=False,
     ):
 
         """
@@ -67,27 +150,29 @@ class PhaseImaging(PhaseData):
             pixel_scale_binned_cluster_grid=pixel_scale_binned_cluster_grid,
         )
 
-        super(PhaseImaging, self).__init__(
+        super().__init__(
             phase_name=phase_name,
             phase_tag=phase_tag,
+            meta_data_fit=MetaImagingFit(
+                variable=self.variable,
+                bin_up_factor=bin_up_factor,
+                psf_shape=psf_shape,
+                sub_size=sub_size,
+                signal_to_noise_limit=signal_to_noise_limit,
+                positions_threshold=positions_threshold,
+                mask_function=mask_function,
+                inner_mask_radii=inner_mask_radii,
+                pixel_scale_interpolation_grid=pixel_scale_interpolation_grid,
+                pixel_scale_binned_cluster_grid=pixel_scale_binned_cluster_grid,
+                inversion_uses_border=inversion_uses_border,
+                inversion_pixel_limit=inversion_pixel_limit,
+            ),
             phase_folders=phase_folders,
             galaxies=galaxies,
-            sub_size=sub_size,
-            signal_to_noise_limit=signal_to_noise_limit,
-            positions_threshold=positions_threshold,
-            mask_function=mask_function,
-            inner_mask_radii=inner_mask_radii,
-            pixel_scale_interpolation_grid=pixel_scale_interpolation_grid,
-            pixel_scale_binned_cluster_grid=pixel_scale_binned_cluster_grid,
-            inversion_uses_border=inversion_uses_border,
-            inversion_pixel_limit=inversion_pixel_limit,
             optimizer_class=optimizer_class,
             cosmology=cosmology,
             auto_link_priors=auto_link_priors,
         )
-
-        self.bin_up_factor = bin_up_factor
-        self.psf_shape = psf_shape
 
         self.hyper_image_sky = hyper_image_sky
         self.hyper_background_noise = hyper_background_noise
@@ -144,48 +229,20 @@ class PhaseImaging(PhaseData):
             An lens object that the non-linear optimizer calls to determine the fit of a set of values
         """
 
-        mask = self.setup_phase_mask(data=data, mask=mask)
-
-        self.check_positions(positions=positions)
-
-        pixel_scale_binned_grid = self.pixel_scale_binned_grid_from_mask(mask=mask)
-
-        preload_pixelization_grids_of_planes = self.preload_pixelization_grids_of_planes_from_results(
-            results=results
-        )
-
-        lens_imaging_data = ld.LensImagingData(
-            imaging_data=data,
-            mask=mask,
-            trimmed_psf_shape=self.psf_shape,
-            positions=positions,
-            positions_threshold=self.positions_threshold,
-            pixel_scale_interpolation_grid=self.pixel_scale_interpolation_grid,
-            pixel_scale_binned_grid=pixel_scale_binned_grid,
-            hyper_noise_map_max=self.hyper_noise_map_max,
-            inversion_pixel_limit=self.inversion_pixel_limit,
-            inversion_uses_border=self.inversion_uses_border,
-            preload_pixelization_grids_of_planes=preload_pixelization_grids_of_planes,
-        )
-
         modified_image = self.modify_image(
-            image=lens_imaging_data.image(return_in_2d=True, return_masked=False),
+            image=data.image.mapping.scaled_array_2d_from_array_1d(
+                array_1d=data.image
+            ),
             results=results,
         )
 
-        lens_imaging_data = lens_imaging_data.new_lens_imaging_data_with_modified_image(
+        lens_imaging_data = self.meta_data_fit.data_fit_from(
+            data=data,
+            mask=mask,
+            positions=positions,
+            results=results,
             modified_image=modified_image
         )
-
-        if self.signal_to_noise_limit is not None:
-            lens_imaging_data = lens_imaging_data.new_lens_imaging_data_with_signal_to_noise_limit(
-                signal_to_noise_limit=self.signal_to_noise_limit
-            )
-
-        if self.bin_up_factor is not None:
-            lens_imaging_data = lens_imaging_data.new_lens_imaging_data_with_binned_up_imaging_data_and_mask(
-                bin_up_factor=self.bin_up_factor
-            )
 
         self.output_phase_info()
 
@@ -204,10 +261,10 @@ class PhaseImaging(PhaseData):
 
         with open(file_phase_info, "w") as phase_info:
             phase_info.write("Optimizer = {} \n".format(type(self.optimizer).__name__))
-            phase_info.write("Sub-grid size = {} \n".format(self.sub_size))
-            phase_info.write("PSF shape = {} \n".format(self.psf_shape))
+            phase_info.write("Sub-grid size = {} \n".format(self.meta_data_fit.sub_size))
+            phase_info.write("PSF shape = {} \n".format(self.meta_data_fit.psf_shape))
             phase_info.write(
-                "Positions Threshold = {} \n".format(self.positions_threshold)
+                "Positions Threshold = {} \n".format(self.meta_data_fit.positions_threshold)
             )
             phase_info.write("Cosmology = {} \n".format(self.cosmology))
             phase_info.write("Auto Link Priors = {} \n".format(self.auto_link_priors))
@@ -215,11 +272,11 @@ class PhaseImaging(PhaseData):
             phase_info.close()
 
     def extend_with_multiple_hyper_phases(
-        self,
-        hyper_galaxy=False,
-        inversion=False,
-        include_background_sky=False,
-        include_background_noise=False,
+            self,
+            hyper_galaxy=False,
+            inversion=False,
+            include_background_sky=False,
+            include_background_noise=False,
     ):
         hyper_phase_classes = []
 
@@ -359,7 +416,7 @@ class PhaseImaging(PhaseData):
             """
             if hasattr(self, "hyper_galaxy_image_1d_path_dict"):
                 for galaxy_path, galaxy in instance.path_instance_tuples_for_class(
-                    g.Galaxy
+                        g.Galaxy
                 ):
                     if galaxy_path in self.hyper_galaxy_image_1d_path_dict:
                         galaxy.hyper_model_image_1d = self.hyper_model_image_1d
@@ -367,8 +424,8 @@ class PhaseImaging(PhaseData):
                             galaxy_path
                         ]
                         if (
-                            hasattr(self, "binned_hyper_galaxy_image_1d_path_dict")
-                            and self.binned_hyper_galaxy_image_1d_path_dict is not None
+                                hasattr(self, "binned_hyper_galaxy_image_1d_path_dict")
+                                and self.binned_hyper_galaxy_image_1d_path_dict is not None
                         ):
                             galaxy.binned_hyper_galaxy_image_1d = self.binned_hyper_galaxy_image_1d_path_dict[
                                 galaxy_path
@@ -390,7 +447,7 @@ class PhaseImaging(PhaseData):
                 return None
 
         def lens_imaging_fit_for_tracer(
-            self, tracer, hyper_image_sky, hyper_background_noise
+                self, tracer, hyper_image_sky, hyper_background_noise
         ):
 
             return lens_fit.LensImagingFit.from_lens_data_and_tracer(
@@ -506,7 +563,7 @@ class PhaseImaging(PhaseData):
                     minimum_galaxy_value = hyper_minimum_percent * max(galaxy_image_1d)
                     galaxy_image_1d[
                         galaxy_image_1d < minimum_galaxy_value
-                    ] = minimum_galaxy_value
+                        ] = minimum_galaxy_value
 
                 hyper_galaxy_image_1d_path_dict[path] = galaxy_image_1d
 
@@ -574,7 +631,7 @@ class PhaseImaging(PhaseData):
                     )
                     binned_galaxy_image_1d[
                         binned_galaxy_image_1d < minimum_hyper_value
-                    ] = minimum_hyper_value
+                        ] = minimum_hyper_value
 
                     binned_hyper_galaxy_image_path_dict[path] = binned_galaxy_image_1d
 
