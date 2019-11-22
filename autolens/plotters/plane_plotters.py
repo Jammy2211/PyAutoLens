3ee87de17fb3c513ddc44119de1c9207cbf916ca
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-11-22 10:11:14 +0000

    imaging fit plotters now use new plotter API.

--- autolens/plotters/plane_plotters.py
+++ autolens/plotters/plane_plotters.py
@@ -48,7 +48,7 @@ def profile_image(
 
     profile_image = plane.profile_image_from_grid(grid=grid)
 
-    lines = lens_plotter_util.get_critical_curve_and_caustic(
+    lines = lens_plotter_util.get_critical_curves_and_caustics(
             obj=plane,
             include_critical_curves=include_critical_curves,
             include_caustics=include_caustics,
@@ -211,7 +211,7 @@ def convergence(
 
     convergence = plane.convergence_from_grid(grid=grid)
 
-    lines = lens_plotter_util.get_critical_curve_and_caustic(
+    lines = lens_plotter_util.get_critical_curves_and_caustics(
         obj=plane,
         include_critical_curves=include_critical_curves,
         include_caustics=include_caustics,
@@ -283,7 +283,7 @@ def potential(
 
     potential = plane.potential_from_grid(grid=grid)
 
-    lines = lens_plotter_util.get_critical_curve_and_caustic(
+    lines = lens_plotter_util.get_critical_curves_and_caustics(
         obj=plane,
         include_critical_curves=include_critical_curves,
         include_caustics=include_caustics,
@@ -356,7 +356,7 @@ def deflections_y(
     deflections = plane.deflections_from_grid(grid=grid)
     deflections_y = grid.mapping.array_from_sub_array_1d(sub_array_1d=deflections[:, 0])
 
-    lines = lens_plotter_util.get_critical_curve_and_caustic(
+    lines = lens_plotter_util.get_critical_curves_and_caustics(
         obj=plane,
         include_critical_curves=include_critical_curves,
         include_caustics=include_caustics,
@@ -429,7 +429,7 @@ def deflections_x(
     deflections = plane.deflections_from_grid(grid=grid)
     deflections_x = grid.mapping.array_from_sub_array_1d(sub_array_1d=deflections[:, 1])
 
-    lines = lens_plotter_util.get_critical_curve_and_caustic(
+    lines = lens_plotter_util.get_critical_curves_and_caustics(
         obj=plane,
         include_critical_curves=include_critical_curves,
         include_caustics=include_caustics,
@@ -501,7 +501,7 @@ def magnification(
 
     magnification = plane.magnification_from_grid(grid=grid)
 
-    lines = lens_plotter_util.get_critical_curve_and_caustic(
+    lines = lens_plotter_util.get_critical_curves_and_caustics(
         obj=plane,
         include_critical_curves=include_critical_curves,
         include_caustics=include_caustics,
@@ -558,7 +558,7 @@ def image_and_source_plane_subplot(
         number_subplots=2
     )
 
-    lines = lens_plotter_util.get_critical_curve_and_caustic(
+    lines = lens_plotter_util.get_critical_curves_and_caustics(
         obj=image_plane, include_critical_curves=True, include_caustics=True
     )
 
