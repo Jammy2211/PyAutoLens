bcebb7306a1948928f08ab150743470ae783ad4c
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-11-26 19:19:44 +0000

    release

--- release.sh
+++ release.sh
@@ -23,6 +23,8 @@ set +e
 git commit -m "Incremented version number"
 set -e
 
+pytest $p
+
 python setup.py sdist bdist_wheel
 twine upload dist/* --skip-existing --username $PYPI_USERNAME --password $PYPI_PASSWORD
 
