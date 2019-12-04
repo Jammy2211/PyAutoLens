303611bb3dc8195c2955a684a0fbaa5a56c16d4f
Richard <richardhayes777@gmail.com>
2019-12-04 14:25:03 +0000

    demonstrate optional

--- test_autolens/integration/tests/imaging/full_pipeline/test_mock_nlo.py
+++ test_autolens/integration/tests/imaging/full_pipeline/test_mock_nlo.py
@@ -1,10 +1,5 @@
 from test_autolens.integration.tests.imaging.full_pipeline import hyper_no_lens_light_bg
-from test_autolens.integration.tests.imaging.full_pipeline import (
-    hyper_with_lens_light_bg,
-)
-from test_autolens.integration.tests.imaging.full_pipeline import (
-    hyper_with_lens_light_bg,
-)
+from test_autolens.integration.tests.imaging.full_pipeline import hyper_no_lens_light_bg_extension_false
 from test_autolens.integration.tests.imaging.full_pipeline import (
     hyper_with_lens_light_bg,
 )
@@ -12,6 +7,9 @@ from test_autolens.integration.tests.imaging.runner import run_a_mock
 
 
 class TestCase:
+    def test_hyper_no_lens_light_bg_extension_false(self):
+        run_a_mock(hyper_no_lens_light_bg_extension_false)
+
     def _test_hyper_no_lens_light_bg(self):
         run_a_mock(hyper_no_lens_light_bg)
 
