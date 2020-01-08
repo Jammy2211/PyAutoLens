10fdd822e3408acfe2626445b9592959bd051fa8
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-01-08 20:46:51 +0000

    Ray tracing plots now work.

--- test_autolens/unit/plots/test_ray_tracing_plots.py
+++ test_autolens/unit/plots/test_ray_tracing_plots.py
@@ -3,11 +3,23 @@ import os
 
 import pytest
 
+from os import path
+
+from autofit import conf
+
+directory = path.dirname(path.realpath(__file__))
+
 
 @pytest.fixture(name="ray_tracing_plotter_path")
 def make_ray_tracing_plotter_setup():
     return "{}/../../test_files/plotting/ray_tracing/".format(
         os.path.dirname(os.path.realpath(__file__))
+    )
+
+@pytest.fixture(autouse=True)
+def set_config_path():
+    conf.instance = conf.Config(
+        path.join(directory, "../test_files/plotters"), path.join(directory, "output")
     )
 
 
@@ -18,61 +30,56 @@ def test__all_individual_plotters(
         tracer=tracer_x2_plane_7x7,
         grid=sub_grid_7x7,
         mask=mask_7x7,
-        cb_tick_values=[1.0],
-        cb_tick_labels=["1.0"],
-        output_path=ray_tracing_plotter_path,
-        output_format="png",
+        array_plotter=al.plotter.array(
+            output_path=ray_tracing_plotter_path, output_format="png"
+        ),
     )
 
-    assert ray_tracing_plotter_path + "tracer_profile_image.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "profile_image.png" in plot_patch.paths
 
     al.plot.tracer.convergence(
         tracer=tracer_x2_plane_7x7,
         grid=sub_grid_7x7,
         mask=mask_7x7,
-        cb_tick_values=[1.0],
-        cb_tick_labels=["1.0"],
-        output_path=ray_tracing_plotter_path,
-        output_format="png",
+        array_plotter=al.plotter.array(
+            output_path=ray_tracing_plotter_path, output_format="png"
+        ),
     )
 
-    assert ray_tracing_plotter_path + "tracer_convergence.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "convergence.png" in plot_patch.paths
 
     al.plot.tracer.potential(
         tracer=tracer_x2_plane_7x7,
         grid=sub_grid_7x7,
         mask=mask_7x7,
-        cb_tick_values=[1.0],
-        cb_tick_labels=["1.0"],
-        output_path=ray_tracing_plotter_path,
-        output_format="png",
+        array_plotter=al.plotter.array(
+            output_path=ray_tracing_plotter_path, output_format="png"
+        ),
     )
 
-    assert ray_tracing_plotter_path + "tracer_potential.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "potential.png" in plot_patch.paths
 
     al.plot.tracer.deflections_y(
         tracer=tracer_x2_plane_7x7,
         grid=sub_grid_7x7,
         mask=mask_7x7,
-        cb_tick_values=[1.0],
-        cb_tick_labels=["1.0"],
-        output_path=ray_tracing_plotter_path,
-        output_format="png",
+        array_plotter=al.plotter.array(
+            output_path=ray_tracing_plotter_path, output_format="png"
+        ),
     )
 
-    assert ray_tracing_plotter_path + "tracer_deflections_y.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "deflections_y.png" in plot_patch.paths
 
     al.plot.tracer.deflections_x(
         tracer=tracer_x2_plane_7x7,
         grid=sub_grid_7x7,
         mask=mask_7x7,
-        cb_tick_values=[1.0],
-        cb_tick_labels=["1.0"],
-        output_path=ray_tracing_plotter_path,
-        output_format="png",
+        array_plotter=al.plotter.array(
+            output_path=ray_tracing_plotter_path, output_format="png"
+        ),
     )
 
-    assert ray_tracing_plotter_path + "tracer_deflections_x.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "deflections_x.png" in plot_patch.paths
 
 
 def test__tracer_sub_plot_output(
@@ -82,8 +89,9 @@ def test__tracer_sub_plot_output(
         tracer=tracer_x2_plane_7x7,
         grid=sub_grid_7x7,
         mask=mask_7x7,
-        output_path=ray_tracing_plotter_path,
-        output_format="png",
+        array_plotter=al.plotter.array(
+            output_path=ray_tracing_plotter_path, output_format="png"
+        ),
     )
 
     assert ray_tracing_plotter_path + "tracer.png" in plot_patch.paths
@@ -99,18 +107,19 @@ def test__tracer_individuals__dependent_on_input(
         plot_profile_image=True,
         plot_source_plane=True,
         plot_potential=True,
-        output_path=ray_tracing_plotter_path,
-        output_format="png",
+        array_plotter=al.plotter.array(
+            output_path=ray_tracing_plotter_path, output_format="png"
+        ),
     )
 
-    assert ray_tracing_plotter_path + "tracer_profile_image.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "profile_image.png" in plot_patch.paths
 
-    assert ray_tracing_plotter_path + "tracer_source_plane.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "source_plane.png" in plot_patch.paths
 
-    assert ray_tracing_plotter_path + "tracer_convergence.png" not in plot_patch.paths
+    assert ray_tracing_plotter_path + "convergence.png" not in plot_patch.paths
 
-    assert ray_tracing_plotter_path + "tracer_potential.png" in plot_patch.paths
+    assert ray_tracing_plotter_path + "potential.png" in plot_patch.paths
 
-    assert ray_tracing_plotter_path + "tracer_deflections_y.png" not in plot_patch.paths
+    assert ray_tracing_plotter_path + "deflections_y.png" not in plot_patch.paths
 
-    assert ray_tracing_plotter_path + "tracer_deflections_x.png" not in plot_patch.paths
+    assert ray_tracing_plotter_path + "deflections_x.png" not in plot_patch.paths
