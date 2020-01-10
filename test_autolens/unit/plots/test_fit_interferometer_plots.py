e9dee1a3cc8f4b77a8e60b709b2bb27246d64240
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-01-10 16:28:36 +0000

    fit interferometer plotters use new plotter API

--- test_autolens/unit/plots/test_fit_interferometer_plots.py
+++ test_autolens/unit/plots/test_fit_interferometer_plots.py
@@ -4,51 +4,45 @@ import os
 import autolens as al
 
 
-@pytest.fixture(name="interferometer_fit_plotter_path")
-def make_interferometer_fit_plotter_setup():
+@pytest.fixture(name="fit_interferometer_plotter_path")
+def make_fit_interferometer_plotter_setup():
     return "{}/../../test_files/plotting/fit/".format(
         os.path.dirname(os.path.realpath(__file__))
     )
 
 
 def test__fit_sub_plot(
-    masked_interferometer_fit_x2_plane_7x7, interferometer_fit_plotter_path, plot_patch
+    masked_interferometer_fit_x2_plane_7x7, fit_interferometer_plotter_path, plot_patch
 ):
 
     al.plot.fit_interferometer.subplot(
         fit=masked_interferometer_fit_x2_plane_7x7,
-        cb_tick_values=[1.0],
-        cb_tick_labels=["1.0"],
-        output_path=interferometer_fit_plotter_path,
-        format="png",
+        array_plotter=al.plotter.array(
+            output=al.plotter.Output(path=fit_interferometer_plotter_path, format="png")
+        ),
     )
 
-    assert interferometer_fit_plotter_path + "fit.png" in plot_patch.paths
+    assert fit_interferometer_plotter_path + "fit_interferometer.png" in plot_patch.paths
 
 
 def test__fit_sub_plot_real_space(
-    masked_interferometer_fit_x2_plane_7x7, interferometer_fit_plotter_path, plot_patch
+    masked_interferometer_fit_x2_plane_7x7, fit_interferometer_plotter_path, plot_patch
 ):
 
     al.plot.fit_interferometer.subplot_real_space(
         fit=masked_interferometer_fit_x2_plane_7x7,
-        mask=True,
-        include_critical_curves=True,
-        include_caustics=True,
-        include_image_plane_pix=True,
-        cb_tick_values=[1.0],
-        cb_tick_labels=["1.0"],
-        output_path=interferometer_fit_plotter_path,
-        format="png",
+        array_plotter=al.plotter.array(
+            output=al.plotter.Output(path=fit_interferometer_plotter_path, format="png")
+        ),
     )
 
-    assert interferometer_fit_plotter_path + "fit_real_space.png" in plot_patch.paths
+    assert fit_interferometer_plotter_path + "fit_real_space.png" in plot_patch.paths
 
 
 def test__fit_individuals__source_and_lens__depedent_on_input(
     masked_interferometer_fit_x1_plane_7x7,
     masked_interferometer_fit_x2_plane_7x7,
-    interferometer_fit_plotter_path,
+    fit_interferometer_plotter_path,
     plot_patch,
 ):
 
@@ -59,36 +53,43 @@ def test__fit_individuals__source_and_lens__depedent_on_input(
         plot_signal_to_noise_map=False,
         plot_model_visibilities=True,
         plot_chi_squared_map=True,
-        output_path=interferometer_fit_plotter_path,
-        format="png",
+        array_plotter=al.plotter.array(
+            output=al.plotter.Output(path=fit_interferometer_plotter_path, format="png")
+        ),
+        grid_plotter=al.plotter.grid(
+            output=al.plotter.Output(path=fit_interferometer_plotter_path, format="png")
+        ),
+        line_plotter=al.plotter.line(
+            output=al.plotter.Output(path=fit_interferometer_plotter_path, format="png")
+        ),
     )
 
-    assert interferometer_fit_plotter_path + "fit_visibilities.png" in plot_patch.paths
+    assert fit_interferometer_plotter_path + "visibilities.png" in plot_patch.paths
 
-    assert interferometer_fit_plotter_path + "fit_noise_map.png" not in plot_patch.paths
+    assert fit_interferometer_plotter_path + "noise_map.png" not in plot_patch.paths
 
     assert (
-        interferometer_fit_plotter_path + "fit_signal_to_noise_map.png"
+        fit_interferometer_plotter_path + "signal_to_noise_map.png"
         not in plot_patch.paths
     )
 
     assert (
-        interferometer_fit_plotter_path + "fit_model_visibilities.png"
+        fit_interferometer_plotter_path + "model_visibilities.png"
         in plot_patch.paths
     )
 
     assert (
-        interferometer_fit_plotter_path + "fit_residual_map_vs_uv_distances_real.png"
+        fit_interferometer_plotter_path + "residual_map_vs_uv_distances_real.png"
         not in plot_patch.paths
     )
 
     assert (
-        interferometer_fit_plotter_path
-        + "fit_normalized_residual_map_vs_uv_distances_real.png"
+        fit_interferometer_plotter_path
+        + "normalized_residual_map_vs_uv_distances_real.png"
         not in plot_patch.paths
     )
 
     assert (
-        interferometer_fit_plotter_path + "fit_chi_squared_map_vs_uv_distances_real.png"
+        fit_interferometer_plotter_path + "chi_squared_map_vs_uv_distances_real.png"
         in plot_patch.paths
     )
