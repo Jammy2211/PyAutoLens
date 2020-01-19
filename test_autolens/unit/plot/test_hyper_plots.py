619ab97c5968fcd3a4ed5c0974bb2c7f4b889f23
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-01-19 17:25:15 +0000

    Hyper plots work

--- test_autolens/unit/plot/test_hyper_plots.py
+++ test_autolens/unit/plot/test_hyper_plots.py
@@ -4,9 +4,9 @@ import os
 import pytest
 
 
-@pytest.fixture(name="hyper_plotter_path")
+@pytest.fixture(name="plot_path")
 def make_hyper_plotter_setup():
-    return "{}/../../test_files/plotting/hyper_galaxies/".format(
+    return "{}/../test_files/plotting/hyper_galaxies/".format(
         os.path.dirname(os.path.realpath(__file__))
     )
 
@@ -15,7 +15,7 @@ def test__plot_individual_images(
     hyper_galaxy_image_0_7x7,
     contribution_map_7x7,
     include_all,
-    hyper_plotter_path,
+    plot_path,
     plot_patch,
 ):
 
@@ -23,28 +23,28 @@ def test__plot_individual_images(
         galaxy_image=hyper_galaxy_image_0_7x7,
         include=include_all,
         plotter=aplt.Plotter(
-            output=aplt.Output(hyper_plotter_path, format="png")
+            output=aplt.Output(plot_path, format="png")
         ),
     )
 
-    assert hyper_plotter_path + "hyper_galaxy_image.png" in plot_patch.paths
+    assert plot_path + "hyper_galaxy_image.png" in plot_patch.paths
 
     aplt.hyper.contribution_map(
         contribution_map_in=contribution_map_7x7,
         include=include_all,
         plotter=aplt.Plotter(
-            output=aplt.Output(hyper_plotter_path, format="png")
+            output=aplt.Output(plot_path, format="png")
         ),
     )
 
-    assert hyper_plotter_path + "contribution_map.png" in plot_patch.paths
+    assert plot_path + "contribution_map.png" in plot_patch.paths
 
 def test__plot_subplot_of_hyper_galaxy(
     hyper_galaxy_image_0_7x7,
     contribution_map_7x7,
     masked_imaging_fit_x2_plane_7x7,
         include_all,
-    hyper_plotter_path,
+    plot_path,
     plot_patch,
 ):
     aplt.hyper.subplot_fit_hyper_galaxy(
@@ -54,18 +54,18 @@ def test__plot_subplot_of_hyper_galaxy(
         contribution_map_in=contribution_map_7x7,
         include=include_all,
         sub_plotter=aplt.SubPlotter(
-            output=aplt.Output(hyper_plotter_path, format="png")
+            output=aplt.Output(plot_path, format="png")
         ),
     )
 
-    assert hyper_plotter_path + "subplot_fit_hyper_galaxy.png" in plot_patch.paths
+    assert plot_path + "subplot_fit_hyper_galaxy.png" in plot_patch.paths
 
 
 def test__plot_hyper_galaxy_images(
     hyper_galaxy_image_path_dict_7x7,
     mask_7x7,
         include_all,
-    hyper_plotter_path,
+    plot_path,
     plot_patch,
 ):
 
@@ -74,8 +74,8 @@ def test__plot_hyper_galaxy_images(
         mask=mask_7x7,
         include=include_all,
         sub_plotter=aplt.SubPlotter(
-            output=aplt.Output(hyper_plotter_path, format="png")
+            output=aplt.Output(plot_path, format="png")
         ),
     )
 
-    assert hyper_plotter_path + "subplot_hyper_galaxy_images.png" in plot_patch.paths
+    assert plot_path + "subplot_hyper_galaxy_images.png" in plot_patch.paths
