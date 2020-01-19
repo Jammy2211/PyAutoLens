f711dbbc14eb027080ae361302155eb0f4d99f83
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-01-19 15:45:05 +0000

    mapper plots added from autoarray

--- autolens/plot/__init__.py
+++ autolens/plot/__init__.py
@@ -1,9 +1,6 @@
 from autoarray.plot.mat_objs import Units, Figure, ColorMap, ColorBar, Ticks, Labels, Legend, Output, Scatterer, Liner
 from autoarray.plot import imaging_plots as imaging
 from autoarray.plot import interferometer_plots as interferometer
-from autoarray.plot import fit_imaging_plots as fit_imaging
-from autoarray.plot import fit_interferometer_plots as fit_interferometer
-from autoarray.plot import mapper_plots as mapper
 
 from autoastro.plot.lensing_plotters import Plotter, SubPlotter, Include
 from autoastro.plot import light_profile_plots as lp
@@ -13,6 +10,7 @@ from autoastro.plot import fit_galaxy_plots as fit_galaxy
 
 from autolens.plot import plane_plots as plane
 from autolens.plot import ray_tracing_plots as tracer
+from autolens.plot import mapper_plots as mapper
 from autolens.plot import inversion_plots as inversion
 from autolens.plot.fit_imaging_plots import fit_imaging_plots as fit_imaging
 from autolens.plot.fit_interferometer_plots import (
