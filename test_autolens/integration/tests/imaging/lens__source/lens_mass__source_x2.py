946af9e0b8aa3ca8205c65a6776d84fe4bd45cb6
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-01-27 20:07:25 +0000

    Fixed minor bug where source plane doesnt plot

--- test_autolens/integration/tests/imaging/lens__source/lens_mass__source_x2.py
+++ test_autolens/integration/tests/imaging/lens__source/lens_mass__source_x2.py
@@ -4,7 +4,7 @@ from test_autolens.integration.tests.imaging import runner
 
 test_type = "lens__source"
 test_name = "lens_mass__source_x2"
-data_type = "lens_mass__source_smooth"
+data_type = "lens_sie__source_smooth"
 data_resolution = "euclid"
 
 
--- test_autolens/integration/tests/interferometer/lens__source_inversion/adaptive_magnification/lens_mass__source__offset_centre.py
+++ test_autolens/integration/tests/interferometer/lens__source_inversion/adaptive_magnification/lens_mass__source__offset_centre.py
@@ -4,7 +4,7 @@ from test_autolens.integration.tests.interferometer import runner
 
 test_type = "lens__source_inversion"
 test_name = "lens_mass__source_adaptive_magnification__offset_centre"
-data_type = "lens_mass__source_smooth__offset_centre"
+data_type = "lens_sie__source_smooth__offset_centre"
 data_resolution = "sma"
 
 
