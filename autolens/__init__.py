8f220235e649123042e434fec565c7b048a14db7
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-02-23 13:46:59 +0000

    Incremented version number

--- autolens/__init__.py
+++ autolens/__init__.py
@@ -62,4 +62,4 @@ from autolens.pipeline.pipeline import PipelineDataset, PipelinePositions
 from autolens.pipeline import setup
 from autolens import plot
 
-__version__ = "0.38.4"
+__version__ = '0.39.0'
