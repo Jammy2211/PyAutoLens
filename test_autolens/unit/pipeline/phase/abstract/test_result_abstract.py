c843c101c68a922e01f0da13b073ef3390bd0e38
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-04-15 15:21:38 +0100

    abstrast phase results centres use lists properly

--- test_autolens/unit/pipeline/phase/abstract/test_result_abstract.py
+++ test_autolens/unit/pipeline/phase/abstract/test_result_abstract.py
@@ -114,7 +114,8 @@ class TestTracer:
         )
 
         assert result.source_plane_light_profile_centres.in_list == [
-            [(1.0, 2.0), (3.0, 4.0), (5.0, 6.0)]
+            [(1.0, 2.0), (3.0, 4.0)],
+            [(5.0, 6.0)],
         ]
 
         phase_dataset_7x7 = al.PhaseImaging(
@@ -127,7 +128,7 @@ class TestTracer:
             dataset=imaging_7x7, mask=mask_7x7, results=mock_pipeline.MockResults()
         )
 
-        assert result.source_plane_light_profile_centres == None
+        assert result.source_plane_light_profile_centres == []
 
     def test__most_likely_tracer_source_inversion_centres_correct(
         self, imaging_7x7, mask_7x7
@@ -186,7 +187,7 @@ class TestTracer:
             dataset=imaging_7x7, mask=mask_7x7, results=mock_pipeline.MockResults()
         )
 
-        assert result.source_plane_inversion_centres == None
+        assert result.source_plane_inversion_centres == []
 
     def test__most_likely_tracer_source_centres_correct(self, imaging_7x7, mask_7x7):
 
@@ -210,7 +211,7 @@ class TestTracer:
             dataset=imaging_7x7, mask=mask_7x7, results=mock_pipeline.MockResults()
         )
 
-        assert result.source_plane_centres.in_list == [[(9.0, 8.0)], [(0.0, 0.0)]]
+        assert result.source_plane_centres.in_list == [[(9.0, 8.0), (0.0, 0.0)]]
 
     def test__most_likely_tracer__multiple_image_positions_of_source_plane_centres_and_separations(
         self, imaging_7x7, mask_7x7
