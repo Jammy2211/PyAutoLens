b8dff507403951b557ac9a0c102d8b1d2bffb1d4
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-07-02 12:47:13 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = "2020, James Nightingale, Richard Hayes"
 author = "James Nightingale, Richard Hayes"
 
 # The full version, including alpha/beta/rc tags
-release = "0.39.3"
+release = "1.0.15"
 master_doc = "index"
 
 
