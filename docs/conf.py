43cc74e05da0b44649d873140e4b752a7c023dbc
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-07-02 20:50:47 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@ import datetime
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../build/lib/autolens'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
@@ -24,7 +24,7 @@ copyright = "2020, James Nightingale, Richard Hayes"
 author = "James Nightingale, Richard Hayes"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.15"
+release = "1.0.16"
 master_doc = "index"
 
 
