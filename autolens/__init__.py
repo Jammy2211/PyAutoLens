ac50ab02fd4ed739254ecb894e8d40f54d3aa50a
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-09-08 12:09:34 +0100

    Incremented version number

--- autolens/__init__.py
+++ autolens/__init__.py
@@ -84,5 +84,5 @@ from .pipeline.phase.extensions.stochastic_phase import StochasticPhase
 from .pipeline.phase.phase_galaxy import PhaseGalaxy
 from .pipeline.pipeline import PipelineDataset, PipelinePositions
 
-__version__ = '1.5.0'
 
+__version__ = '1.5.1'
