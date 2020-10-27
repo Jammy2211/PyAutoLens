931f942cb03709e5971312385561b8abc18eb41a
James Nightingale <james.w.nightingale@durham.ac.uk>
2020-10-27 12:50:13 +0000

    docas

--- docs/conf.py
+++ docs/conf.py
@@ -117,3 +117,9 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- IMAGES --
+
+
+from sphinx.builders.html import StandaloneHTMLBuilder
+StandaloneHTMLBuilder.supported_image_types = ['image/gif', 'image/png', 'image/jpeg']
