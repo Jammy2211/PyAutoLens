ddd022f0ae977886c7b445153d23d7d2b60cf1fd
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-04-02 16:07:18 +0100

    Preloads code so much less crap

--- test_autolens/analysis/test_preloads.py
+++ test_autolens/analysis/test_preloads.py
@@ -2,7 +2,7 @@ import autofit as af
 import autolens as al
 from autolens import exc
 from autolens.mock import mock
-from autolens.analysis import preloads
+from autolens.analysis import preloads as pload
 
 import pytest
 
@@ -16,7 +16,7 @@ def test__preload_pixelization():
     )
 
     with pytest.raises(exc.PreloadException):
-        preloads.preload_pixelization_grid_from(result=result, model=model)
+        pload.preload_pixelization_grid_from(result=result, model=model)
 
     model = af.Collection(
         galaxies=af.Collection(
@@ -30,7 +30,7 @@ def test__preload_pixelization():
     )
 
     with pytest.raises(exc.PreloadException):
-        preloads.preload_pixelization_grid_from(result=result, model=model)
+        pload.preload_pixelization_grid_from(result=result, model=model)
 
     model = af.Collection(
         galaxies=af.Collection(
@@ -43,8 +43,84 @@ def test__preload_pixelization():
         )
     )
 
-    preload_pixelization_grid = preloads.preload_pixelization_grid_from(
-        result=result, model=model
+    preloads = pload.preload_pixelization_grid_from(result=result, model=model)
+
+    assert preloads.sparse_grids_of_planes == 1
+
+    preloads = pload.Preloads.setup(result=result, model=model, pixelization=True)
+
+    assert preloads.sparse_grids_of_planes == 1
+
+
+def test__preload_inversion_with_fixed_profiles(imaging_fit_x2_plane_inversion_7x7):
+
+    result = mock.MockResult(
+        max_log_likelihood_fit=imaging_fit_x2_plane_inversion_7x7,
+        max_log_likelihood_pixelization_grids_of_planes=1,
     )
 
-    assert preload_pixelization_grid == 1
+    # model = af.Collection(
+    #     galaxies=af.Collection(lens=af.Model(al.Galaxy, mass=al.mp.SphIsothermal), source=af.Model(al.Galaxy))
+    # )
+    #
+    # with pytest.raises(exc.PreloadException):
+    #     pload.preload_inversion_with_fixed_profiles(result=result, model=model)
+
+    model = af.Collection(
+        galaxies=af.Collection(
+            lens=af.Model(al.Galaxy),
+            source=af.Model(
+                al.Galaxy,
+                pixelization=al.pix.VoronoiBrightnessImage,
+                regularization=al.reg.AdaptiveBrightness,
+            ),
+        )
+    )
+
+    with pytest.raises(exc.PreloadException):
+        pload.preload_inversion_with_fixed_profiles(result=result, model=model)
+
+    model = af.Collection(
+        galaxies=af.Collection(
+            lens=af.Model(al.Galaxy),
+            source=af.Model(
+                al.Galaxy,
+                pixelization=al.pix.VoronoiBrightnessImage(),
+                regularization=al.reg.AdaptiveBrightness(),
+            ),
+        )
+    )
+
+    preloads = pload.preload_inversion_with_fixed_profiles(result=result, model=model)
+
+    assert preloads.sparse_grids_of_planes == 1
+    assert (
+        preloads.blurred_mapping_matrix
+        == imaging_fit_x2_plane_inversion_7x7.inversion.blurred_mapping_matrix
+    ).all()
+    assert (
+        preloads.curvature_matrix_sparse_preload
+        == imaging_fit_x2_plane_inversion_7x7.inversion.curvature_matrix_sparse_preload
+    ).all()
+    assert (
+        preloads.curvature_matrix_preload_counts
+        == imaging_fit_x2_plane_inversion_7x7.inversion.curvature_matrix_preload_counts
+    ).all()
+    assert preloads.mapper == imaging_fit_x2_plane_inversion_7x7.inversion.mapper
+
+    preloads = pload.Preloads.setup(result=result, model=model, inversion=True)
+
+    assert preloads.sparse_grids_of_planes == 1
+    assert (
+        preloads.blurred_mapping_matrix
+        == imaging_fit_x2_plane_inversion_7x7.inversion.blurred_mapping_matrix
+    ).all()
+    assert (
+        preloads.curvature_matrix_sparse_preload
+        == imaging_fit_x2_plane_inversion_7x7.inversion.curvature_matrix_sparse_preload
+    ).all()
+    assert (
+        preloads.curvature_matrix_preload_counts
+        == imaging_fit_x2_plane_inversion_7x7.inversion.curvature_matrix_preload_counts
+    ).all()
+    assert preloads.mapper == imaging_fit_x2_plane_inversion_7x7.inversion.mapper
