3f4a40bb7914a5504ba403fefb0c8e9e290f22d0
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-04-16 10:20:22 +0100

    decomposed 1D figures

--- autolens/__init__.py
+++ autolens/__init__.py
@@ -22,7 +22,9 @@ from autoarray.structures.grids.two_d.grid_2d_irregular import Grid2DIrregular
 from autoarray.structures.grids.two_d.grid_2d_irregular import Grid2DIrregularUniform
 from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DRectangular
 from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DVoronoi
-from autoarray.structures.vector_fields.vector_field_irregular import VectorField2DIrregular
+from autoarray.structures.vector_fields.vector_field_irregular import (
+    VectorField2DIrregular,
+)
 from autoarray.structures.kernel_2d import Kernel2D
 from autoarray.structures.visibilities import Visibilities
 from autogalaxy import util
