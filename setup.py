082444adbb24aafa9e3dfcecf0ddf6e33194eb96
Jonathan Frawley <jonathan.frawley@durham.ac.uk>
2021-05-05 10:52:15 +0100

    Change name of autoconf temporarily

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ with open(join(this_dir, "requirements.txt")) as f:
 
 version = environ.get("VERSION", "1.0.dev0")
 requirements.extend(
-    [f"autoarray=={version}", f"autoconf=={version}", f"autofit=={version}", f"autogalaxy=={version}"]
+    #[f"autoarray=={version}", f"autoconf=={version}", f"autofit=={version}", f"autogalaxy=={version}"]
+    [f"autoarray=={version}", f"autoconf-tmp=={version}", f"autofit=={version}", f"autogalaxy=={version}"]
 )
 
 setup(
