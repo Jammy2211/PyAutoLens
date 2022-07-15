dced8670b84c23ed53528d40f236bc879396040a
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-07-15 17:20:44 +0100

    unit test on linear light profiles of max tracwer

--- test_autolens/imaging/model/test_result_imaging.py
+++ test_autolens/imaging/model/test_result_imaging.py
@@ -1,8 +1,25 @@
-import os
+import numpy as np
+import pytest
 
-directory = os.path.dirname(os.path.realpath(__file__))
+import autofit as af
+import autolens as al
+
+from autolens.imaging.model.result import ResultImaging
 
 
-class TestResultImaging:
+def test___linear_light_profiles_in_result(analysis_imaging_7x7):
+    
+    galaxies = af.ModelInstance()
+    galaxies.galaxy = al.Galaxy(redshift=0.5, bulge=al.lp_linear.EllSersic())
 
-    pass
+    instance = af.ModelInstance()
+    instance.galaxies = galaxies
+
+    samples = al.m.MockSamples(max_log_likelihood_instance=instance)
+
+    result = ResultImaging(
+        samples=samples, analysis=analysis_imaging_7x7, model=None
+    )
+
+    assert not isinstance(result.max_log_likelihood_tracer.galaxies[0].bulge, al.lp_linear.LightProfileLinear)
+    assert result.max_log_likelihood_tracer.galaxies[0].bulge.intensity == pytest.approx(0.002310735, 1.0e-4)
