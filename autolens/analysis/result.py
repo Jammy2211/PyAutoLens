7531d2a7bc0d409a1e2cd32adf319e2e559e27c2
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-12-24 17:29:59 +0000

    masks refactor

--- autolens/analysis/result.py
+++ autolens/analysis/result.py
@@ -329,8 +329,8 @@ class ResultDataset(Result):
         The hyper model image is the sum of the hyper galaxy image of every individual galaxy.
         """
         hyper_model_image = aa.Array2D.manual_mask(
-            array=np.zeros(self.mask.mask_sub_1.pixels_in_mask),
-            mask=self.mask.mask_sub_1,
+            array=np.zeros(self.mask.derived_masks.sub_1.pixels_in_mask),
+            mask=self.mask.derived_masks.sub_1,
         )
 
         for path, galaxy in self.path_galaxy_tuples:
