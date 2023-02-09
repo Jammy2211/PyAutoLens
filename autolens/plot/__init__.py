8a4ad5ee949790ea332a4ec3a4780753b17b8f3e
Jammy2211 <james.w.nightingale@durham.ac.uk>
2023-02-09 13:16:35 +0000

    add annotate

--- autolens/plot/__init__.py
+++ autolens/plot/__init__.py
@@ -20,7 +20,7 @@ from autoarray.plot.wrap.base import (
     XLabel,
     Legend,
     Text,
-    Annotate,
+    Annotate, 
     Output,
 )
 from autoarray.plot.wrap.one_d import YXPlot, FillBetween
