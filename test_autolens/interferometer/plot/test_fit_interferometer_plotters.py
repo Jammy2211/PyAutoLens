7e090a9b7feb5e6bbb5bbcb9eaeba53f01d36ab7
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-06-01 19:56:38 +0100

    fix unit test

--- test_autolens/interferometer/plot/test_fit_interferometer_plotters.py
+++ test_autolens/interferometer/plot/test_fit_interferometer_plotters.py
@@ -120,15 +120,3 @@ def test__fit_sub_plot_real_space(
 
     fit_interferometer_plotter.subplot_fit_real_space()
     assert path.join(plot_path, "subplot_fit_real_space.png") in plot_patch.paths
-
-    plot_patch.paths = []
-
-    fit_interferometer_plotter = aplt.FitInterferometerPlotter(
-        fit=fit_interferometer_x2_plane_inversion_7x7,
-        include_2d=include_2d_all,
-        mat_plot_1d=aplt.MatPlot1D(output=aplt.Output(plot_path, format="png")),
-        mat_plot_2d=aplt.MatPlot2D(output=aplt.Output(plot_path, format="png")),
-    )
-
-    fit_interferometer_plotter.subplot_fit_real_space()
-    assert path.join(plot_path, "subplot_fit_real_space.png") in plot_patch.paths
