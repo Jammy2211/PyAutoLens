114760baacba80d965ce0064c5abf29c7fbb8132
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-07-12 15:59:27 +0100

    missing trst thing

--- test_autolens/aggregator/conftest.py
+++ test_autolens/aggregator/conftest.py
@@ -45,4 +45,5 @@ def make_samples(model):
 
     return al.m.MockSamples(
         model=model, sample_list=sample_list, max_log_likelihood_instance=tracer,
+        gaussian_tuples=[(1.0, 2.0)] * model.prior_count
     )
