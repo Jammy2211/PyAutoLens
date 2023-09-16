c3cd15de47a72cd4697643875bb91ab824f93494
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-09-16 11:22:41 +0100

    added NautilusPlotter to plot imports

--- autolens/plot/__init__.py
+++ autolens/plot/__init__.py
@@ -1,5 +1,6 @@
 from autofit.plot.samples_plotters import SamplesPlotter
 from autofit.non_linear.search.nest.dynesty.plotter import DynestyPlotter
+from autofit.non_linear.search.nest.nautilus.plotter import NautilusPlotter
 from autofit.non_linear.search.nest.ultranest.plotter import UltraNestPlotter
 from autofit.non_linear.search.mcmc.emcee.plotter import EmceePlotter
 from autofit.non_linear.search.mcmc.zeus.plotter import ZeusPlotter
