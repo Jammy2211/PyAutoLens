52c9f55027cfacc913c285f9dc25083caec0857c
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-05-07 18:18:10 +0100

    interferometer plots

--- autolens/interferometer/model/plotter_interface.py
+++ autolens/interferometer/model/plotter_interface.py
@@ -97,6 +97,9 @@ class PlotterInterfaceInterferometer(PlotterInterface):
             dirty_chi_squared_map=should_plot("chi_squared_map"),
         )
 
+        if plot_setting(section="inversion", name="subplot_mappings"):
+            fit_plotter.subplot_mappings_of_plane(plane_index=len(fit.tracer.planes) - 1)
+
         if not during_analysis and should_plot("all_at_end_png"):
             mat_plot_1d = self.mat_plot_1d_from(subfolders=path.join(subfolders, "end"))
             mat_plot_2d = self.mat_plot_2d_from(subfolders=path.join(subfolders, "end"))
