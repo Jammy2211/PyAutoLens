3e8874dd76a0fd45f9a2712664e5ef7a6a44e777
Richard <richard@rghsoftware.co.uk>
2024-06-17 13:40:52 +0100

    refactor to extract fixtures

--- test_autolens/point/test_likelihood.py
+++ test_autolens/point/test_likelihood.py
@@ -1,23 +1,41 @@
+import pytest
+
 from autolens.point.analysis import AnalysisAllToAllPointSource
 import autolens as al
 import autofit as af
 
 
-def test_likelihood():
+@pytest.fixture
+def grid():
+    return al.Grid2D.uniform(
+        shape_native=(100, 100),
+        pixel_scales=0.05,
+    )
+
+
+@pytest.fixture
+def model():
+    lens = af.Model(
+        al.Galaxy,
+        redshift=0.5,
+        mass=al.mp.Isothermal,
+    )
+    source = af.Model(al.ps.PointSourceChi)
+
+    return af.Collection(
+        lens=lens,
+        source=source,
+    )
+
+
+def test_likelihood(grid, model):
     analysis = AnalysisAllToAllPointSource(
-        [
+        coordinates=[
             (0.0, 0.0),
         ],
-        0.1,
-        al.Grid2D.uniform(
-            shape_native=(100, 100),
-            pixel_scales=0.05,
-        ),
+        error=0.1,
+        grid=grid,
         pixel_scale_precision=0.025,
     )
 
-    lens = af.Model(al.Galaxy, redshift=0.5, mass=al.mp.Isothermal)
-    source = af.Model(al.ps.PointSourceChi)
-
-    model = af.Collection(lens=lens, source=source)
     assert analysis.log_likelihood_function(model.instance_from_prior_medians())
