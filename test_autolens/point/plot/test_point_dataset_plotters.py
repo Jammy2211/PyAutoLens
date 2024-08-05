082054a777e8a4e955d3e427d258f5855eb9fcde
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-08-05 13:18:34 +0100

    delete PointDictPlotter test

--- test_autolens/point/plot/test_point_dataset_plotters.py
+++ test_autolens/point/plot/test_point_dataset_plotters.py
@@ -68,18 +68,3 @@ def test__subplot_dataset(point_dataset, include_2d_all, plot_path, plot_patch):
     assert path.join(plot_path, "subplot_dataset.png") in plot_patch.paths
 
 
-def test__subplot_point_dict(point_dict, include_2d_all, plot_path, plot_patch):
-    point_dict_plotter = aplt.PointDictPlotter(
-        point_dict=point_dict,
-        include_2d=include_2d_all,
-        mat_plot_1d=aplt.MatPlot1D(output=aplt.Output(path=plot_path, format="png")),
-        mat_plot_2d=aplt.MatPlot2D(output=aplt.Output(path=plot_path, format="png")),
-    )
-
-    point_dict_plotter.subplot_positions()
-
-    assert path.join(plot_path, "subplot_point_dict_positions.png") in plot_patch.paths
-
-    point_dict_plotter.subplot_fluxes()
-
-    assert path.join(plot_path, "subplot_point_dict_fluxes.png") in plot_patch.paths
