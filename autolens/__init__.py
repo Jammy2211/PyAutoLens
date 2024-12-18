e81bae0fb2893b110aa451ae20b1ff40417e5a49
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-12-18 09:47:29 +0000

    remove Grid2DIRregularUniform

--- autolens/__init__.py
+++ autolens/__init__.py
@@ -35,7 +35,6 @@ from autoarray.structures.arrays.irregular import ArrayIrregular
 from autoarray.structures.grids.uniform_1d import Grid1D
 from autoarray.structures.grids.uniform_2d import Grid2D
 from autoarray.structures.grids.irregular_2d import Grid2DIrregular
-from autoarray.structures.grids.irregular_2d import Grid2DIrregularUniform
 from autoarray.structures.mesh.rectangular_2d import Mesh2DRectangular
 from autoarray.structures.mesh.voronoi_2d import Mesh2DVoronoi
 from autoarray.structures.mesh.delaunay_2d import Mesh2DDelaunay
