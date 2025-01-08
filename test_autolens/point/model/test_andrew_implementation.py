949a38846c2056def5822bcbf3d8d383da315b80
Richard <richard@rghsoftware.co.uk>
2025-01-08 12:44:35 +0000

    working implementation that deals with nans

--- test_autolens/point/model/test_andrew_implementation.py
+++ test_autolens/point/model/test_andrew_implementation.py
@@ -18,29 +18,51 @@ def test_andrew_implementation(
     data,
     noise_map,
 ):
-    model_positions = np.array([(-1.0749, -1.1), (1.19117, 1.175)])
-
-    assert (
-        Fit(
-            data=data,
-            noise_map=noise_map,
-            model_positions=model_positions,
-        ).log_likelihood()
-        == -4.40375330990644
+    model_positions = np.array(
+        [
+            (-1.0749, -1.1),
+            (1.19117, 1.175),
+        ]
     )
+
+    fit = Fit(
+        data=data,
+        noise_map=noise_map,
+        model_positions=model_positions,
+    )
+
+    assert np.allclose(
+        fit.all_permutations_log_likelihoods(),
+        [
+            -1.51114426,
+            -1.50631469,
+        ],
+    )
+    assert fit.log_likelihood() == -4.40375330990644
 
 
 def test_nan_model_positions(
     data,
     noise_map,
 ):
-    model_positions = np.array([(-1.0749, -1.1), (1.19117, 1.175), (np.nan, np.nan)])
-
-    assert (
-        Fit(
-            data=data,
-            noise_map=noise_map,
-            model_positions=model_positions,
-        ).log_likelihood()
-        == -4.40375330990644
+    model_positions = np.array(
+        [
+            (-1.0749, -1.1),
+            (1.19117, 1.175),
+            (np.nan, np.nan),
+        ]
     )
+    fit = Fit(
+        data=data,
+        noise_map=noise_map,
+        model_positions=model_positions,
+    )
+
+    assert np.allclose(
+        fit.all_permutations_log_likelihoods(),
+        [
+            -1.51114426,
+            -1.50631469,
+        ],
+    )
+    assert fit.log_likelihood() == -4.40375330990644
