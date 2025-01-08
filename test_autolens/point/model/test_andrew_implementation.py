a58c2d5ee8740167d799c9e7eab8ac662ca6f968
Richard <richard@rghsoftware.co.uk>
2025-01-08 12:08:54 +0000

    pull out values for individual permutations

--- test_autolens/point/model/test_andrew_implementation.py
+++ test_autolens/point/model/test_andrew_implementation.py
@@ -1,19 +1,45 @@
 import numpy as np
+import pytest
+
 from autolens.point.fit.positions.image.pair_repeat import Fit
 
 
-def test_andrew_implementation():
-    data = np.array([(0.0, 0.0), (1.0, 0.0)])
-    model_positions = np.array([(-1.0749, -1.1), (1.19117, 1.175)])
+@pytest.fixture
+def data():
+    return np.array([(0.0, 0.0), (1.0, 0.0)])
 
-    error = 1.0
+
+@pytest.fixture
+def noise_map():
+    return np.array([1.0, 1.0])
+
+
+def test_andrew_implementation(
+    data,
+    noise_map,
+):
+    model_positions = np.array([(-1.0749, -1.1), (1.19117, 1.175)])
 
     assert (
         Fit(
             data=data,
-            noise_map=np.array(
-                [error, error],
-            ),
+            noise_map=noise_map,
+            model_positions=model_positions,
+        ).log_likelihood()
+        == -4.40375330990644
+    )
+
+
+def test_nan_model_positions(
+    data,
+    noise_map,
+):
+    model_positions = np.array([(-1.0749, -1.1), (1.19117, 1.175), (np.nan, np.nan)])
+
+    assert (
+        Fit(
+            data=data,
+            noise_map=noise_map,
             model_positions=model_positions,
         ).log_likelihood()
         == -4.40375330990644
