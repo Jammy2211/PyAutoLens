0e14636d2f0d40ed84e5cbdaf2465ef403a48fba
James Nightingale <james.w.nightingale@durham.ac.uk>
2025-01-29 11:23:43 +0000

    source chi squared docstrong

--- autolens/point/fit/positions/source/separations.py
+++ autolens/point/fit/positions/source/separations.py
@@ -19,15 +19,51 @@ class FitPositionsSource(AbstractFitPositions):
         profile: Optional[ag.ps.Point] = None,
     ):
         """
-        A lens position fitter, which takes a set of positions (e.g. from a plane in the tracer) and computes \
-        their maximum separation, such that points which tracer closer to one another have a higher log_likelihood.
+        Fits a point source dataset using a `Tracer` object with a source-plane chi-squared based on the separation of
+        image-plane positions ray-traced to the source-plane compared to the centre of the source galaxy.
+
+        The fit performs the following steps:
+
+        1) Ray-trace the positions in the point source to the source-plane via the `Tracer`, including accounting for
+           multi-plane ray-tracing.
+
+        2) Determine the source-plane centre of the source-galaxy, which could be a free model parameter or computed
+           as the barycenter of ray-traced positions in the source-plane.
+
+        3) Compute the distance of each ray-traced position to the source-plane centre and compute the residuals,
+
+        4) Compute the magnification of each image-plane position via the Hessian of the tracer's deflection angles.
+
+        5) Compute the chi-squared of each position as the square of the residual multiplied by the magnification and
+           divided by the RMS noise-map value.
+
+        6) Sum the chi-squared values to compute the overall log likelihood of the fit.
+
+        Point source fitting uses name pairing, whereby the `name` of the `Point` object is paired to the name of the
+        point source dataset to ensure that point source datasets are fitted to the correct point source.
+
+        This fit object is used in the `FitPointDataset` to perform position based fitting of a `PointDataset`,
+        which may also fit other components of the point dataset like fluxes or time delays.
+
+        When performing a `model-fit`via an `AnalysisPoint` object the `figure_of_merit` of this object
+        is called and returned in the `log_likelihood_function`.
 
         Parameters
         ----------
-        data : Grid2DIrregular
-            The (y,x) arc-second coordinates of positions which the maximum distance and log_likelihood is computed using.
-        noise_value
-            The noise-value assumed when computing the log likelihood.
+        name
+            The name of the point source dataset which is paired to a `Point` profile.
+        data
+            The positions of the point source in the image-plane which are fitted.
+        noise_map
+            The noise-map of the positions which are used to compute the log likelihood of the positions.
+        tracer
+            The tracer of galaxies whose point source profile are used to fit the positions.
+        solver
+            Solves the lens equation in order to determine the image-plane positions of a point source by ray-tracing
+            triangles to and from the source-plane. This is not used in this source-plane point source fit.
+        profile
+            Manually input the profile of the point source, which is used instead of the one extracted from the
+            tracer via name pairing if that profile is not found.
         """
 
         super().__init__(
@@ -42,10 +78,12 @@ class FitPositionsSource(AbstractFitPositions):
     @property
     def model_data(self) -> aa.Grid2DIrregular:
         """
-        Returns the model positions, which are computed via the point solver.
+        Returns the source-plane model positions of the point source, which are the positions of the image-plane
+        positions ray-traced to the source-plane.
 
-        It if common for many more image-plane positions to be computed than actual positions in the dataset. In this
-        case, each data point is paired with its closest model position.
+        This calculation accounts for multi-plane ray-tracing, whereby if the tracer has more than 2 planees the
+        redshift of the point source galaxy is extracted and the deflections between the image-plane and source-plane
+        at its specific redshift are used.
         """
         if len(self.tracer.planes) <= 2:
             deflections = self.tracer.deflections_yx_2d_from(grid=self.data)
@@ -58,6 +96,10 @@ class FitPositionsSource(AbstractFitPositions):
 
     @property
     def residual_map(self) -> aa.ArrayIrregular:
+        """
+        Returns the residuals of the point-source source-plane fit, which are the distances of each source-plane
+        position from the source-plane centre.
+        """
         return self.model_data.distances_to_coordinate_from(
             coordinate=self.source_plane_coordinate
         )
