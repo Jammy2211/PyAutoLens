5d1cb4395bce8f65ffede16e57101d74f6d318da
Jammy2211 <JNightingale2211@gmail.com>
2025-05-09 11:26:42 +0100

    implement pyproject.toml

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from codecs import open
 from os import environ
 from os.path import abspath, dirname, join
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, "README.rst"), encoding="utf-8") as file:
@@ -22,7 +22,6 @@ requirements.extend(
     ]
 )
 
-
 def config_packages(directory):
     paths = [directory.replace("/", ".")]
     for path, directories, filenames in os.walk(directory):
@@ -30,33 +29,7 @@ def config_packages(directory):
             paths.append(f"{path}/{directory}".replace("/", "."))
     return paths
 
-
 setup(
-    name="autolens",
     version=version,
-    description="Open-Source Strong Lensing",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/Jammy2211/PyAutoLens",
-    author="James Nightingale and Richard Hayes",
-    author_email="james.w.nightingale@durham.ac.uk",
-    include_package_data=True,
-    license="MIT License",
-    classifiers=[
-        "Intended Audience :: Science/Research",
-        "Topic :: Scientific/Engineering :: Physics",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    keywords="cli",
-    packages=find_packages(exclude=["docs", "test_autolens", "test_autolens*"])
-    + config_packages("autolens/config"),
     install_requires=requirements,
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
 )
