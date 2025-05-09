04f185b45af1be69bae6d867afcb02cfba933c7c
Jammy2211 <JNightingale2211@gmail.com>
2025-05-09 16:11:28 +0100

    remove setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def config_packages(directory):
-    paths = [directory.replace("/", ".")]
-    for path, directories, filenames in os.walk(directory):
-        for directory in directories:
-            paths.append(f"{path}/{directory}".replace("/", "."))
-    return paths
