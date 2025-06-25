6d01455c9aca1b4e465140b0890c80ffa6ec72c0
Jammy2211 <JNightingale2211@gmail.com>
2025-06-25 19:25:30 +0100

    fix unit tests from rectangular

--- autolens/plot/abstract_plotters.py
+++ autolens/plot/abstract_plotters.py
@@ -2,13 +2,48 @@ from autoarray.plot.wrap.base.abstract import set_backend
 
 set_backend()
 
-from autoarray.plot.abstract_plotters import AbstractPlotter
-
 from autolens.plot.get_visuals.one_d import GetVisuals1D
 from autolens.plot.get_visuals.two_d import GetVisuals2D
 
+from autogalaxy.plot.abstract_plotters import AbstractPlotter
+
+from autogalaxy.plot.mat_plot.one_d import MatPlot1D
+from autogalaxy.plot.mat_plot.two_d import MatPlot2D
+from autogalaxy.plot.visuals.one_d import Visuals1D
+from autogalaxy.plot.visuals.two_d import Visuals2D
+from autogalaxy.plot.include.one_d import Include1D
+from autogalaxy.plot.include.two_d import Include2D
+
 
 class Plotter(AbstractPlotter):
+
+    def __init__(
+        self,
+        mat_plot_1d: MatPlot1D = None,
+        visuals_1d: Visuals1D = None,
+        include_1d: Include1D = None,
+        mat_plot_2d: MatPlot2D = None,
+        visuals_2d: Visuals2D = None,
+        include_2d: Include2D = None,
+    ):
+
+        super().__init__(
+            mat_plot_1d=mat_plot_1d,
+            visuals_1d=visuals_1d,
+            include_1d=include_1d,
+            mat_plot_2d=mat_plot_2d,
+            visuals_2d=visuals_2d,
+            include_2d=include_2d,
+        )
+
+        self.visuals_1d = visuals_1d or Visuals1D()
+        self.include_1d = include_1d or Include1D()
+        self.mat_plot_1d = mat_plot_1d or MatPlot1D()
+
+        self.visuals_2d = visuals_2d or Visuals2D()
+        self.include_2d = include_2d or Include2D()
+        self.mat_plot_2d = mat_plot_2d or MatPlot2D()
+
     @property
     def get_1d(self):
         return GetVisuals1D(visuals=self.visuals_1d, include=self.include_1d)
