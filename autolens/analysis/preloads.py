7b51f6fff200f250fc6aa94eb6b29b08012ec4d6
Jammy2211 <JNightingale2211@gmail.com>
2025-07-24 10:25:31 +0100

    black

--- autolens/analysis/preloads.py
+++ autolens/analysis/preloads.py
@@ -1,25 +1,9 @@
 import jax.numpy as jnp
-import numpy as np
-
-import autoarray as aa
-
-from autolens.lens.tracer import Tracer
-from autolens.lens.to_inversion import TracerToInversion
-
-from autolens.fixtures import make_masked_imaging_7x7_no_blur
 
 
-def mapper_indices_from(model):
-
-    instance = model.instance_from_prior_medians()
-    tracer = Tracer(galaxies=instance.galaxies)
-    tracer_to_inversion = TracerToInversion(
-        dataset=make_masked_imaging_7x7_no_blur(),
-        tracer=tracer,
+def mapper_indices_from(total_linear_light_profiles, total_mapper_pixels):
+    return jnp.arange(
+        total_linear_light_profiles,
+        total_linear_light_profiles + total_mapper_pixels,
+        dtype=int,
     )
-
-    mapper_indices = aa.util.inversion.param_range_list_from(
-        cls=aa.AbstractMapper, linear_obj_list=tracer_to_inversion.linear_obj_list
-    )
-
-    return jnp.arange(np.min(mapper_indices), np.max(mapper_indices), dtype=int)
