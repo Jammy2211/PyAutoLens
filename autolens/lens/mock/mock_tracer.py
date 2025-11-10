823a355134abefe69877f0017af7e0fdb6a23b55
Jammy2211 <JNightingale2211@gmail.com>
2025-11-10 13:11:18 +0000

    fix unit test

--- autolens/lens/mock/mock_tracer.py
+++ autolens/lens/mock/mock_tracer.py
@@ -8,7 +8,7 @@ class MockTracer:
         self.image_plane_mesh_grid_pg_list = image_plane_mesh_grid_pg_list
         self._traced_grid_2d_list_from = traced_grid_2d_list_from
 
-    def traced_grid_2d_list_from(self, grid):
+    def traced_grid_2d_list_from(self, grid, xp=np):
         return self._traced_grid_2d_list_from
 
     def plane_index_via_redshift_from(self, redshift):
