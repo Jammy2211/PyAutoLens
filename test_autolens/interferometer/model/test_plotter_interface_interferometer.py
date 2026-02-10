29177d147eb3bc033e11cd62fb77760100aeff3c
Jammy2211 <JNightingale2211@gmail.com>
2026-02-10 09:29:21 +0000

    fix unitt est by making interferometer plotter look for galaxy images.fits

--- test_autolens/interferometer/model/test_plotter_interface_interferometer.py
+++ test_autolens/interferometer/model/test_plotter_interface_interferometer.py
@@ -32,7 +32,7 @@ def test__fit_interferometer(
     assert path.join(plot_path, "subplot_fit_dirty_images.png") in plot_patch.paths
 
     image = al.ndarray_via_fits_from(
-        file_path=path.join(plot_path, "model_galaxy_images.fits"), hdu=0
+        file_path=path.join(plot_path, "galaxy_images.fits"), hdu=0
     )
 
     assert image.shape == (5, 5)
